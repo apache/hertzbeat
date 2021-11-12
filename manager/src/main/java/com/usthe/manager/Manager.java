8005420f3cb410529c3e2f724b8e668d73146c16
tomsun28 <tomsun28@outlook.com>
2021-11-12 16:06:08 +0800

    init swagger config

---
+++ manager/src/main/java/com/usthe/manager/Manager.java
@@ -0,0 +1,17 @@
+package com.usthe.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ */
+
+@SpringBootApplication
+public class Manager {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Manager.class, args);
+    }
+}
---
+++ manager/src/main/java/com/usthe/manager/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.usthe.manager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+/**
+ * swagger config
+ * url: /swagger-ui/
+ *
+ *
+ */
+@Configuration
+@EnableOpenApi
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .enable(true)
+                .groupName("usthe.com")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "usthe api",
+                "monitor project",
+                "v1.0",
+                "usthe.com",
+                new Contact("tom", "usthe.com", "tomsun28@outlook.com"),
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                Collections.emptyList());
+    }
+}
