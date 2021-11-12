9e3e6f30f654cff8fe67e24ddf4fe8dd538d9f4a
tomsun28 <tomsun28@outlook.com>
2021-11-12 16:06:08 +0800

    [scheduler] 初步完成周期任务调度器编码

---
+++ manager/src/main/java/com/usthe/manager/Manager.java
@@ -0,0 +1,17 @@
+package com.usthe.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tomsun28
+ * @date 2021/11/11 16:45
+ */
+
+@SpringBootApplication
+public class Manager {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Manager.class, args);
+    }
+}
