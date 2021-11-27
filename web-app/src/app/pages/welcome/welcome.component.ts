bd1530b225bac42088b9ac8da346d229145f24a9
tomsun28 <tomsun28@outlook.com>
2021-11-27 11:14:26 +0800

    [web-app] 初始化web端，angular+ts+less+ng-zorro

---
+++ web-app/src/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.less']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
