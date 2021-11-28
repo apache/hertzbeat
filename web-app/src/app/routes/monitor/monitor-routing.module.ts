fa65c31753c37ecce6a1828c53ca4674beb9940b
tomsun28 <tomsun28@outlook.com>
2021-11-28 14:58:20 +0800

    [web-app] 菜单功能布局大致样式定义

---
+++ web-app/src/app/routes/monitor/monitor-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MonitorRoutingModule { }
