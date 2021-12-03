c2842183a49fd5d55f0e21ccccdf6553ed250fb0
tomsun28 <tomsun28@outlook.com>
2021-12-03 16:46:10 +0800

    [web-app] bugfix-小部件渲染，新增网站类型监控

--- web-app/src/app/layout/basic/widgets/notify.component.ts
+++ web-app/src/app/layout/basic/widgets/notify.component.ts
@@ -23,25 +23,11 @@ import { NzMessageService } from 'ng-zorro-antd/message';
 export class HeaderNotifyComponent {
   data: NoticeItem[] = [
     {
-      title: '通知',
+      title: '告警通知',
       list: [],
-      emptyText: '你已查看所有通知',
+      emptyText: '暂无告警通知',
       emptyImage: 'https://gw.alipayobjects.com/zos/rmsportal/wAhyIChODzsoKIOBHcBk.svg',
-      clearText: '清空通知'
-    },
-    {
-      title: '消息',
-      list: [],
-      emptyText: '您已读完所有消息',
-      emptyImage: 'https://gw.alipayobjects.com/zos/rmsportal/sAuJeJzSKbUmHfBQRzmZ.svg',
-      clearText: '清空消息'
-    },
-    {
-      title: '待办',
-      list: [],
-      emptyText: '你已完成所有待办',
-      emptyImage: 'https://gw.alipayobjects.com/zos/rmsportal/HsIsxMZiWKrNUavQUXqx.svg',
-      clearText: '清空待办'
+      clearText: '清空告警'
     }
   ];
   count = 5;
@@ -87,94 +73,38 @@ export class HeaderNotifyComponent {
         {
           id: '000000001',
           avatar: 'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
-          title: '你收到了 14 份新周报',
+          title: '监控-MYSQL_192.135.34.2-发出2级告警',
           datetime: add(now, { days: 10 }),
-          type: '通知'
+          type: '告警通知'
         },
         {
           id: '000000002',
-          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png',
-          title: '你推荐的 曲妮妮 已通过第三轮面试',
+          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
+          title: '监控-MYSQL_192.135.44.2-发出1级告警',
           datetime: add(now, { days: -3 }),
-          type: '通知'
+          type: '告警通知'
         },
         {
           id: '000000003',
-          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/kISTdvpyTAhtGxpovNWd.png',
-          title: '这种模板可以区分多种通知类型',
+          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
+          title: '监控-WEBSITE_www.baidu.com-发出4级告警',
           datetime: add(now, { months: -3 }),
           read: true,
-          type: '通知'
+          type: '告警通知'
         },
         {
           id: '000000004',
           avatar: 'https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png',
-          title: '左侧图标用于区分不同的类型',
+          title: '监控-REDIS_192.34.55.3-发出4级告警',
           datetime: add(now, { years: -1 }),
-          type: '通知'
+          type: '告警通知'
         },
         {
           id: '000000005',
           avatar: 'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
-          title: '内容不要超过两行字，超出时自动截断',
-          datetime: '2017-08-07',
-          type: '通知'
-        },
-        {
-          id: '000000006',
-          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
-          title: '曲丽丽 评论了你',
-          description: '描述信息描述信息描述信息',
-          datetime: '2017-08-07',
-          type: '消息'
-        },
-        {
-          id: '000000007',
-          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
-          title: '朱偏右 回复了你',
-          description: '这种模板用于提醒谁与你发生了互动，左侧放『谁』的头像',
-          datetime: '2017-08-07',
-          type: '消息'
-        },
-        {
-          id: '000000008',
-          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
-          title: '标题',
-          description: '这种模板用于提醒谁与你发生了互动，左侧放『谁』的头像',
-          datetime: '2017-08-07',
-          type: '消息'
-        },
-        {
-          id: '000000009',
-          title: '任务名称',
-          description: '任务需要在 2017-01-12 20:00 前启动',
-          extra: '未开始',
-          status: 'todo',
-          type: '待办'
-        },
-        {
-          id: '000000010',
-          title: '第三方紧急代码变更',
-          description: '冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务',
-          extra: '马上到期',
-          status: 'urgent',
-          type: '待办'
-        },
-        {
-          id: '000000011',
-          title: '信息安全考试',
-          description: '指派竹尔于 2017-01-09 前完成更新并发布',
-          extra: '已耗时 8 天',
-          status: 'doing',
-          type: '待办'
-        },
-        {
-          id: '000000012',
-          title: 'ABCD 版本发布',
-          description: '冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务',
-          extra: '进行中',
-          status: 'processing',
-          type: '待办'
+          title: '监控-REDIS_192.34.55.6-发出4级告警',
+          datetime: '2020-08-07',
+          type: '告警通知'
         }
       ]);
 
@@ -184,7 +114,13 @@ export class HeaderNotifyComponent {
   }
 
   clear(type: string): void {
-    this.msg.success(`清空了 ${type}`);
+    this.loading = true;
+    setTimeout(() => {
+      this.data = this.updateNoticeData([]);
+      this.loading = false;
+      this.cdr.detectChanges();
+    }, 500);
+    this.msg.success(`已清空 ${type}`);
   }
 
   select(res: NoticeIconSelect): void {
