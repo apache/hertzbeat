b1403cea81c9360161ac851284f72c152931c4ea
tomsun28 <tomsun28@outlook.com>
2021-12-06 22:14:35 +0800

    [web-app] 监控项数据详情编码

---
+++ web-app/src/app/routes/monitor/monitor-data-chart/monitor-data-chart.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MonitorDataChartComponent } from './monitor-data-chart.component';
+
+describe('MonitorDataChartComponent', () => {
+  let component: MonitorDataChartComponent;
+  let fixture: ComponentFixture<MonitorDataChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MonitorDataChartComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MonitorDataChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
