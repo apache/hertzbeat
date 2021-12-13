826fe394f38d7eea109f55415ced5d2c9690ce79
tomsun28 <tomsun28@outlook.com>
2021-12-13 14:51:08 +0800

    [web-app] 告警中心告警列表展示

---
+++ web-app/src/app/service/alert.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AlertService } from './alert.service';
+
+describe('AlertService', () => {
+  let service: AlertService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AlertService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
