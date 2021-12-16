c0c8d8179be6db18af5c19e5c36bb1527b411c9e
tomsun28 <tomsun28@outlook.com>
2021-12-16 22:10:57 +0800

    [web-app] 告警通知-接收人配置和消息策略配置。全局字体基础大小修改为12px

---
+++ web-app/src/app/service/notice-receiver.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Message} from "../pojo/Message";
+import {NoticeReceiver} from "../pojo/NoticeReceiver";
+
+const notice_receiver_uri = '/notice/receiver';
+const notice_receivers_uri = '/notice/receivers';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoticeReceiverService {
+
+  constructor(private http : HttpClient) { }
+
+  public newReceiver(body: NoticeReceiver) : Observable<Message<any>> {
+    return this.http.post<Message<any>>(notice_receiver_uri, body);
+  }
+
+  public editReceiver(body: NoticeReceiver) : Observable<Message<any>> {
+    return this.http.put<Message<any>>(notice_receiver_uri, body);
+  }
+  public deleteReceiver(receiverId: number) : Observable<Message<any>> {
+    return this.http.delete<Message<any>>(`${notice_receiver_uri}/${receiverId}`);
+  }
+
+  public getReceivers() : Observable<Message<NoticeReceiver[]>> {
+    return this.http.get<Message<NoticeReceiver[]>>(notice_receivers_uri);
+  }
+
+}
