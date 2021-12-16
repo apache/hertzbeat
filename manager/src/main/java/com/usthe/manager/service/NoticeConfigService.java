30ec5e327facacde8f765bb905e607c774a43c62
tomsun28 <tomsun28@outlook.com>
2021-12-16 18:00:20 +0800

    [manager] 消息通知配置API-消息接收人管理，消息通知策略管理

---
+++ manager/src/main/java/com/usthe/manager/service/NoticeConfigService.java
@@ -0,0 +1,65 @@
+package com.usthe.manager.service;
+
+import com.usthe.manager.pojo.entity.NoticeReceiver;
+import com.usthe.manager.pojo.entity.NoticeRule;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
+/**
+ * 消息通知配置接口
+ * @author tom
+ * @date 2021/12/16 16:14
+ */
+public interface NoticeConfigService {
+
+    /**
+     * 动态条件查询
+     * @param specification 查询条件
+     * @return 查询结果
+     */
+    List<NoticeReceiver> getNoticeReceivers(Specification<NoticeReceiver> specification);
+
+    /**
+     * 动态条件查询
+     * @param specification 查询条件
+     * @return 查询结果
+     */
+    List<NoticeRule> getNoticeRules(Specification<NoticeRule> specification);
+
+    /**
+     * 新增一个通知接收人
+     * @param noticeReceiver 接收人信息
+     */
+    void addReceiver(NoticeReceiver noticeReceiver);
+
+    /**
+     * 修改通知接收人
+     * @param noticeReceiver 接收人信息
+     */
+    void editReceiver(NoticeReceiver noticeReceiver);
+
+    /**
+     * 根据接收人ID删除接收人信息
+     * @param receiverId 接收人ID
+     */
+    void deleteReceiver(Long receiverId);
+
+    /**
+     * 新增通知策略
+     * @param noticeRule 通知策略
+     */
+    void addNoticeRule(NoticeRule noticeRule);
+
+    /**
+     * 修改通知策略
+     * @param noticeRule 通知策略
+     */
+    void editNoticeRule(NoticeRule noticeRule);
+
+    /**
+     * 删除指定的通知策略
+     * @param ruleId 通知策略ID
+     */
+    void deleteNoticeRule(Long ruleId);
+}
---
+++ manager/src/main/java/com/usthe/manager/service/NoticeDispatchService.java
@@ -0,0 +1,10 @@
+package com.usthe.manager.service;
+
+/**
+ * 消息通知转发接口
+ * @author tom
+ * @date 2021/12/16 16:14
+ */
+public interface NoticeDispatchService {
+
+}
