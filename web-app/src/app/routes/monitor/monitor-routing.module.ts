251c4aaedbdf0516ac2dbb6b3d304366ac70b982
tomsun28 <tomsun28@outlook.com>
2022-01-30 10:19:29 +0800

    [webapp] 监控详情页面标题

--- web-app/src/app/routes/monitor/monitor-routing.module.ts
+++ web-app/src/app/routes/monitor/monitor-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: '', component: MonitorListComponent },
   { path: 'new', component: MonitorNewComponent },
   { path: ':monitorId/edit', component: MonitorEditComponent },
-  { path: ':monitorId', component: MonitorDetailComponent },
+  { path: ':monitorId', component: MonitorDetailComponent, data: { title: '监控详情' } },
   { path: '**', component: MonitorListComponent }
 ];
 
