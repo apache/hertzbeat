801d066212e176d1131ed0ef47ea6baa72870d0a
tomsun28 <tomsun28@outlook.com>
2022-02-05 15:50:02 +0800

    [home]官方网站内容更新

--- home/src/constants.js
+++ home/src/constants.js
@@ -13,11 +13,6 @@ export const features = [{
                 TANCLOUD: <strong>TANCLOUD</strong>,
                 SAAS: <strong>SAAS</strong>,
                 console: <a href={'https://www.console.tancloud.cn'}><strong>注册登陆</strong></a>,
-                authorization: <strong>authorization</strong>,
-                simple: <strong>simple</strong>,
-                useful: <strong>useful</strong>,
-                interface: <strong>扩展接口</strong>,
-                sample: <strong>样例</strong>,
                 doc: <strong>文档</strong>,
                 br: <br/>
             }}>
@@ -38,20 +33,12 @@ export const features = [{
                 WebSockets: <strong>WebSockets</strong>,
                 Servlet: <strong>Servlet</strong>,
                 JaxRs: <strong>JAX-RS</strong>,
-                native: <strong>Native</strong>,
-                SpringBoot: <strong>Spring Boot</strong>,
-                SpringWebFlux: <strong>Spring WebFlux</strong>,
-                Javalin: <strong>Javalin</strong>,
-                Quarkus: <strong>Quarkus</strong>,
-                Ktor: <strong>Ktor</strong>,
-                Micronaut: <strong>Micronaut</strong>,
-                Solon: <strong>Solon</strong>,
-                Jfinal: <strong>Jfinal</strong>,
+                custom: <a href={'/docs/advanced/extend-point'}><strong>自定义监控</strong></a>,
                 br: <br/>
             }}>
                 {'TANCLOUD目前支持对网站，API，PING连通性，端口可用性，MYSQL数据库等的监控，不久我们将兼容 prometheus 协议，提供更多的监控类型和性能指标。{br}' +
                 '我们提供了更自由化的阈值告警配置，支持邮箱，短信，webhook等告警通知，更多三方应用通知正在集成中。{br}' +
-                '不同团队的监控需求千变万化，我们提供自定义监控，仅需配置YML就能快速接入监控系统。'
+                '不同团队的监控需求千变万化，我们提供{custom}，仅需配置YML就能快速接入监控系统。'
                 }
             </Translate>
         </>
@@ -64,20 +51,14 @@ export const features = [{
         description: (
             <>
                 <Translate values={{
-                    jwt: <strong>JWT</strong>,
-                    basic: <strong>Basic Auth</strong>,
-                    digest: <strong>Digest Auth</strong>,
-                    dynamic: <strong>Dynamic</strong>,
-                    perm: <strong>permissions</strong>,
-                    dynamicCn: <strong>动态修改权限</strong>,
-                    high: <strong>High performance</strong>,
-                    highCn: <strong>高性能</strong>,
-                    tree: <strong>Dictionary Matching Tree</strong>,
-                    treeCn: <strong>改进的字典匹配树</strong>,
+                    github: <a href={'https://github.com/usthe/hertzbeat'}><strong>HertzBeat Github 代码仓库</strong></a>,
+                    gitee: <a href={'https://gitee.com/usthe/hertzbeat'}><strong>HertzBeat Gitee 代码仓库</strong></a>,
                     br: <br/>
                 }}>
                     {'TANCLOUD监控系统代码开源，非常欢迎任何对此有兴趣的同学参与中来，我们一起进步，丰富的资源文档正在完善中。{br}' +
-                    '我们相信开源改变世界！'
+                    '中二的我们相信开源改变世界！{br}' +
+                    '{github} {br}' +
+                    '{gitee}'
                     }
                 </Translate>
             </>
