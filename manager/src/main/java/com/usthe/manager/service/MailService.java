c65ea28901ce3bacf9c9806483040001d30eef4b
chenghua <huacheng>
2022-02-19 19:07:37 +0800

    add email alram template html

---
+++ manager/src/main/java/com/usthe/manager/service/MailService.java
@@ -0,0 +1,24 @@
+package com.usthe.manager.service;
+
+import com.usthe.common.entity.alerter.Alert;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Service;
+
+/**
+ * 邮箱发送服务
+ *
+ *
+ * @version 1.0
+ *
+ * @Description
+ */
+public interface MailService {
+
+    /**
+     * 构建告警邮件模版
+     * @param alert     告警信息
+     * @return          邮件内容
+     */
+    String buildHTMLTemplate(Alert alert);
+}
