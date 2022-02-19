f1bd17a7af6d25eada147fac184e44bdff9c4274
tomsun28 <tomsun28@outlook.com>
2022-02-19 14:32:07 +0800

    [web-app]bugfix email notice diapaly, resize pop

--- web-app/src/app/pojo/NoticeReceiver.ts
+++ web-app/src/app/pojo/NoticeReceiver.ts
@@ -4,7 +4,7 @@ export class NoticeReceiver {
   // 通知信息方式: 0-手机短信 1-邮箱 2-webhook 3-微信公众号
   type: number = 1;
   phone!: string;
-  email!: number;
+  email!: string;
   hookUrl!: string;
   wechatId!: string;
   creator!: string;
