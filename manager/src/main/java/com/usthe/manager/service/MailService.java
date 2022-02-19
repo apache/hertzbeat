1fe88d3d9cac5a0ce1f1e66a7ed3802f7040958d
学习代码的小白 <8145789+learning-code@user.noreply.gitee.com>
2022-02-19 13:59:36 +0000

    !5 新增告警邮件模版 * Merge remote-tracking branch 'origin/master' * fix: 优化代码注释，以及日志输出 #I4U9BT * Update web-app/package.json * Update web-app/package.json * Update web-app/package.json * Update web-app/package.json * Update manager/src/main/resources/application.yml * Update manager/src/main/resources/application.yml * feature: 新增邮件告警模版 #I4U9BT * feature: 新增controller接口入参限定修饰符 #I4U9BT

---
+++ manager/src/main/java/com/usthe/manager/service/MailService.java
@@ -0,0 +1,24 @@
+package com.usthe.manager.service;
+
+import com.usthe.common.entity.alerter.Alert;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Service;
+
+/**
+ * 邮箱发送服务
+ *
+ * @author 花城
+ * @version 1.0
+ * @date 2022/2/19 6:11 下午
+ * @Description
+ */
+public interface MailService {
+
+    /**
+     * 构建告警邮件模版
+     * @param alert     告警信息
+     * @return          邮件内容
+     */
+    String buildHTMLTemplate(Alert alert);
+}
