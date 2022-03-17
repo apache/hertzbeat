0e3ba8474d62cc4e0e354525d62469d84f608e5f
tomsun28 <tomsun28@outlook.com>
2022-03-17 15:09:08 +0800

    [script, webapp]时间本地时区格式化 (#35)

---
+++ web-app/src/app/shared/pipe/timezone.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TimezonePipe } from './timezone.pipe';
+
+describe('TimezonePipe', () => {
+  it('create an instance', () => {
+    const pipe = new TimezonePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- web-app/src/app/shared/utils/yuan.ts
+++ web-app/src/app/shared/utils/yuan.ts
@@ -1,6 +1,7 @@
 /**
  * 转化成RMB元字符串
  *
+ * @param value 值
  * @param digits 当数字类型时，允许指定小数点后数字的个数，默认2位小数
  */
 export function yuan(value: number | string, digits: number = 2): string {
