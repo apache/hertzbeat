f710795f0fb8e90fdf25d471664014dd3cafe8b1
tomsun28 <tomsun28@outlook.com>
2022-03-17 15:09:08 +0800

    [script, webapp]时间本地时区格式化 (#35)

---
+++ web-app/src/app/shared/pipe/timezone.pipe.ts
@@ -0,0 +1,20 @@
+import { formatDate, Location } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timezone'
+})
+export class TimezonePipe implements PipeTransform {
+  timeZone: string = 'Asia/Shanghai';
+
+  constructor() {
+    this.timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    if (this.timeZone == undefined) {
+      this.timeZone = 'Asia/Shanghai';
+    }
+  }
+
+  transform(value: any): string {
+    return formatDate(value, 'YYYY-MM-DD HH:mm:ss', 'zh-cn');
+  }
+}
