c392aeaa09627a8e64fc37688258c845d4b2b5f7
tomsun28 <tomsun28@outlook.com>
2022-04-03 21:35:56 +0800

    [monitor]feature:param yml support key-value map (#57)

--- web-app/src/app/pojo/ParamDefine.ts
+++ web-app/src/app/pojo/ParamDefine.ts
@@ -9,4 +9,7 @@ export class ParamDefine {
   limit: number | undefined;
   //'[{"label":"GET请求","value":"GET"},{"label":"PUT请求","value":"PUT"}]'
   options!: any[];
+  // 当type为key-value时有效,表示别名描述
+  keyAlias!: string;
+  valueAlias!: string;
 }
