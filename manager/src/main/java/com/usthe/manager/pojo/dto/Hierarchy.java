5abbea10ba329811265d3def454a780005ba7f29
huacheng <1252532896@qq.com>
2022-04-14 21:46:58 +0800

    feat: [manager]feature:App/Account Controller compatible with Chinese and English #wqh

--- manager/src/main/java/com/usthe/manager/pojo/dto/Hierarchy.java
+++ manager/src/main/java/com/usthe/manager/pojo/dto/Hierarchy.java
@@ -11,8 +11,11 @@ import java.util.List;
 import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_WRITE;
 
 /**
+ * Hierarchical structure
  * 层级关系结构
+ * eg: Monitoring Type Indicator Group Indicator Information Hierarchy Relationship
  * eg: 监控类型指标组指标信息层级关系
+ *
  * @author tom
  * @date 2021/12/12 16:23
  */
@@ -21,18 +24,33 @@ import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_WRITE;
 @Data
 public class Hierarchy {
 
+    /**
+     * Category value
+     */
     @ApiModelProperty(value = "类别值", example = "os", accessMode = READ_WRITE, position = 0)
     String category;
 
+    /**
+     * Attribute value
+     */
     @ApiModelProperty(value = "属性值", example = "linux", accessMode = READ_WRITE, position = 1)
     String value;
 
+    /**
+     * Attribute internationalization tag
+     */
     @ApiModelProperty(value = "属性国际化标签", example = "Linux系统", accessMode = READ_WRITE, position = 2)
     String label;
 
+    /**
+     * Is it a leaf node
+     */
     @ApiModelProperty(value = "是否是叶子节点", example = "true", accessMode = READ_WRITE, position = 3)
     Boolean isLeaf = false;
 
+    /**
+     * Next level of association
+     */
     @ApiModelProperty(value = "下一关联层级", accessMode = READ_WRITE, position = 4)
     private List<Hierarchy> children;
 }
--- manager/src/main/java/com/usthe/manager/service/AppService.java
+++ manager/src/main/java/com/usthe/manager/service/AppService.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * Monitoring Type Management Interface
  * 监控类型管理接口
  *
  * @author tomsun28
@@ -16,10 +17,11 @@ import java.util.Map;
 public interface AppService {
 
     /**
+     * Query the defined parameter structure based on the monitoring type
      * 根据监控类型查询定义的参数结构
      *
-     * @param app 监控类型
-     * @return 参数结构列表
+     * @param app Monitoring type   监控类型
+     * @return list of parameter structures     参数结构列表
      */
     List<ParamDefine> getAppParamDefines(String app);
 
@@ -35,18 +37,20 @@ public interface AppService {
     Job getAppDefine(String app) throws IllegalArgumentException;
 
     /**
+     * Get defined monitoring I 18 N resources
      * 获取定义的监控I18N资源
      *
-     * @param lang 语言类型
-     * @return I18N资源
+     * @param lang Language type    语言类型
+     * @return I18N Resources     I18N资源
      */
     Map<String, String> getI18nResources(String lang);
 
     /**
+     * Query all types of monitoring - indicator group - indicator level
      * 查询所有监控的类型-指标组-指标层级
      *
-     * @param lang 语言
-     * @return 层级信息
+     * @param lang language     语言
+     * @return level information        层级信息
      */
     List<Hierarchy> getAllAppHierarchy(String lang);
 
