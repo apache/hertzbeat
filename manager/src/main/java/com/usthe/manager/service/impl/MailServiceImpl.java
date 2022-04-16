2cca0d3f0ce7b8c2d6ee3f8ea088128eb32e37a9
会编程的王学长 <71161318+wang1027-wqh@users.noreply.github.com>
2022-04-16 10:34:20 +0800

    feature:Add alarm template custom console  and help document  (#93)
    * feat: [manager,alerter,home]feature:Add alarm template custom console and help document #wqh
    
    * Update common/src/main/java/com/usthe/common/util/CommonConstants.java
    
    Co-authored-by: tomsun28 <tomsun28@outlook.com>
    
    Co-authored-by: tomsun28 <tomsun28@outlook.com>

--- manager/src/main/java/com/usthe/manager/service/impl/MailServiceImpl.java
+++ manager/src/main/java/com/usthe/manager/service/impl/MailServiceImpl.java
@@ -1,5 +1,6 @@
 package com.usthe.manager.service.impl;
 
+import com.usthe.alert.AlerterProperties;
 import com.usthe.common.entity.alerter.Alert;
 import com.usthe.common.util.CommonUtil;
 import com.usthe.manager.service.MailService;
@@ -11,6 +12,7 @@ import org.thymeleaf.context.Context;
 import javax.annotation.Resource;
 
 /**
+ * Mailbox sending service interface implementation class
  * 邮箱发送服务接口实现类
  *
  * @author 花城
@@ -23,16 +25,20 @@ public class MailServiceImpl implements MailService {
 
     @Resource
     private TemplateEngine templateEngine;
+    @Resource
+    private AlerterProperties alerterProperties;
 
     @Override
     public String buildAlertHtmlTemplate(final Alert alert) {
+        // Introduce thymeleaf context parameters to render pages
         // 引入thymeleaf上下文参数渲染页面
         Context context = new Context();
-        context.setVariable("target",alert.getTarget());
-        context.setVariable("monitorId",alert.getMonitorId());
-        context.setVariable("monitorName",alert.getMonitorName());
+        context.setVariable("target", alert.getTarget());
+        context.setVariable("monitorId", alert.getMonitorId());
+        context.setVariable("monitorName", alert.getMonitorName());
         context.setVariable("priority", CommonUtil.transferAlertPriority(alert.getPriority()));
-        context.setVariable("content",alert.getContent());
+        context.setVariable("content", alert.getContent());
+        context.setVariable("consoleUrl", alerterProperties.getConsoleUrl());
         return templateEngine.process("mailAlarm", context);
     }
 }
