de71384f5094f4ca32c5e916451e1421a2796fa7
tomsun28 <tomsun28@outlook.com>
2022-05-15 17:25:37 +0800

    [manager,webapp]feature: support alert define appHierarchy i18n (#124)

--- manager/src/main/java/com/usthe/manager/controller/AppController.java
+++ manager/src/main/java/com/usthe/manager/controller/AppController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Locale;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -60,8 +61,13 @@ public class AppController {
         if (lang == null || "".equals(lang)) {
             lang = "zh-CN";
         }
-        lang = "zh-cn".equalsIgnoreCase(lang) ? "zh-CN" : lang;
-        lang = "en-us".equalsIgnoreCase(lang) ? "en-US" : lang;
+        if (lang.contains(Locale.ENGLISH.getLanguage())) {
+            lang = "en-US";
+        } else if (lang.contains(Locale.CHINESE.getLanguage())) {
+            lang = "zh-CN";
+        } else {
+            lang = "en-US";
+        }
         List<Hierarchy> appHierarchies = appService.getAllAppHierarchy(lang);
         return ResponseEntity.ok(new Message<>(appHierarchies));
     }
