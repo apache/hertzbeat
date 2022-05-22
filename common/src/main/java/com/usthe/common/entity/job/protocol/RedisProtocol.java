6b02af0aedbe6cdf5e66becdc28f353ef6f6caa1
观沧海 <gcwm99@gmail.com>
2022-05-22 23:29:11 +0800

    [monitor] feature: support redis monitor protocol (#142)
    * [manager]feature: refactor DispatchAlarm
    
    * [alerter]bugfix nextEvalInterval npe
    
    * [monitor] 支持Redis监控类型
    
    * [monitor] Redis监控类型参数校验
    
    Co-authored-by: Musk.Chen <Musk.Chen@fanruan.com>
    Co-authored-by: tomsun28 <tomsun28@outlook.com>

---
+++ common/src/main/java/com/usthe/common/entity/job/protocol/RedisProtocol.java
@@ -0,0 +1,44 @@
+package com.usthe.common.entity.job.protocol;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author <a href="mailto:Musk.Chen@fanruan.com">Musk.Chen</a>
+ * @version 1.0
+ * Created by Musk.Chen on 2022/5/17
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RedisProtocol {
+
+    /**
+     * 对端主机ip或域名
+     */
+    private String host;
+
+    /**
+     * 端口号
+     */
+    private String port;
+
+    /**
+     * Redis用户名(可选)
+     */
+    private String username;
+
+    /**
+     * Redis密码(可选)
+     */
+    private String password;
+
+    /**
+     * 超时时间
+     */
+    private String timeout;
+
+}
