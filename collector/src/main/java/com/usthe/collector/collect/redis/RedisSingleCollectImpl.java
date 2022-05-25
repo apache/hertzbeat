af145281c7f48b01307c39433c58c486757a6c5e
tomsun28 <tomsun28@outlook.com>
2022-05-25 08:29:39 +0800

    [collector]feature: reuse redis connection cache (#146)
      [manager]feature: refactor DispatchAlarm
    
      [alerter]bugfix nextEvalInterval npe
    
       
    
      [monitor] Redis监控类型参数校验
    
      [monitor]feature: update redis protocol
    
      [collector]feature: reuse redis connection cache
    
    Co-authored-by: Musk.Chen <Musk.Chen@fanruan.com>

--- collector/src/main/java/com/usthe/collector/collect/redis/RedisSingleCollectImpl.java
+++ collector/src/main/java/com/usthe/collector/collect/redis/RedisSingleCollectImpl.java
@@ -1,20 +1,27 @@
 package com.usthe.collector.collect.redis;
 
 import com.usthe.collector.collect.AbstractCollect;
+import com.usthe.collector.collect.common.cache.CacheIdentifier;
 import com.usthe.collector.collect.common.cache.CommonCache;
+import com.usthe.collector.collect.common.cache.RedisConnect;
+import com.usthe.collector.util.CollectUtil;
 import com.usthe.common.entity.job.Metrics;
 import com.usthe.common.entity.job.protocol.RedisProtocol;
 import com.usthe.common.entity.message.CollectRep;
 import com.usthe.common.util.CommonConstants;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisConnectionException;
+import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Redis 单机指标收集器
@@ -32,37 +39,49 @@ public class RedisSingleCollectImpl extends AbstractCollect {
 
     @Override
     public void collect(CollectRep.MetricsData.Builder builder, long appId, String app, Metrics metrics) {
-        preCheck(metrics);
-        RedisClient redisClient = buildClient(metrics.getRedis());
-
-        StatefulRedisConnection<String, String> connection = redisClient.connect();
-
-        String info = connection.sync().info();
-        Map<String, String> valueMap = parseInfo(info);
-        if (log.isDebugEnabled()) {
-            log.debug("[RedisSingleCollectImpl] fetch redis info");
-            valueMap.forEach((k, v) -> log.debug("{} : {}", k, v));
+        try {
+            preCheck(metrics);
+        } catch (Exception e) {
+            builder.setCode(CollectRep.Code.FAIL);
+            builder.setMsg(e.getMessage());
+            return;
         }
-        CollectRep.ValueRow.Builder valueRowBuilder = CollectRep.ValueRow.newBuilder();
-        metrics.getAliasFields().forEach(it -> {
-            if (valueMap.containsKey(it)) {
-                String fieldValue = valueMap.get(it);
-                if (fieldValue == null) {
-                    valueRowBuilder.addColumns(CommonConstants.NULL_VALUE);
-                } else {
-                    valueRowBuilder.addColumns(fieldValue);
-                }
-            } else {
-                valueRowBuilder.addColumns(CommonConstants.NULL_VALUE);
+        try {
+            StatefulRedisConnection<String, String> connection = getConnection(metrics.getRedis());
+            String info = connection.sync().info();
+            Map<String, String> valueMap = parseInfo(info);
+            if (log.isDebugEnabled()) {
+                log.debug("[RedisSingleCollectImpl] fetch redis info");
+                valueMap.forEach((k, v) -> log.debug("{} : {}", k, v));
             }
-        });
-        builder.addValues(valueRowBuilder.build());
-
-        connection.closeAsync();
+            CollectRep.ValueRow.Builder valueRowBuilder = CollectRep.ValueRow.newBuilder();
+            metrics.getAliasFields().forEach(it -> {
+                if (valueMap.containsKey(it)) {
+                    String fieldValue = valueMap.get(it);
+                    if (fieldValue == null) {
+                        valueRowBuilder.addColumns(CommonConstants.NULL_VALUE);
+                    } else {
+                        valueRowBuilder.addColumns(fieldValue);
+                    }
+                } else {
+                    valueRowBuilder.addColumns(CommonConstants.NULL_VALUE);
+                }
+            });
+            builder.addValues(valueRowBuilder.build());
+        } catch (RedisConnectionException connectionException) {
+            log.info("[redis connection] error: {}", connectionException.getMessage());
+            builder.setCode(CollectRep.Code.UN_CONNECTABLE);
+            builder.setMsg(connectionException.getMessage());
+        } catch (Exception e) {
+            // todo 根据不同的异常判断是连接失败还是采集失败
+            log.warn("[redis collect] error: {}", e.getMessage(), e);
+            builder.setCode(CollectRep.Code.FAIL);
+            builder.setMsg(e.getMessage());
+        }
     }
 
     /**
-     * pre check params
+     * preCheck params
      */
     private void preCheck(Metrics metrics) {
         if (metrics == null || metrics.getRedis() == null) {
@@ -73,23 +92,54 @@ public class RedisSingleCollectImpl extends AbstractCollect {
         Assert.hasText(redisProtocol.getPort(), "Redis Protocol port is required.");
     }
 
+    private StatefulRedisConnection<String, String> getConnection(RedisProtocol redisProtocol) {
+        CacheIdentifier identifier = CacheIdentifier.builder()
+                .ip(redisProtocol.getHost())
+                .port(redisProtocol.getPort())
+                .username(redisProtocol.getUsername())
+                .password(redisProtocol.getPassword())
+                .build();
+        StatefulRedisConnection<String, String> connection = null;
+        Optional<Object> cacheOption = CommonCache.getInstance().getCache(identifier, true);
+        if (cacheOption.isPresent()) {
+            RedisConnect redisConnect = (RedisConnect) cacheOption.get();
+            connection = redisConnect.getConnection();
+            if (!connection.isOpen()) {
+                try {
+                    connection.closeAsync();
+                } catch (Exception e) {
+                    log.info("The redis connect form cache, close error: {}", e.getMessage());
+                }
+                connection = null;
+                CommonCache.getInstance().removeCache(identifier);
+            }
+        }
+        if (connection != null) {
+            return connection;
+        }
+        // reuse connection failed, new one
+        RedisClient redisClient = buildClient(redisProtocol);
+        connection = redisClient.connect();
+        CommonCache.getInstance().addCache(identifier, new RedisConnect(connection));
+        return connection;
+    }
+
     /**
      * build single redis client
-     *
      * @param redisProtocol redis protocol config
      * @return redis single client
      */
     private RedisClient buildClient(RedisProtocol redisProtocol) {
-        String uri = String.format("redis://%s:%d", redisProtocol.getHost(), Integer.parseInt(redisProtocol.getPort()));
-        CommonCache commonCache = CommonCache.getInstance();
-        return commonCache.getCache(uri, true)
-                .map(r -> (RedisClient) r)
-                .orElseGet(() -> {
-                    // create new redis client
-                    RedisClient redisClient = RedisClient.create(uri);
-                    commonCache.addCache(uri, redisClient);
-                    return redisClient;
-                });
+        RedisURI redisUri = RedisURI.create(redisProtocol.getHost(), Integer.parseInt(redisProtocol.getPort()));
+        if (StringUtils.hasText(redisProtocol.getUsername())) {
+            redisUri.setUsername(redisProtocol.getUsername());
+        }
+        if (StringUtils.hasText(redisProtocol.getPassword())) {
+            redisUri.setPassword(redisProtocol.getPassword().toCharArray());
+        }
+        Duration timeout = Duration.ofMillis(CollectUtil.getTimeout(redisProtocol.getTimeout()));
+        redisUri.setTimeout(timeout);
+        return RedisClient.create(redisUri);
     }
 
     /**
@@ -100,10 +150,10 @@ public class RedisSingleCollectImpl extends AbstractCollect {
      */
     private Map<String, String> parseInfo(String info) {
         String[] lines = info.split("\n");
-        Map<String, String> result = new HashMap<>();
+        Map<String, String> result = new HashMap<>(16);
         Arrays.stream(lines)
                 .filter(it -> StringUtils.hasText(it) && !it.startsWith("#") && it.contains(":"))
-                .map(this::removeCR)
+                .map(this::removeCr)
                 .map(r -> r.split(":"))
                 .forEach(it -> {
                     if (it.length > 1) {
@@ -113,7 +163,7 @@ public class RedisSingleCollectImpl extends AbstractCollect {
         return result;
     }
 
-    private String removeCR(String value) {
+    private String removeCr(String value) {
         return value.replace("\r", "");
     }
 
