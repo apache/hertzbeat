9249a41cd3e125ae9b7d47c37f17088c2cd06a8a
tomsun28 <tomsun28@outlook.com>
2022-05-29 15:50:03 +0800

    [web-app]bugfix:fix input blocking when input error in dark theme (#157)

--- web-app/src/app/routes/setting/setting.module.ts
+++ web-app/src/app/routes/setting/setting.module.ts
@@ -1,21 +1,32 @@
 import { NgModule, Type } from '@angular/core';
 import { SharedModule } from '@shared';
-
-import { SettingRoutingModule } from './setting-routing.module';
-import { SettingTagsComponent } from './tags/tags.component';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzCascaderModule } from 'ng-zorro-antd/cascader';
 import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
-import { ColorPickerModule } from 'ngx-color-picker';
 import { NzTagModule } from 'ng-zorro-antd/tag';
+import { ColorPickerModule } from 'ngx-color-picker';
+
+import { SettingRoutingModule } from './setting-routing.module';
+import { SettingTagsComponent } from './tags/tags.component';
 
 const COMPONENTS: Array<Type<void>> = [SettingTagsComponent];
 
 @NgModule({
-  imports: [SharedModule, SettingRoutingModule, NzDividerModule, NzBreadCrumbModule, NzCascaderModule, NzCollapseModule, NzListModule, NzSwitchModule, ColorPickerModule, NzTagModule],
+  imports: [
+    SharedModule,
+    SettingRoutingModule,
+    NzDividerModule,
+    NzBreadCrumbModule,
+    NzCascaderModule,
+    NzCollapseModule,
+    NzListModule,
+    NzSwitchModule,
+    ColorPickerModule,
+    NzTagModule
+  ],
   declarations: COMPONENTS
 })
 export class SettingModule {}
