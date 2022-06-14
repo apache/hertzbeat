82e9125c14f6b000ce9dbca7eba8f8aaacfd5460
tomsun28 <tomsun28@outlook.com>
2022-06-14 22:30:02 +0800

    [monitor]change default database mysql to h2

--- common/src/main/java/com/usthe/common/entity/manager/TagMonitorBind.java
+++ common/src/main/java/com/usthe/common/entity/manager/TagMonitorBind.java
@@ -6,12 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_ONLY;
@@ -30,6 +29,7 @@ import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_WRITE;
 @AllArgsConstructor
 @NoArgsConstructor
 @ApiModel(description = "Tag Bind Monitor | 标签与监控关联实体")
+@EntityListeners(AuditingEntityListener.class)
 public class TagMonitorBind {
 
     @Id
@@ -46,11 +46,11 @@ public class TagMonitorBind {
     private Long monitorId;
 
     @ApiModelProperty(value = "记录创建时间(毫秒时间戳)", example = "1612198922000", accessMode = READ_ONLY, position = 3)
-    @Column(insertable = false, updatable = false)
+    @CreatedDate
     private LocalDateTime gmtCreate;
 
     @ApiModelProperty(value = "记录最新修改时间(毫秒时间戳)", example = "1612198444000", accessMode = READ_ONLY, position = 4)
-    @Column(insertable = false, updatable = false)
+    @LastModifiedDate
     private LocalDateTime gmtUpdate;
 
 }
