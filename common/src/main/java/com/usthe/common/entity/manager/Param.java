97160f5296557faa6a81f6505c32d09ee9dd785a
tomsun28 <tomsun28@outlook.com>
2022-06-19 18:50:56 +0800

    [monitor]database tables append prefix hzb_ (#193)

--- common/src/main/java/com/usthe/common/entity/manager/Param.java
+++ common/src/main/java/com/usthe/common/entity/manager/Param.java
@@ -28,7 +28,7 @@ import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_WRITE;
  * @date 2021/11/13 22:19
  */
 @Entity
-@Table(name = "param")
+@Table(name = "hzb_param")
 @Data
 @Builder
 @AllArgsConstructor
