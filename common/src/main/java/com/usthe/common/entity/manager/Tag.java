8495b3c66182b7c32aea9ada09094fc2f75c005a
tomsun28 <tomsun28@outlook.com>
2022-06-19 09:28:46 +0800

    [monitor]change default database mysql to h2 (#191)
    * [monitor]change default database mysql to h2
    
    * Add @wyt199905 as a contributor
    
    * Add @weifuqing as a contributor
    
    * Add @zklmcookle as a contributor
    
    * Update @ChineseTony as a contributor
    
    * [monitor]update config
    
    * [docs]update config doc
    
    * [home]update docs

--- common/src/main/java/com/usthe/common/entity/manager/Tag.java
+++ common/src/main/java/com/usthe/common/entity/manager/Tag.java
@@ -6,6 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import javax.validation.constraints.Max;
@@ -30,6 +35,7 @@ import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_WRITE;
 @AllArgsConstructor
 @NoArgsConstructor
 @ApiModel(description = "Tag Entity | 标签实体")
+@EntityListeners(AuditingEntityListener.class)
 public class Tag {
 
     @Id
@@ -42,6 +48,7 @@ public class Tag {
     private String name;
 
     @ApiModelProperty(value = "Tag Value | 标签值", example = "23", accessMode = READ_WRITE, position = 2)
+    @Column(name = "`value`")
     private String value;
 
     @ApiModelProperty(value = "Tag Color | 标签颜色", example = "#ffff", accessMode = READ_WRITE, position = 3)
@@ -53,17 +60,19 @@ public class Tag {
     private byte type;
 
     @ApiModelProperty(value = "此条记录创建者", example = "tom", accessMode = READ_ONLY, position = 5)
+    @CreatedBy
     private String creator;
 
     @ApiModelProperty(value = "此条记录最新修改者", example = "tom", accessMode = READ_ONLY, position = 6)
+    @LastModifiedBy
     private String modifier;
 
     @ApiModelProperty(value = "记录创建时间(毫秒时间戳)", example = "1612198922000", accessMode = READ_ONLY, position = 7)
-    @Column(insertable = false, updatable = false)
+    @CreatedDate
     private LocalDateTime gmtCreate;
 
     @ApiModelProperty(value = "记录最新修改时间(毫秒时间戳)", example = "1612198444000", accessMode = READ_ONLY, position = 8)
-    @Column(insertable = false, updatable = false)
+    @LastModifiedDate
     private LocalDateTime gmtUpdate;
 
     @Override
---
+++ manager/src/main/java/com/usthe/manager/config/JpaAuditorConfig.java
@@ -0,0 +1,32 @@
+package com.usthe.manager.config;
+
+import com.usthe.sureness.subject.SubjectSum;
+import com.usthe.sureness.util.SurenessContextHolder;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+/**
+ * generate auditor for jpa auditing
+ * @author tom
+ * @date 2022/6/14 23:47
+ */
+@Configuration
+public class JpaAuditorConfig implements AuditorAware<String> {
+
+    @NotNull
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        SubjectSum subjectSum = SurenessContextHolder.getBindSubject();
+        String username = null;
+        if (subjectSum != null) {
+            Object principal = subjectSum.getPrincipal();
+            if (principal != null) {
+                username = String.valueOf(principal);
+            }
+        }
+        return Optional.ofNullable(username);
+    }
+}
