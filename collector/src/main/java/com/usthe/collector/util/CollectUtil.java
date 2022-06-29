111733a7fe0e3fed41e26e9d351c4794b74d31e6
tomsun28 <tomsun28@outlook.com>
2022-06-29 21:55:30 +0800

    [manager] feature support use pre collect metrics data to replace next metrics config params

--- collector/src/main/java/com/usthe/collector/util/CollectUtil.java
+++ collector/src/main/java/com/usthe/collector/util/CollectUtil.java
@@ -1,5 +1,16 @@
 package com.usthe.collector.util;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.reflect.TypeToken;
+import com.usthe.common.entity.job.Configmap;
+import com.usthe.common.util.GsonUtil;
+
+import java.lang.reflect.Type;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,6 +22,10 @@ import java.util.regex.Pattern;
 public class CollectUtil {
 
     private static final int DEFAULT_TIMEOUT = 60000;
+    private static final String SMILING_PLACEHOLDER = "^_^";
+    private static final String SMILING_PLACEHOLDER_REGEX = "\\^_\\^";
+    private static final String CRYING_PLACEHOLDER = "-_-";
+    private static final String CRYING_PLACEHOLDER_REGEX = "\\-_\\-";
 
     /**
      * 关键字匹配计数
@@ -59,5 +74,151 @@ public class CollectUtil {
         } catch (Exception e) {
             return defaultTimeout;
         }
+    }
+
+    /**
+     * json parameter replacement
+     * json 参数替换
+     *
+     * @param jsonElement json
+     * @param configmap   parameter map
+     * @return json
+     */
+    public static JsonElement replaceCryPlaceholder(JsonElement jsonElement, Map<String, Configmap> configmap) {
+        if (jsonElement.isJsonObject()) {
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            Iterator<Map.Entry<String, JsonElement>> iterator = jsonObject.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, JsonElement> entry = iterator.next();
+                JsonElement element = entry.getValue();
+                // Replace normal VALUE value
+                if (element.isJsonPrimitive()) {
+                    // Check if there are special characters Replace
+                    String value = element.getAsString();
+                    if (value.startsWith(CRYING_PLACEHOLDER) && value.endsWith(CRYING_PLACEHOLDER)) {
+                        value = value.replaceAll(CRYING_PLACEHOLDER_REGEX, "");
+                        Configmap param = configmap.get(value);
+                        if (param != null) {
+                            value = (String) param.getValue();
+                            jsonObject.addProperty(entry.getKey(), value);
+                        } else {
+                            iterator.remove();
+                        }
+                    }
+                } else {
+                    jsonObject.add(entry.getKey(), replaceSmilingPlaceholder(entry.getValue(), configmap));
+                }
+            }
+        } else if (jsonElement.isJsonArray()) {
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            Iterator<JsonElement> iterator = jsonArray.iterator();
+            int index = 0;
+            while (iterator.hasNext()) {
+                JsonElement element = iterator.next();
+                if (element.isJsonPrimitive()) {
+                    // Check if there are special characters Replace
+                    String value = element.getAsString();
+                    if (value.startsWith(CRYING_PLACEHOLDER) && value.endsWith(CRYING_PLACEHOLDER)) {
+                        value = value.replaceAll(CRYING_PLACEHOLDER_REGEX, "");
+                        Configmap param = configmap.get(value);
+                        if (param != null) {
+                            value = (String) param.getValue();
+                            jsonArray.set(index, new JsonPrimitive(value));
+                        } else {
+                            iterator.remove();
+                        }
+                    }
+                } else {
+                    jsonArray.set(index, replaceSmilingPlaceholder(element, configmap));
+                }
+                index++;
+            }
+        }
+        return jsonElement;
+    }
+
+    /**
+     * json parameter replacement
+     * json 参数替换
+     *
+     * @param jsonElement json
+     * @param configmap   parameter map
+     * @return json
+     */
+    public static JsonElement replaceSmilingPlaceholder(JsonElement jsonElement, Map<String, Configmap> configmap) {
+        if (jsonElement.isJsonObject()) {
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            Iterator<Map.Entry<String, JsonElement>> iterator = jsonObject.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, JsonElement> entry = iterator.next();
+                JsonElement element = entry.getValue();
+                String key = entry.getKey();
+                // Replace the attributes of the KEY-VALUE case such as http headers params
+                // 替换KEY-VALUE情况的属性 比如http headers params
+                if (key != null && key.startsWith(SMILING_PLACEHOLDER) && key.endsWith(SMILING_PLACEHOLDER)) {
+                    key = key.replaceAll(SMILING_PLACEHOLDER_REGEX, "");
+                    Configmap param = configmap.get(key);
+                    if (param != null && param.getType() == (byte) 3) {
+                        String jsonValue = (String) param.getValue();
+                        Type type = new TypeToken<Map<String, String>>(){}.getType();
+                        Map<String, String> map = GsonUtil.fromJson(jsonValue, type);
+                        if (map != null) {
+                            map.forEach((name, value) -> {
+                                if (name != null && !"".equals(name.trim())) {
+                                    jsonObject.addProperty(name, value);
+                                }
+                            });
+                        }
+                    }
+                    iterator.remove();
+                    continue;
+                }
+                // Replace normal VALUE value
+                // 替换正常的VALUE值
+                if (element.isJsonPrimitive()) {
+                    // Check if there are special characters Replace
+                    // 判断是否含有特殊字符 替换
+                    String value = element.getAsString();
+                    if (value.startsWith(SMILING_PLACEHOLDER) && value.endsWith(SMILING_PLACEHOLDER)) {
+                        value = value.replaceAll(SMILING_PLACEHOLDER_REGEX, "");
+                        Configmap param = configmap.get(value);
+                        if (param != null) {
+                            value = (String) param.getValue();
+                            jsonObject.addProperty(entry.getKey(), value);
+                        } else {
+                            iterator.remove();
+                        }
+                    }
+                } else {
+                    jsonObject.add(entry.getKey(), replaceSmilingPlaceholder(entry.getValue(), configmap));
+                }
+            }
+        } else if (jsonElement.isJsonArray()) {
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            Iterator<JsonElement> iterator = jsonArray.iterator();
+            int index = 0;
+            while (iterator.hasNext()) {
+                JsonElement element = iterator.next();
+                if (element.isJsonPrimitive()) {
+                    // Check if there are special characters Replace
+                    // 判断是否含有特殊字符 替换
+                    String value = element.getAsString();
+                    if (value.startsWith(SMILING_PLACEHOLDER) && value.endsWith(SMILING_PLACEHOLDER)) {
+                        value = value.replaceAll(SMILING_PLACEHOLDER_REGEX, "");
+                        Configmap param = configmap.get(value);
+                        if (param != null) {
+                            value = (String) param.getValue();
+                            jsonArray.set(index, new JsonPrimitive(value));
+                        } else {
+                            iterator.remove();
+                        }
+                    }
+                } else {
+                    jsonArray.set(index, replaceSmilingPlaceholder(element, configmap));
+                }
+                index++;
+            }
+        }
+        return jsonElement;
     }
 }
