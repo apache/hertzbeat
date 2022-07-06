04cf2c137254e01e6d573d7107920a67bd69586d
tomsun28 <tomsun28@outlook.com>
2022-07-06 08:02:35 +0800

    [common] bugfix SnowFlakeId cannot exceed hexadecimal 0x1FFFFFFFFFFFFFF (#211)

--- common/src/test/java/com/usthe/common/util/SnowFlakeIdGeneratorTest.java
+++ common/src/test/java/com/usthe/common/util/SnowFlakeIdGeneratorTest.java
@@ -13,10 +13,11 @@ class SnowFlakeIdGeneratorTest {
 
     @Test
     void generateId() {
-        // 注意 由于前端JS TS 在json解析大数会造成精度丢失 UUID 不能超过 9007199254740991（16位）
-        for (int i = 0; i < 1000; i++) {
+        // 注意 由于前端JS TS 在json解析大数会造成精度丢失 UUID 不能超过 16进制 0x1FFFFFFFFFFFFF (小于53bit)
+        // Note that because the front-end JS TS parses large numbers in json, the precision will be lost. UUID cannot exceed hexadecimal 0x1FFFFFFFFFFFFFF (less than 53bit)
+        for (int i = 0; i < 10000; i++) {
             long id = SnowFlakeIdGenerator.generateId();
-            Assertions.assertTrue(id < 9007199254740991L);
+            Assertions.assertTrue(id < 0x1FFFFFFFFFFFFFL);
         }
     }
 }
