08a470e1f9a4f69aa7e173fe7dc132eb15b35855
tomsun28 <tomsun28@outlook.com>
2022-08-07 13:57:16 +0800

    [monitor] support jmx protocol, add tomcat monitor type (#239)
      [monitor] feature support jmx protocol (#232)
    
      feat: [manager,collertor,common]feature: Preliminary implementation of JMX protocol #wqh
    
      feat: [manager,collertor,common]feature: Implementation of tomcat jmx protocol on memory and basic information monitoring #wqh
    
      [monitor] review
    
      feat: [collertor]feature: JMX link, reuse code optimization #wqh
    
      feat: [collertor]feature: code optimization #wqh
    
      feat: [manager]feature: tomcat thread configuration details #wqh
    
      feat: [manager]feature: tomcat thread configuration details #wqh
    
    Co-authored-by: tomsun28 <tomsun28@outlook.com>
    
      [collector, manager]update tomcat monitor
    
    Co-authored-by: 会编程的王学长 <71161318+wang1027-wqh@users.noreply.github.com>

--- common/src/main/java/com/usthe/common/entity/job/Metrics.java
+++ common/src/main/java/com/usthe/common/entity/job/Metrics.java
@@ -17,14 +17,7 @@
 
 package com.usthe.common.entity.job;
 
-import com.usthe.common.entity.job.protocol.HttpProtocol;
-import com.usthe.common.entity.job.protocol.IcmpProtocol;
-import com.usthe.common.entity.job.protocol.JdbcProtocol;
-import com.usthe.common.entity.job.protocol.RedisProtocol;
-import com.usthe.common.entity.job.protocol.SnmpProtocol;
-import com.usthe.common.entity.job.protocol.SshProtocol;
-import com.usthe.common.entity.job.protocol.TcpUdpProtocol;
-import com.usthe.common.entity.job.protocol.TelnetProtocol;
+import com.usthe.common.entity.job.protocol.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -118,6 +111,13 @@ public class Metrics {
      * 使用公共的redis协议的监控配置信息
      */
     private RedisProtocol redis;
+
+    /**
+     * Get monitoring configuration information using public JMX protocol
+     * 使用公共JMX协议获取监控配置信息
+     */
+    private JmxProtocol jmx;
+
     /**
      * Monitoring configuration information using the public snmp protocol
      * 使用公共的snmp协议的监控配置信息
