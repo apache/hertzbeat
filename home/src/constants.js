bdfccbeadbd4b1cdd72d874674184ae56299b1f1
DreamGirl524 <81132838+DreamGirl524@users.noreply.github.com>
2022-09-07 09:05:21 +0800

    [home] hertzbeat home website revision (#271)
    * [manager] bugfix alert tags is null when tags map key is normal but value is null. (#270)
    
    * 网站改版 调整了首页菜单和帮助文档中的帮助入门布局
    
    * [web-app] update layout ui - help button, nav menu (#272)
    
    * [web-app] update layout ui
    
    * [web-app] update layout ui - help button, nav menu
    
    * [alert,webapp] support delete all alerts at once. (#273)
    
    * [web-app] support switch button config in table list (#274)
    
    * [alert] bugfix: the alert global preset config do not take effect (#275)
    
    * [web-app] update home background image (#276)
    
    * [monitor] release hertzbeat version v1.1.3 (#277)
    
    * merge dev分支代码 更新轮播图图片 更改为‘实时监控系统’
    
    * 网站改版 调整了首页菜单和帮助文档中的帮助入门布局
    
    * merge dev分支代码 更新轮播图图片 更改为‘实时监控系统’

--- home/src/constants.js
+++ home/src/constants.js
@@ -1,168 +1,207 @@
 import React from 'react'
 
-import Translate, {translate} from '@docusaurus/Translate'
+import Translate, { translate } from '@docusaurus/Translate'
 
-
-export const features = [{
+export const features = [
+  {
     title: translate({
-        message: '开箱即用'
+      message: '开箱即用',
     }),
     description: (
-        <>
-            <Translate values={{
-                TANCLOUD: <strong>TANCLOUD</strong>,
-                SAAS: <strong>SAAS</strong>,
-                console: <a href={'https://www.console.tancloud.cn'}><strong>Login Now</strong></a>,
-                br: <br/>
-            }}>
-                {'中小团队和个人无需再为了监控自己的网站资源，而去部署一套繁琐的监控系统。往往有时候，那套监控系统比自身网站消耗的资源还大。' +
-                '{TANCLOUD} 提供{SAAS}云版本，{console}即可开始您的服务监控。{br}' +
-                '安全是最重要的，我们对账户密钥和监控密钥全链路加密。'
-                }
-            </Translate>
-        </>
+      <>
+        <Translate
+          values={{
+            TANCLOUD: <strong>TANCLOUD</strong>,
+            SAAS: <strong>SAAS</strong>,
+            console: (
+              <a href={'https://www.console.tancloud.cn'}>
+                <strong>Login Now</strong>
+              </a>
+            ),
+            br: <br />,
+          }}
+        >
+          {'中小团队和个人无需再为了监控自己的网站资源，而去部署一套繁琐的监控系统。往往有时候，那套监控系统比自身网站消耗的资源还大。' +
+            '{TANCLOUD} 提供{SAAS}云版本，{console}即可开始您的服务监控。{br}' +
+            '安全是最重要的，我们对账户密钥和监控密钥全链路加密。'}
+        </Translate>
+      </>
     ),
-}, {
+  },
+  {
     title: translate({
-        message: '多支持与自定义'
+      message: '多支持与自定义',
     }),
     description: (
-        <>
-            <Translate values={{
-                custom: <a href={'/docs/advanced/extend-point'}><strong>Custom-Monitoring 自定义监控</strong></a>,
-                br: <br/>
-            }}>
-                {'HertzBeat目前支持对网站，API，PING连通性，端口可用性，SiteMap全站，数据库，操作系统等的监控，快速迭代提供更多的监控类型和性能指标。{br}' +
-                '我们提供了更自由化的阈值告警配置，支持邮箱，短信，webhook，钉钉，企业微信，飞书机器人等告警通知。{br}' +
-                '不同团队的监控需求千变万化，我们提供{custom}，仅需配置YML就能快速接入监控系统。'
-                }
-            </Translate>
-        </>
+      <>
+        <Translate
+          values={{
+            custom: (
+              <a href={'/docs/advanced/extend-point'}>
+                <strong>Custom-Monitoring</strong>
+              </a>
+            ),
+            br: <br />,
+          }}
+        >
+          {'HertzBeat目前支持对网站，API，PING连通性，端口可用性，SiteMap全站，数据库，操作系统等的监控，快速迭代提供更多的监控类型和性能指标。{br}' +
+            '我们提供了更自由化的阈值告警配置，支持邮箱，短信，webhook，钉钉，企业微信，飞书机器人等告警通知。{br}' +
+            '不同团队的监控需求千变万化，我们提供{custom}，仅需配置YML就能快速接入监控系统。'}
+        </Translate>
+      </>
     ),
-},
-    {
-        title: translate({
-            message: '拥抱开源'
-        }),
-        description: (
-            <>
-                <Translate values={{
-                    github: <a href={'https://github.com/dromara/hertzbeat'}><strong>HertzBeat Code Github</strong></a>,
-                    gitee: <a href={'https://gitee.com/dromara/hertzbeat'}><strong>HertzBeat Code Gitee</strong></a>,
-                    br: <br/>
-                }}>
-                    {'HertzBeat监控系统代码开源，非常欢迎任何对此有兴趣的同学参与中来，我们一起进步，丰富的资源文档正在完善中。{br}' +
-                    '中二的我们相信开源改变世界！{br}' +
-                    '{github} {br}' +
-                    '{gitee}'
-                    }
-                </Translate>
-            </>
-        ),
-    }]
+  },
+  {
+    title: translate({
+      message: '拥抱开源',
+    }),
+    description: (
+      <>
+        <Translate
+          values={{
+            github: (
+              <a href={'https://github.com/dromara/hertzbeat'}>
+                <strong>HertzBeat Code Github</strong>
+              </a>
+            ),
+            gitee: (
+              <a href={'https://gitee.com/dromara/hertzbeat'}>
+                <strong>HertzBeat Code Gitee</strong>
+              </a>
+            ),
+            br: <br />,
+          }}
+        >
+          {'HertzBeat监控系统代码开源，非常欢迎任何对此有兴趣的同学参与中来，我们一起进步，丰富的资源文档正在完善中。{br}' +
+            '中二的我们相信开源改变世界！{br}' +
+            '{github} {br}' +
+            '{gitee}'}
+        </Translate>
+      </>
+    ),
+  },
+]
 
 export const dromaraFriends = [
-    {
-        img: 'maxkey_logo.png',
-        alt: 'MaxKey',
-        url: 'https://maxkey.top/'
-    }, {
-        img: 'jpom_logo.png',
-        alt: 'jpom',
-        url: 'https://jpom.io/'
-    },
-    {
-        img: 'fast_request_logo.png',
-        alt: 'fast-request',
-        url: 'https://plugins.sheng90.wang/fast-request/'
-    },
-    {
-        img: 'tlog_logo.png',
-        alt: 'TLog',
-        url: 'https://yomahub.com/tlog/'
-    }, {
-        img: 'hutool_logo.jpg',
-        alt: 'Hutool',
-        url: 'https://hutool.cn/'
-    }, {
-        img: 'satoken_logo.png',
-        alt: 'Sa-Token',
-        url: 'http://sa-token.dev33.cn/'
-    }, {
-        img: 'koalas_rpc_logo.png',
-        alt: 'koalas_rpc',
-        url: 'https://gitee.com/dromara/koalas-rpc'
-    }, {
-        img: 'liteflow_logo.png',
-        alt: 'LiteFlow',
-        url: 'https://yomahub.com/liteflow/'
-    }, {
-        img: 'hmily_logo.png',
-        alt: 'hmily',
-        url: 'https://dromara.org/'
-    }, {
-        img: 'forest_logo.png',
-        alt: 'Forest',
-        url: 'https://forest.dtflyx.com/'
-    }, {
-        img: 'cubic_logo.png',
-        alt: 'cubic',
-        url: 'https://cubic.jiagoujishu.com/'
-    }, {
-        img: 'raincat_logo.png',
-        alt: 'Raincat',
-        url: 'https://dromara.org/'
-    }, {
-        img: 'sureness_logo.png',
-        alt: 'Sureness',
-        url: 'https://su.usthe.com/'
-    }, {
-        img: 'easy_es_logo.png',
-        alt: 'easy-es',
-        url: 'https://www.easy-es.cn/'
-    }, {
-        img: 'dynamic_tp_logo.png',
-        alt: 'dynamic-tp',
-        url: 'https://dynamictp.cn/'
-    }, {
-        img: 'northstar_logo.png',
-        alt: 'northstar',
-        url: 'https://gitee.com/dromara/northstar'
-    }, {
-        img: 'mendmix_logo.png',
-        alt: 'mendmix',
-        url: 'https://www.jeesuite.com/'
-    }, {
-        img: 'gobrs_logo.png',
-        alt: 'Gobrs-Async',
-        url: 'https://async.sizegang.cn/'
-    }, {
-        img: 'easypdf_logo.png',
-        alt: 'easypdf',
-        url: 'https://dromara.gitee.io/x-easypdf/'
-    }, {
-        img: 'dante-cloud_logo.png',
-        alt: 'dante-cloud',
-        url: 'https://www.herodotus.cn/'
-    }, {
-        img: 'image-combiner_logo.png',
-        alt: 'image-combiner',
-        url: 'https://gitee.com/dromara/image-combiner'
-    }, {
-        img: 'dromara.png',
-        alt: 'dromara',
-        url: 'https://dromara.org/'
-    }
+  {
+    img: 'maxkey_logo.png',
+    alt: 'MaxKey',
+    url: 'https://maxkey.top/',
+  },
+  {
+    img: 'jpom_logo.png',
+    alt: 'jpom',
+    url: 'https://jpom.io/',
+  },
+  {
+    img: 'fast_request_logo.png',
+    alt: 'fast-request',
+    url: 'https://plugins.sheng90.wang/fast-request/',
+  },
+  {
+    img: 'tlog_logo.png',
+    alt: 'TLog',
+    url: 'https://yomahub.com/tlog/',
+  },
+  {
+    img: 'hutool_logo.jpg',
+    alt: 'Hutool',
+    url: 'https://hutool.cn/',
+  },
+  {
+    img: 'satoken_logo.png',
+    alt: 'Sa-Token',
+    url: 'http://sa-token.dev33.cn/',
+  },
+  {
+    img: 'koalas_rpc_logo.png',
+    alt: 'koalas_rpc',
+    url: 'https://gitee.com/dromara/koalas-rpc',
+  },
+  {
+    img: 'liteflow_logo.png',
+    alt: 'LiteFlow',
+    url: 'https://yomahub.com/liteflow/',
+  },
+  {
+    img: 'hmily_logo.png',
+    alt: 'hmily',
+    url: 'https://dromara.org/',
+  },
+  {
+    img: 'forest_logo.png',
+    alt: 'Forest',
+    url: 'https://forest.dtflyx.com/',
+  },
+  {
+    img: 'cubic_logo.png',
+    alt: 'cubic',
+    url: 'https://cubic.jiagoujishu.com/',
+  },
+  {
+    img: 'raincat_logo.png',
+    alt: 'Raincat',
+    url: 'https://dromara.org/',
+  },
+  {
+    img: 'sureness_logo.png',
+    alt: 'Sureness',
+    url: 'https://su.usthe.com/',
+  },
+  {
+    img: 'easy_es_logo.png',
+    alt: 'easy-es',
+    url: 'https://www.easy-es.cn/',
+  },
+  {
+    img: 'dynamic_tp_logo.png',
+    alt: 'dynamic-tp',
+    url: 'https://dynamictp.cn/',
+  },
+  {
+    img: 'northstar_logo.png',
+    alt: 'northstar',
+    url: 'https://gitee.com/dromara/northstar',
+  },
+  {
+    img: 'mendmix_logo.png',
+    alt: 'mendmix',
+    url: 'https://www.jeesuite.com/',
+  },
+  {
+    img: 'gobrs_logo.png',
+    alt: 'Gobrs-Async',
+    url: 'https://async.sizegang.cn/',
+  },
+  {
+    img: 'easypdf_logo.png',
+    alt: 'easypdf',
+    url: 'https://dromara.gitee.io/x-easypdf/',
+  },
+  {
+    img: 'dante-cloud_logo.png',
+    alt: 'dante-cloud',
+    url: 'https://www.herodotus.cn/',
+  },
+  {
+    img: 'image-combiner_logo.png',
+    alt: 'image-combiner',
+    url: 'https://gitee.com/dromara/image-combiner',
+  },
+  {
+    img: 'dromara.png',
+    alt: 'dromara',
+    url: 'https://dromara.org/',
+  },
 ]
 
 export const friendsLink = [
-    {
-        img: 'hippo4j_logo.png',
-        alt: 'hippo4j',
-        url: 'https://hippo4j.cn/'
-    },
+  {
+    img: 'hippo4j_logo.png',
+    alt: 'hippo4j',
+    url: 'https://hippo4j.cn/',
+  },
 ]
-
 
 export const SetupExample = `
 <dependency>
