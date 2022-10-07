d138104cda0af42708f0a1e700897785616c2da3
tomsun28 <tomsun28@outlook.com>
2022-10-07 19:20:07 +0800

    [hertzbeat] release hertzbeat version v1.2.0 (#330)

--- script/assembly/server/bin/shutdown.sh
+++ script/assembly/server/bin/shutdown.sh
@@ -25,9 +25,9 @@ APPLICATION_JAR="${project.build.finalName}.jar"
 PID=$(ps -ef | grep java | grep "${APPLICATION_JAR}" | grep -v grep | awk '{ print $2 }')
 if [[ -z "$PID" ]]
 then
-    echo ${APPLICATION} is already stopped
+    echo HertzBeat ${APPLICATION} is already stopped
 else
     echo kill  ${PID}
     kill -9 ${PID}
-    echo ${APPLICATION} stopped successfully
+    echo Shutdown HertzBeat ${APPLICATION} Success!
 fi
