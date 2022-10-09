ffe74184a0d2aa1784fb2af1bccaceabe7dd25ca
tomsun28 <tomsun28@outlook.com>
2022-10-09 23:14:50 +0800

    [web-app] fix eslint warning

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, ElementRef, Inject, OnInit, Renderer2 } from '@angular/core';
 import { NavigationEnd, NavigationError, RouteConfigLoadStart, Router } from '@angular/router';
+import { I18NService } from '@core';
 import { ALAIN_I18N_TOKEN, TitleService, VERSION as VERSION_ALAIN } from '@delon/theme';
 import { environment } from '@env/environment';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { VERSION as VERSION_ZORRO } from 'ng-zorro-antd/version';
-import { I18NService } from '@core';
 
 @Component({
   selector: 'app-root',
