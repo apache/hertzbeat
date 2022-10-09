a167b5bfd1bf78f6fb32eb6cf9c27b414c5d4a29
tomsun28 <tomsun28@outlook.com>
2022-10-09 23:13:27 +0800

    [manager] bugfix server cors policy error (#338)

--- manager/src/main/java/com/usthe/manager/config/SecurityCorsConfiguration.java
+++ manager/src/main/java/com/usthe/manager/config/SecurityCorsConfiguration.java
@@ -33,12 +33,14 @@ import java.util.Collections;
 @Configuration
 public class SecurityCorsConfiguration {
 
+    private static final String ORIGIN_PATTERN = "http://localhost:*";
+
     @Bean
     public FilterRegistrationBean corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList(CorsConfiguration.ALL));
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList(ORIGIN_PATTERN));
         corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
         corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
         source.registerCorsConfiguration("/**", corsConfiguration);
