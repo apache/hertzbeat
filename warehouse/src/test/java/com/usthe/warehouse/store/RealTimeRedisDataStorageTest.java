cf632a2dd4646b7b13a3259ebf3646fd7aaf17cc
zcx <48920254+Ceilzcx@users.noreply.github.com>
2022-10-20 21:04:27 +0800

    [warehouse] support Iotdb metrics data storage (#372)
    * add iotdb storage (saveData part)
    
    * implement getHistoryMetricData
    
    * save multiple instances at the same time
    implement getHistoryIntervalMetricData
    
    * [warehouse] reformat code
    
    * [warehouse] refactor code
    
    * [warehouse] refactor code
    
    * [warehouse] refactor code
    
    * [warehouse] fix insert node path container " '  parsed error
    
    * [warehouse] fix insert node path container " ' *  parsed error
    
    * [warehouse] support iotdb rpc-port
    
    * support iotdb version 0.12
    fix save data and select bug
    
    * replace Session with SessionPool
    
    * rule fix, 修复魔法值问题
    
    Co-authored-by: tomsun28 <tomsun28@outlook.com>
    Co-authored-by: hujiaofen <hujiaofen@2dfire.com>

--- warehouse/src/test/java/com/usthe/warehouse/store/RealTimeRedisDataStorageTest.java
+++ warehouse/src/test/java/com/usthe/warehouse/store/RealTimeRedisDataStorageTest.java
@@ -3,12 +3,10 @@ package com.usthe.warehouse.store;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
- * Test case for {@link RedisDataStorage}
+ * Test case for {@link RealTimeRedisDataStorage}
  */
-class RedisDataStorageTest {
+class RealTimeRedisDataStorageTest {
 
     @BeforeEach
     void setUp() {
