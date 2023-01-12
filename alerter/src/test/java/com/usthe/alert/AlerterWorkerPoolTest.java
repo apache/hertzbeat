c0535b4e151cc96341d3d7c3cb36f326e6fbe481
sunxinbo <898349230@qq.com>
2023-01-12 11:21:37 +0800

    [alerter] add AlerterWorkerPoolTest (#443) (#565)

--- alerter/src/test/java/com/usthe/alert/AlerterWorkerPoolTest.java
+++ alerter/src/test/java/com/usthe/alert/AlerterWorkerPoolTest.java
@@ -1,21 +1,34 @@
 package com.usthe.alert;
 
-import com.usthe.alert.controller.AlertDefineController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test case for {@link AlerterWorkerPool}
  */
 class AlerterWorkerPoolTest {
 
+    private AlerterWorkerPool pool ;
+
     @BeforeEach
     void setUp() {
+        pool = new AlerterWorkerPool();
     }
 
     @Test
     void executeJob() {
+        for (int i = 1; i <= 10; i++) {
+            int c = i;
+            pool.executeJob(() -> {
+                System.out.println("currentTIme ==> " + System.currentTimeMillis() + " threadName " + " ==> " + Thread.currentThread() + " current = " + c);
+            });
+        }
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("currentTIme ==> " + System.currentTimeMillis() + " threadName " + " ==> " + Thread.currentThread() + " done... ");
     }
 }
