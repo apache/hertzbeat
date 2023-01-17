73ef87185803fc4c2e48c176cda9db715cbaac77
进击的阿晨 <gcwm99@gmail.com>
2023-01-17 10:48:31 +0800

    [alerter, manager] use thymeleaf to render notify content (#583)
    * [web-app] fix history echarts chart tooltip only show one instance (#365)
    
    * [workflow] upgrade issues-translator version 2.7 (#367)
    
    * [manger] add mockito unit test
    
    * [manger] add globalExceptionHandler mockito unit test (#368)
    
    * [manager] impl appController unit test case (#370)
    
    * 英文注释的补充
    
    * Supplement of English notes
    
    * Supplementary English notes
    
    * Supplement of English notes
    
    * AppControllerTest Implementation of unit tests
    
    Co-authored-by: gaoxingcun <gxc01514416@alibaba-inc.com>
    
    * [warehouse] support Iotdb metrics data storage (#372)
    
    * add iotdb storage (saveData part)
    
    * implement getHistoryMetricData
    
    * save multiple instances at the same time
    implement getHistoryIntervalMetricData
    
    * [warehouse] reformat code
    
    * [warehouse] refactor code
    
    * [warehouse] refactor code
    
    * [warehouse] refactor code
    
    * [warehouse] fix insert node path container " '  parsed error
    
    * [warehouse] fix insert node path container " ' *  parsed error
    
    * [warehouse] support iotdb rpc-port
    
    * support iotdb version 0.12
    fix save data and select bug
    
    * replace Session with SessionPool
    
    * rule fix, 修复魔法值问题
    
    Co-authored-by: tomsun28 <tomsun28@outlook.com>
    Co-authored-by: hujiaofen <hujiaofen@2dfire.com>
    
    * [webapp,warehouse] support historical interval aggregate data chart (#373)
    
    * [manger] add mockito unit test
    
    * [manger] impl unit test ParamDaoTest #360
    
    * [manager] fix NoticeRuleDaoTest
    
    * [manger] impl unit test ParamDefineDaoTest #361
    
    * [manger] impl unit test TagDaoTest #362
    
    * [manger] add manger sql dao mockito unit test (#375)
    
    * [manger] add mockito unit test
    
    * [manger] add mockito unit test
    
    * [manger] impl unit test ParamDaoTest #360
    
    * [manager] fix NoticeRuleDaoTest
    
    * [manger] impl unit test ParamDefineDaoTest #361
    
    * [manger] impl unit test TagDaoTest #362
    
    * [manager] I18nControllerTest and MonitorControllerTest Unit tests are written (#376)
    
    * I18ControllerTest Completion of the test class
    
    * MonitorControllerTest Unit tests are written
    
    * [manger] impl unit test AppServiceTest #355
    
    * [hertzbeat] update use PromQL to collect metrics from promethues server (#456)
    
    * [manager] enhancement manager-monitors-page added gmtUpdate desc (#455)
    
    * [manager-monitors]（增强）应用服务检测-网站检测-分页：添加默认name升序 （enhancement）manager-monitors-page added name asc
    
    * [manager-monitors]（增强）应用服务检测-网站检测-分页：添加默认gmtUpdate降序 （enhancement）manager-monitors-page added gmtUpdate desc
    
    * [webapp] support monitors list sort by name, date (#458)
    
    * [manager] bugfix the gmtUpdate time not change when update monitor params (#459)
    
    * [alerter,collector,common,warehouse] refactor auto import component
    
    * [manager] Notification type supports telegram
    
    * [web-app] Notification type supports telegram
    
    * fix PMD Failure LowerCamelCaseVariableNamingRule
    
    * fix PMD check
    
    * fix ESLint Error
    
    * [manager] remove useless judgment
    
    * [manager] use thymeleaf to render notify content
    
    * [alerter, manager] use thymeleaf to render notify content
    
    Co-authored-by: tomsun28 <tomsun28@outlook.com>
    Co-authored-by: 铁甲小宝 <85919258+TJxiaobao@users.noreply.github.com>
    Co-authored-by: gaoxingcun <gxc01514416@alibaba-inc.com>
    Co-authored-by: zcx <48920254+Ceilzcx@users.noreply.github.com>
    Co-authored-by: hujiaofen <hujiaofen@2dfire.com>
    Co-authored-by: luxx <58515565+luxx-lq@users.noreply.github.com>

--- manager/src/test/java/com/usthe/manager/component/alerter/impl/FlyBookAlertNotifyHandlerImplTest.java
+++ manager/src/test/java/com/usthe/manager/component/alerter/impl/FlyBookAlertNotifyHandlerImplTest.java
@@ -1,25 +1,49 @@
 package com.usthe.manager.component.alerter.impl;
 
-import com.usthe.manager.service.TagService;
-import org.junit.jupiter.api.BeforeEach;
+import com.usthe.common.entity.alerter.Alert;
+import com.usthe.common.entity.manager.NoticeReceiver;
+import com.usthe.common.util.CommonConstants;
+import com.usthe.manager.AbstractSpringIntegrationTest;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.StringUtils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Test case for {@link FlyBookAlertNotifyHandlerImpl}
  */
-class FlyBookAlertNotifyHandlerImplTest {
+@Slf4j
+class FlyBookAlertNotifyHandlerImplTest extends AbstractSpringIntegrationTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Resource
+    private FlyBookAlertNotifyHandlerImpl flyBookAlertNotifyHandler;
 
     @Test
     void send() {
+        String flyBookId = System.getenv("FLY_BOOK_ID");
+        if (!StringUtils.hasText(flyBookId)) {
+            log.warn("Please provide environment variables FLY_BOOK_ID");
+            return;
+        }
+        NoticeReceiver receiver = new NoticeReceiver();
+        receiver.setId(1L);
+        receiver.setName("Mock 告警");
+        receiver.setWechatId(flyBookId);
+        Alert alert = new Alert();
+        alert.setId(1L);
+        alert.setTarget("Mock Target");
+        Map<String, String> map = new HashMap<>();
+        map.put(CommonConstants.TAG_MONITOR_ID, "Mock monitor id");
+        map.put(CommonConstants.TAG_MONITOR_NAME, "Mock monitor name");
+        alert.setTags(map);
+        alert.setContent("mock content");
+        alert.setPriority((byte) 0);
+        alert.setLastTriggerTime(System.currentTimeMillis());
+
+        flyBookAlertNotifyHandler.send(receiver, alert);
     }
 
-    @Test
-    void type() {
-    }
 }
