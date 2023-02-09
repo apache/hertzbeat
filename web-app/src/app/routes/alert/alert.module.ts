bbb4cbb41e6d5d2511b5cb163dbc620d3868dab6
tomsun28 <tomsun28@outlook.com>
2023-02-09 19:55:57 +0800

    [webapp] fix style warning

--- web-app/src/app/routes/alert/alert.module.ts
+++ web-app/src/app/routes/alert/alert.module.ts
@@ -8,13 +8,13 @@ import { NzListModule } from 'ng-zorro-antd/list';
 import { NzRadioModule } from 'ng-zorro-antd/radio';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
 import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
 import { NzTransferModule } from 'ng-zorro-antd/transfer';
 
 import { AlertCenterComponent } from './alert-center/alert-center.component';
 import { AlertNoticeComponent } from './alert-notice/alert-notice.component';
 import { AlertRoutingModule } from './alert-routing.module';
 import { AlertSettingComponent } from './alert-setting/alert-setting.component';
-import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
 
 const COMPONENTS: Array<Type<void>> = [AlertCenterComponent, AlertSettingComponent, AlertNoticeComponent];
 
