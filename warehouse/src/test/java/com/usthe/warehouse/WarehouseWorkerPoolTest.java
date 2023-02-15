e0ce1bdc836995d3ebdbaeab14a5274cdd7b2664
TherChenYang <124348939+TherChenYang@users.noreply.github.com>
2023-02-15 10:52:08 +0800

    [test] add junit WarehouseWorkerPoolTest (#642)
    * [test] add junit WarehouseWorkerPoolTest
    
    * [test] refactor junit WarehouseWorkerPoolTest
    
    * [test] solve WarehouseWorkerPoolTest detail bug
    
    * [test] solve WarehouseWorkerPoolTest detail bug
    
    * [test] solve incrementAndGet not execute

--- warehouse/src/test/java/com/usthe/warehouse/WarehouseWorkerPoolTest.java
+++ warehouse/src/test/java/com/usthe/warehouse/WarehouseWorkerPoolTest.java
@@ -1,20 +1,38 @@
 package com.usthe.warehouse;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Test case for {@link WarehouseWorkerPool}
  */
 class WarehouseWorkerPoolTest {
+    private WarehouseWorkerPool pool;
 
     @BeforeEach
     void setUp() {
+        pool = new WarehouseWorkerPool();
     }
 
     @Test
-    void executeJob() {
+    void executeJob() throws InterruptedException {
+        int numberOfThreads = 10;
+        AtomicInteger counter = new AtomicInteger();
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            pool.executeJob(() -> {
+                counter.incrementAndGet();
+                latch.countDown();
+            });
+        }
+        latch.await();
+
+        Assertions.assertEquals(numberOfThreads, counter.get());
     }
+
 }
