048574ded29ec32cb9d39bd8fc44444dbb494445
moon <chenyang_ma@aliyun.com>
2023-02-15 09:13:05 +0800

    [test] add junit WarehouseWorkerPoolTest

--- warehouse/src/test/java/com/usthe/warehouse/WarehouseWorkerPoolTest.java
+++ warehouse/src/test/java/com/usthe/warehouse/WarehouseWorkerPoolTest.java
@@ -3,18 +3,31 @@ package com.usthe.warehouse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
  * Test case for {@link WarehouseWorkerPool}
  */
 class WarehouseWorkerPoolTest {
+    private WarehouseWorkerPool pool;
 
     @BeforeEach
     void setUp() {
+        pool = new WarehouseWorkerPool();
     }
 
     @Test
     void executeJob() {
+        for (int i = 1; i <= 10; i++) {
+            int c = i;
+            pool.executeJob(() -> {
+                System.out.println("currentTIme ==> " + System.currentTimeMillis() + " threadName " + " ==> " + Thread.currentThread() + " current = " + c);
+            });
+        }
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("currentTIme ==> " + System.currentTimeMillis() + " threadName " + " ==> " + Thread.currentThread() + " done... ");
     }
+
 }
