c03c69b1a8b0ba3f9d10ddd2add75c4a030c78b4
tomsun28 <tomsun28@outlook.com>
2023-03-02 00:38:45 +0800

    support config monitoring define yml in web ui (#678)
    * support config monitoring define yml in web ui
    
    * [webapp] fix code warning
    
    * [webapp] add i18n

--- web-app/src/app/routes/setting/setting-routing.module.ts
+++ web-app/src/app/routes/setting/setting-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DefineComponent } from './define/define.component';
 import { SettingTagsComponent } from './tags/tags.component';
 
-const routes: Routes = [{ path: 'tags', component: SettingTagsComponent }];
+const routes: Routes = [
+  { path: 'tags', component: SettingTagsComponent },
+  { path: 'define', component: DefineComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
