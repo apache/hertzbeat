00fcc29610e5152b8d5a06be50c1a0c3f3e9ec74
hudongdong129 <34374227+hudongdong129@users.noreply.github.com>
2023-03-05 10:54:16 +0800

    [collect]Add redis cluster auto-discovery display metrics information (#685)
    * [collect]Add redis cluster auto-discovery display metrics information
    
    * [collect]review application.yml
    
    ---------
    
    Co-authored-by: hudongdong <hudongdong9406@zto.com>

---
+++ common/src/main/java/com/usthe/common/util/SignConstants.java
@@ -0,0 +1,18 @@
+package com.usthe.common.util;
+
+/**
+ * 特殊服务常量
+ * @author: hdd
+ * @create: 2023/03/04
+ */
+public interface SignConstants {
+
+    String DOUBLE_MARK = ":";
+
+    String WELL_NO = "#";
+
+
+    String LINE_FEED = "\n";
+
+    String CARRIAGE_RETURN = "\r";
+}
