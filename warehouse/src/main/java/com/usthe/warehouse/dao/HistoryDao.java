83c213c2ee4584b1e8caf72948a7357b69e197fc
tomsun28 <tomsun28@outlook.com>
2023-03-05 10:53:06 +0800

    [warehouse] support jpa store metrics history data (#684)

---
+++ warehouse/src/main/java/com/usthe/warehouse/dao/HistoryDao.java
@@ -0,0 +1,15 @@
+package com.usthe.warehouse.dao;
+
+
+import com.usthe.common.entity.warehouse.History;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ * history entity dao
+ * @author tom
+ * @date 2023/2/3 15:01
+ */
+public interface HistoryDao extends JpaRepository<History, Long>, JpaSpecificationExecutor<History> {
+
+}
---
+++ warehouse/src/main/java/com/usthe/warehouse/store/HistoryJpaDatabaseDataStorage.java
@@ -0,0 +1,173 @@
+package com.usthe.warehouse.store;
+
+import com.usthe.common.entity.dto.Value;
+import com.usthe.common.entity.message.CollectRep;
+import com.usthe.common.entity.warehouse.History;
+import com.usthe.common.queue.CommonDataQueue;
+import com.usthe.common.util.CommonConstants;
+import com.usthe.warehouse.WarehouseWorkerPool;
+import com.usthe.warehouse.config.WarehouseProperties;
+import com.usthe.warehouse.dao.HistoryDao;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.criteria.Predicate;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.temporal.TemporalAmount;
+import java.util.*;
+
+/**
+ * data storage by mysql/h2 - jpa
+ *
+ * @author tom
+ * @since 2023/02/03
+ */
+@Component
+@ConditionalOnProperty(prefix = "warehouse.store.jpa",
+        name = "enabled", havingValue = "true")
+@Slf4j
+public class HistoryJpaDatabaseDataStorage extends AbstractHistoryDataStorage {
+    private HistoryDao historyDao;
+
+    public HistoryJpaDatabaseDataStorage(WarehouseWorkerPool workerPool,
+                                         WarehouseProperties properties,
+                                         HistoryDao historyDao,
+                                         CommonDataQueue commonDataQueue) {
+        super(workerPool, properties, commonDataQueue);
+
+        this.serverAvailable = true;
+        this.historyDao = historyDao;
+        this.startStorageData("warehouse-jpa-data-storage", isServerAvailable());
+    }
+
+    @Override
+    void saveData(CollectRep.MetricsData metricsData) {
+        if (metricsData.getCode() != CollectRep.Code.SUCCESS) {
+            return;
+        }
+        if (metricsData.getValuesList().isEmpty()) {
+            log.info("[warehouse jpa] flush metrics data {} is null, ignore.", metricsData.getId());
+            return;
+        }
+        String monitorType = metricsData.getApp();
+        String metrics = metricsData.getMetrics();
+        List<CollectRep.Field> fieldsList = metricsData.getFieldsList();
+        try {
+            List<History> historyList = new LinkedList<>();
+            History.HistoryBuilder historyBuilder = History.builder()
+                    .monitorId(metricsData.getId())
+                    .app(monitorType)
+                    .metrics(metrics)
+                    .time(metricsData.getTime());
+            for (CollectRep.ValueRow valueRow : metricsData.getValuesList()) {
+                String instance = valueRow.getInstance();
+                if (!instance.isEmpty()) {
+                    instance = String.format("\"%s\"", instance);
+                    historyBuilder.instance(instance);
+                } else {
+                    historyBuilder.instance(null);
+                }
+                for (int i = 0; i < fieldsList.size(); i++) {
+                    CollectRep.Field field = fieldsList.get(i);
+                    historyBuilder.metric(field.getName());
+                    if (!CommonConstants.NULL_VALUE.equals(valueRow.getColumns(i))) {
+                        if (fieldsList.get(i).getType() == CommonConstants.TYPE_NUMBER) {
+                            historyBuilder.metricType(CommonConstants.TYPE_NUMBER)
+                                            .dou(Double.parseDouble(valueRow.getColumns(i)));
+                        } else if (fieldsList.get(i).getType() == CommonConstants.TYPE_STRING) {
+                            historyBuilder.metricType(CommonConstants.TYPE_STRING)
+                                    .str(valueRow.getColumns(i));
+                        }
+                    } else {
+                        if (fieldsList.get(i).getType() == CommonConstants.TYPE_NUMBER) {
+                            historyBuilder.metricType(CommonConstants.TYPE_NUMBER).dou(null);
+                        } else if (fieldsList.get(i).getType() == CommonConstants.TYPE_STRING) {
+                            historyBuilder.metricType(CommonConstants.TYPE_STRING).str(null);
+                        }
+                    }
+                    historyList.add(historyBuilder.build());
+                }
+            }
+            historyDao.saveAll(historyList);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * 从数据库获取指标历史数据
+     *
+     * @param monitorId 监控ID
+     * @param app 监控类型
+     * @param metrics 指标集合名
+     * @param metric 指标名
+     * @param instance 实例
+     * @param history 历史范围
+     * @return 指标历史数据列表
+     */
+    @Override
+    public Map<String, List<Value>> getHistoryMetricData(Long monitorId, String app, String metrics, String metric, String instance, String history) {
+        Map<String, List<Value>> instanceValuesMap = new HashMap<>(8);
+        Specification<History> specification = (root, query, criteriaBuilder) -> {
+            List<Predicate> andList = new ArrayList<>();
+            Predicate predicateMonitorId = criteriaBuilder.equal(root.get("monitorId"), monitorId);
+            Predicate predicateMonitorType = criteriaBuilder.equal(root.get("app"), app);
+            Predicate predicateMonitorMetrics = criteriaBuilder.equal(root.get("metrics"), metrics);
+            Predicate predicateMonitorMetric = criteriaBuilder.equal(root.get("metric"), metric);
+            andList.add(predicateMonitorId);
+            andList.add(predicateMonitorType);
+            andList.add(predicateMonitorMetrics);
+            andList.add(predicateMonitorMetric);
+            if (instance != null && !"".equals(instance)) {
+                Predicate predicateMonitorInstance = criteriaBuilder.equal(root.get("instance"), instance);
+                andList.add(predicateMonitorInstance);
+            }
+            if (history != null) {
+                try {
+                    TemporalAmount temporalAmount = Duration.parse("PT" + history);
+                    ZonedDateTime dateTime = ZonedDateTime.now().minus(temporalAmount);
+                    long timeBefore = dateTime.toEpochSecond() * 1000;
+                    Predicate timePredicate = criteriaBuilder.ge(root.get("time"), timeBefore);
+                    andList.add(timePredicate);
+                } catch (Exception e) {
+                    log.error(e.getMessage());
+                }
+            }
+            Predicate[] predicates = new Predicate[andList.size()];
+            Predicate predicate = criteriaBuilder.and(andList.toArray(predicates));
+            return query.where(predicate).getRestriction();
+        };
+        Sort sortExp = Sort.by(new Sort.Order(Sort.Direction.DESC, "time"));
+        List<History> historyList = historyDao.findAll(specification, sortExp);
+        for (History dataItem : historyList) {
+            String value = "";
+            if (dataItem.getMetricType() == CommonConstants.TYPE_NUMBER) {
+                if (dataItem.getDou() != null) {
+                    value = BigDecimal.valueOf(dataItem.getDou()).setScale(4, RoundingMode.HALF_UP)
+                            .stripTrailingZeros().toPlainString();
+                }
+            } else {
+                value = dataItem.getStr();
+            }
+            String instanceValue = dataItem.getInstance() == null ? "" : dataItem.getInstance();
+            List<Value> valueList = instanceValuesMap.computeIfAbsent(instanceValue, k -> new LinkedList<>());
+            valueList.add(new Value(value, dataItem.getTime()));
+        }
+        return instanceValuesMap;
+    }
+
+
+    @Override
+    public Map<String, List<Value>> getHistoryIntervalMetricData(Long monitorId, String app, String metrics, String metric, String instance, String history) {
+        return new HashMap<>(8);
+    }
+
+    @Override
+    public void destroy() throws Exception {}
+}
