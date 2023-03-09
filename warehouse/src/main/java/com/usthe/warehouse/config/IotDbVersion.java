52e22dbecac7235032a7b999cd5fe17de7f230fc
zcx <48920254+Ceilzcx@users.noreply.github.com>
2023-03-09 22:18:46 +0800

    support v1.0.+ iotdb (#702)
    * support v1.0.+ iotdb
    
    * add comment

---
+++ warehouse/src/main/java/com/usthe/warehouse/config/IotDbVersion.java
@@ -0,0 +1,18 @@
+package com.usthe.warehouse.config;
+
+/**
+ * IoTDB user version
+ *
+ * @author ceilzcx
+ * @since 9/3/2023
+ */
+public enum IotDbVersion {
+    /**
+     * version 0.13.*
+     */
+    V_0_13,
+    /**
+     * version 1.0.*
+     */
+    V_1_0
+}
