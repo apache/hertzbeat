50050632f5dc4ba3f684d881fd728a6ab413c034
tomsun28 <tomsun28@outlook.com>
2023-03-26 15:12:41 +0800

    [manager] update greptime

--- warehouse/src/main/java/com/usthe/warehouse/store/HistoryGrepTimeDbDataStorage.java
+++ warehouse/src/main/java/com/usthe/warehouse/store/HistoryGrepTimeDbDataStorage.java
@@ -20,23 +20,23 @@ package com.usthe.warehouse.store;
 import com.usthe.common.entity.dto.Value;
 import com.usthe.common.entity.message.CollectRep;
 import com.usthe.common.util.CommonConstants;
-import com.usthe.warehouse.config.IotDbVersion;
 import com.usthe.warehouse.config.WarehouseProperties;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.iotdb.rpc.IoTDBConnectionException;
-import org.apache.iotdb.rpc.StatementExecutionException;
-import org.apache.iotdb.session.pool.SessionDataSetWrapper;
-import org.apache.iotdb.session.pool.SessionPool;
-import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
-import org.apache.iotdb.tsfile.read.common.RowRecord;
-import org.apache.iotdb.tsfile.write.record.Tablet;
-import org.apache.iotdb.tsfile.write.schema.MeasurementSchema;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
+import io.greptime.GreptimeDB;
+import io.greptime.models.ColumnDataType;
+import io.greptime.models.Err;
+import io.greptime.models.Result;
+import io.greptime.models.SemanticType;
+import io.greptime.models.TableName;
+import io.greptime.models.TableSchema;
+import io.greptime.models.WriteOk;
+import io.greptime.models.WriteRows;
+import io.greptime.options.GreptimeOptions;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * IoTDB data storage
@@ -49,17 +49,12 @@ import java.util.*;
         name = "enabled", havingValue = "true")
 @Slf4j
 public class HistoryGrepTimeDbDataStorage extends AbstractHistoryDataStorage {
-    private static final String BACK_QUOTE = "`";
-    private static final String DOUBLE_QUOTATION_MARKS = "\"";
-    /**
-     * set ttl never expire
-     */
-    private static final String NEVER_EXPIRE = "-1";
 
     /**
-     * storage group (存储组)
+     * storage database
      */
-    private static final String STORAGE_GROUP = "root.hertzbeat";
+    private static final String STORAGE_DATABASE = "hertzbeat";
+    private static final String INSTANCE ="instance";
 
     private static final String SET_TTL = "set ttl to %s %s";
 
@@ -74,19 +69,29 @@ public class HistoryGrepTimeDbDataStorage extends AbstractHistoryDataStorage {
     private static final String QUERY_HISTORY_INTERVAL_WITH_INSTANCE_SQL
             = "SELECT FIRST_VALUE(%s), AVG(%s), MIN_VALUE(%s), MAX_VALUE(%s) FROM %s GROUP BY ([now() - %s, now()), 4h) WITHOUT NULL ANY";
 
-    private SessionPool sessionPool;
-
-    private IotDbVersion version;
-
-    private long queryTimeoutInMs;
+    private GreptimeDB greptimeDb;
 
     public HistoryGrepTimeDbDataStorage(WarehouseProperties properties) {
         this.serverAvailable = this.initDbSession(properties.getStore().getGreptime());
     }
 
     private boolean initDbSession(WarehouseProperties.StoreProperties.GreptimeProperties properties) {
-        String endpoint = "127.0.0.1:4001";
-        return false;
+        String endpoint = properties.getEndpoint();
+        GreptimeOptions opts = GreptimeOptions.newBuilder(endpoint)
+                .writeMaxRetries(1)
+                .readMaxRetries(2)
+                .routeTableRefreshPeriodSeconds(-1)
+                .build();
+        greptimeDb = new GreptimeDB();
+        if (!greptimeDb.init(opts)) {
+            log.error("Fail to start GreptimeDB client");
+            return false;
+        }
+        return createDatabase();
+    }
+    private boolean createDatabase() {
+        // todo auto create database hertzbeat
+        return true;
     }
 
     @Override
@@ -95,66 +100,70 @@ public class HistoryGrepTimeDbDataStorage extends AbstractHistoryDataStorage {
             return;
         }
         if (metricsData.getValuesList().isEmpty()) {
-            log.info("[warehouse iotdb] flush metrics data {} is null, ignore.", metricsData.getId());
+            log.info("[warehouse greptime] flush metrics data {} is null, ignore.", metricsData.getId());
             return;
         }
-        // tablet的deviceId添加引号会导致数据插入失败
-        List<MeasurementSchema> schemaList = new ArrayList<>();
 
-        // todo MeasurementSchema是在客户端生成的数据结构，编码和压缩没有作用
-        // todo 需要使用指定的数据结构，还是需要手动创建timeSeries或template
+        String monitorId = String.valueOf(metricsData.getId());
+        String table = metricsData.getApp() + "_" + metricsData.getMetrics();
+        TableSchema.Builder tableSchemaBuilder = TableSchema.newBuilder(TableName.with(STORAGE_DATABASE, table));
+
+        List<SemanticType> semanticTypes = Arrays.asList(SemanticType.Tag, SemanticType.Tag, SemanticType.Timestamp);
+        List<ColumnDataType> dataTypes = Arrays.asList(ColumnDataType.String, ColumnDataType.String, ColumnDataType.Int64);
+        List<String> columnNames = Arrays.asList("monitor_id", "instance", "ts");
+
         List<CollectRep.Field> fieldsList = metricsData.getFieldsList();
         for (CollectRep.Field field : fieldsList) {
-            MeasurementSchema schema = new MeasurementSchema();
-            schema.setMeasurementId(field.getName());
+            semanticTypes.add(SemanticType.Field);
+            columnNames.add(field.getName());
             // handle field type
             if (field.getType() == CommonConstants.TYPE_NUMBER) {
-                schema.setType(TSDataType.DOUBLE);
+                dataTypes.add(ColumnDataType.Float64);
             } else if (field.getType() == CommonConstants.TYPE_STRING) {
-                schema.setType(TSDataType.TEXT);
+                dataTypes.add(ColumnDataType.String);
             }
-            schemaList.add(schema);
         }
-        Map<String, Tablet> tabletMap = new HashMap<>(8);
+        tableSchemaBuilder.semanticTypes(semanticTypes.toArray(new SemanticType[0]));
+        tableSchemaBuilder.dataTypes(dataTypes.toArray(new ColumnDataType[0]));
+        tableSchemaBuilder.columnNames(columnNames.toArray(new String[0]));
+        WriteRows rows = WriteRows.newBuilder(tableSchemaBuilder.build()).build();
         try {
             long now = System.currentTimeMillis();
+            Object[] values = new Object[3 + fieldsList.size()];
+            values[0] = monitorId;
+            values[2] = now;
             for (CollectRep.ValueRow valueRow : metricsData.getValuesList()) {
                 String instance = valueRow.getInstance();
                 if (!instance.isEmpty()) {
                     instance = String.format("\"%s\"", instance);
-                }
-                String deviceId = getDeviceId(metricsData.getApp(), metricsData.getMetrics(), metricsData.getId(), instance, false);
-                if (tabletMap.containsKey(instance)) {
-                    // 避免Time重复
-                    now++;
+                    values[1] = instance;
                 } else {
-                    tabletMap.put(instance, new Tablet(deviceId, schemaList));
+                    values[1] = null;
                 }
-                Tablet tablet = tabletMap.get(instance);
-                int rowIndex = tablet.rowSize++;
-                tablet.addTimestamp(rowIndex, now);
                 for (int i = 0; i < fieldsList.size(); i++) {
                     if (!CommonConstants.NULL_VALUE.equals(valueRow.getColumns(i))) {
                         if (fieldsList.get(i).getType() == CommonConstants.TYPE_NUMBER) {
-                            tablet.addValue(fieldsList.get(i).getName(), rowIndex, Double.parseDouble(valueRow.getColumns(i)));
+                            values[3 + i] = Double.parseDouble(valueRow.getColumns(i));
                         } else if (fieldsList.get(i).getType() == CommonConstants.TYPE_STRING) {
-                            tablet.addValue(fieldsList.get(i).getName(), rowIndex, valueRow.getColumns(i));
+                            values[3 + i] = valueRow.getColumns(i);
                         }
                     } else {
-                        tablet.addValue(fieldsList.get(i).getName(), rowIndex, null);
+                        values[3 + i] = null;
                     }
                 }
+                rows.insert(values);
             }
-            for (Tablet tablet : tabletMap.values()) {
-                this.sessionPool.insertTablet(tablet, true);
-            }
-        } catch (StatementExecutionException | IoTDBConnectionException e) {
+            rows.finish();
+            CompletableFuture<Result<WriteOk, Err>> writeFuture = greptimeDb.write(rows);
+            writeFuture.whenComplete((result, throwable) -> {
+                if (throwable != null) {
+                    log.error("[warehouse greptime]-write data error:{}", result, throwable);
+                } else {
+                    log.error("[warehouse greptime]-write data error:{}", result);
+                }
+            });
+        } catch (Exception e) {
             log.error(e.getMessage(), e);
-        } finally {
-            for (Tablet tablet : tabletMap.values()) {
-                tablet.reset();
-            }
-            tabletMap.clear();
         }
     }
 
@@ -163,201 +172,32 @@ public class HistoryGrepTimeDbDataStorage extends AbstractHistoryDataStorage {
                                                          String instance, String history) {
         Map<String, List<Value>> instanceValuesMap = new HashMap<>(8);
         if (!isServerAvailable()) {
-            log.error("\n\t---------------IotDb Init Failed---------------\n" +
+            log.error("\n\t---------------GrepTime Init Failed---------------\n" +
                     "\t--------------Please Config IotDb--------------\n" +
                     "\t----------Can Not Use Metric History Now----------\n");
             return instanceValuesMap;
         }
-        String deviceId = getDeviceId(app, metrics, monitorId, instance, true);
-        String selectSql = "";
-        try {
-            if (instance != null) {
-                selectSql = String.format(QUERY_HISTORY_SQL, addQuote(metric), deviceId, history);
-                handleHistorySelect(selectSql, "", instanceValuesMap);
-            } else {
-                // 优先查询底下所有存在device, 如果存在底下所有device的数据, 否则查询deviceId的数据
-                List<String> devices = queryAllDevices(deviceId);
-                if (devices.isEmpty()) {
-                    selectSql = String.format(QUERY_HISTORY_SQL, addQuote(metric), deviceId, history);
-                    handleHistorySelect(selectSql, "", instanceValuesMap);
-                } else {
-                    // todo 改造成一个select查询: select device1.metric, device2.metric from xxx
-                    for (String device : devices) {
-                        String prefixDeviceId = getDeviceId(app, metrics, monitorId, null, false);
-                        String instanceId = device.substring(prefixDeviceId.length() + 1);
-                        selectSql = String.format(QUERY_HISTORY_SQL, addQuote(metric), deviceId + "." + addQuote(instanceId), history);
-                        handleHistorySelect(selectSql, instanceId, instanceValuesMap);
-                    }
-                }
-            }
-        } catch (StatementExecutionException | IoTDBConnectionException e) {
-            log.error("select error history sql: {}", selectSql);
-            log.error(e.getMessage(), e);
-        }
         return instanceValuesMap;
     }
 
-    private void handleHistorySelect(String selectSql, String instanceId, Map<String, List<Value>> instanceValuesMap)
-            throws IoTDBConnectionException, StatementExecutionException {
-        SessionDataSetWrapper dataSet = null;
-        try {
-            dataSet = this.sessionPool.executeQueryStatement(selectSql, this.queryTimeoutInMs);
-            log.debug("iot select sql: {}", selectSql);
-            while (dataSet.hasNext()) {
-                RowRecord rowRecord = dataSet.next();
-                long timestamp = rowRecord.getTimestamp();
-                double value = rowRecord.getFields().get(0).getDoubleV();
-                String strValue = BigDecimal.valueOf(value).setScale(4, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString();
-                List<Value> valueList = instanceValuesMap.computeIfAbsent(instanceId, k -> new LinkedList<>());
-                valueList.add(new Value(strValue, timestamp));
-            }
-        } finally {
-            if (dataSet != null) {
-                // 需要关闭结果集！！！否则会造成服务端堆积
-                this.sessionPool.closeResultSet(dataSet);
-            }
-        }
-    }
 
     @Override
     public Map<String, List<Value>> getHistoryIntervalMetricData(Long monitorId, String app, String metrics,
                                                                  String metric, String instance, String history) {
         Map<String, List<Value>> instanceValuesMap = new HashMap<>(8);
         if (!isServerAvailable()) {
-            log.error("\n\t---------------IotDb Init Failed---------------\n" +
+            log.error("\n\t---------------GrepTime Init Failed---------------\n" +
                     "\t--------------Please Config IotDb--------------\n" +
                     "\t----------Can Not Use Metric History Now----------\n");
             return instanceValuesMap;
         }
-        String deviceId = getDeviceId(app, metrics, monitorId, instance, true);
-        String selectSql = "";
-        if (instance != null) {
-            selectSql = String.format(QUERY_HISTORY_INTERVAL_WITH_INSTANCE_SQL,
-                    addQuote(metric), addQuote(metric), addQuote(metric), addQuote(metric), deviceId, history);
-            handleHistoryIntervalSelect(selectSql, "", instanceValuesMap);
-        } else {
-            List<String> devices = queryAllDevices(deviceId);
-            if (devices.isEmpty()) {
-                selectSql = String.format(QUERY_HISTORY_INTERVAL_WITH_INSTANCE_SQL,
-                        addQuote(metric), addQuote(metric), addQuote(metric), addQuote(metric), deviceId, history);
-                handleHistoryIntervalSelect(selectSql, "", instanceValuesMap);
-            } else {
-                for (String device : devices) {
-                    String prefixDeviceId = getDeviceId(app, metrics, monitorId, null, false);
-                    String instanceId = device.substring(prefixDeviceId.length() + 1);
-                    selectSql = String.format(QUERY_HISTORY_INTERVAL_WITH_INSTANCE_SQL,
-                            addQuote(metric), addQuote(metric), addQuote(metric), addQuote(metric), deviceId + "." + addQuote(instanceId), history);
-                    handleHistoryIntervalSelect(selectSql, instanceId, instanceValuesMap);
-                }
-            }
-        }
         return instanceValuesMap;
-    }
-
-    private void handleHistoryIntervalSelect(String selectSql, String instanceId,
-                                             Map<String, List<Value>> instanceValuesMap) {
-        SessionDataSetWrapper dataSet = null;
-        try {
-            dataSet = this.sessionPool.executeQueryStatement(selectSql, this.queryTimeoutInMs);
-            log.debug("iot select sql: {}", selectSql);
-            while (dataSet.hasNext()) {
-                RowRecord rowRecord = dataSet.next();
-                long timestamp = rowRecord.getTimestamp();
-                double origin = rowRecord.getFields().get(0).getDoubleV();
-                String originStr = BigDecimal.valueOf(origin).setScale(4, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString();
-                double avg = rowRecord.getFields().get(1).getDoubleV();
-                String avgStr = BigDecimal.valueOf(avg).setScale(4, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString();
-                double min = rowRecord.getFields().get(2).getDoubleV();
-                String minStr = BigDecimal.valueOf(min).setScale(4, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString();
-                double max = rowRecord.getFields().get(3).getDoubleV();
-                String maxStr = BigDecimal.valueOf(max).setScale(4, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString();
-                Value value = Value.builder()
-                        .origin(originStr).mean(avgStr)
-                        .min(minStr).max(maxStr)
-                        .time(timestamp)
-                        .build();
-                List<Value> valueList = instanceValuesMap.computeIfAbsent(instanceId, k -> new LinkedList<>());
-                valueList.add(value);
-            }
-        } catch (StatementExecutionException | IoTDBConnectionException e) {
-            log.error("select error history interval sql: {}", selectSql);
-            log.error(e.getMessage(), e);
-        } finally {
-            if (dataSet != null) {
-                // 需要关闭结果集！！！否则会造成服务端堆积
-                this.sessionPool.closeResultSet(dataSet);
-            }
-        }
-    }
-
-    /**
-     * 获取deviceId下的所有设备
-     *
-     * @param deviceId 设备/实体
-     */
-    private List<String> queryAllDevices(String deviceId) {
-        String showDevicesSql = String.format(SHOW_DEVICES, deviceId + ".*");
-        SessionDataSetWrapper dataSet = null;
-        List<String> devices = new ArrayList<>();
-        try {
-            dataSet = this.sessionPool.executeQueryStatement(showDevicesSql, this.queryTimeoutInMs);
-            while (dataSet.hasNext()) {
-                RowRecord rowRecord = dataSet.next();
-                devices.add(rowRecord.getFields().get(0).getStringValue());
-            }
-        } catch (StatementExecutionException | IoTDBConnectionException e) {
-            log.error("query show all devices sql error. sql: {}", showDevicesSql);
-            log.error(e.getMessage(), e);
-        } finally {
-            if (dataSet != null) {
-                // 需要关闭结果集！！！否则会造成服务端堆积
-                this.sessionPool.closeResultSet(dataSet);
-            }
-        }
-        return devices;
-    }
-
-    /**
-     * 获取设备id
-     * 有instanceId的使用 ${group}.${app}.${metrics}.${monitor}.${instanceId} 的方式
-     * 否则使用 ${group}.${app}.${metrics}.${monitor} 的方式
-     * 查询时可以通过 ${group}.${app}.${metrics}.${monitor}.* 的方式获取所有instance数据
-     */
-    private String getDeviceId(String app, String metrics, Long monitorId, String instanceId, boolean useQuote) {
-        String deviceId = STORAGE_GROUP + "." +
-                (useQuote ? addQuote(app) : app) + "." +
-                (useQuote ? addQuote(metrics) : metrics) + "." +
-                ((IotDbVersion.V_1_0.equals(version) || useQuote) ? addQuote(monitorId.toString()) : monitorId.toString());
-        if (instanceId != null && !instanceId.isEmpty() && !instanceId.equals(CommonConstants.NULL_VALUE)) {
-            deviceId += "." + addQuote(instanceId);
-        }
-        return deviceId;
-    }
-
-    /**
-     * add quote，防止查询时关键字报错(eg: nodes)
-     */
-    private String addQuote(String text) {
-        if (text == null || text.isEmpty() || (text.startsWith(BACK_QUOTE) && text.endsWith(BACK_QUOTE))) {
-            return text;
-        }
-        if ((text.startsWith(DOUBLE_QUOTATION_MARKS) && text.endsWith(DOUBLE_QUOTATION_MARKS))) {
-            if (IotDbVersion.V_1_0.equals(version)) {
-                text = text.replace("\"", "`");
-            }
-            return text;
-        }
-        text = text.replace("'", "\\'");
-        text = text.replace("\"", "\\\"");
-        text = text.replace("*", "-");
-        text = String.format("`%s`", text);
-        return text;
     }
 
     @Override
     public void destroy() {
-        if (this.sessionPool != null) {
-            this.sessionPool.close();
+        if (this.greptimeDb != null) {
+            this.greptimeDb.shutdownGracefully();
         }
     }
 }
