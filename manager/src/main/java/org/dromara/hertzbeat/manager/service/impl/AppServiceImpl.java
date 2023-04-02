4472e945fda01d6ff9f5d547cfa2843f895450b1
Carpe-Wang <78642589+Carpe-Wang@users.noreply.github.com>
2023-04-02 21:15:00 +0800

    Add verification for String app. And checkbox and key-value verification (#831)
      [chore] checkbox and key-value verification
    
      [refactor] Add verification for String app
    
      [chore] checkbox and key-value verification
    
      [fix] UT fail
    
    ---------
    
    Co-authored-by: Carpe-Wang <wangcarpe@126.com>

--- manager/src/main/java/org/dromara/hertzbeat/manager/service/impl/AppServiceImpl.java
+++ manager/src/main/java/org/dromara/hertzbeat/manager/service/impl/AppServiceImpl.java
@@ -69,7 +69,7 @@ public class AppServiceImpl implements AppService, CommandLineRunner {
 
     @Override
     public List<ParamDefine> getAppParamDefines(String app) {
-        if (app == null) {
+        if (app == null || app.chars().allMatch(Character::isSpaceChar)) {
             return Collections.emptyList();
         }
         Job appDefine = appDefines.get(app.toLowerCase());
@@ -82,7 +82,7 @@ public class AppServiceImpl implements AppService, CommandLineRunner {
 
     @Override
     public Job getAppDefine(String app) throws IllegalArgumentException {
-        if (app == null) {
+        if (app == null || app.chars().allMatch(Character::isSpaceChar)) {
             throw new IllegalArgumentException("The app can not null.");
         }
         Job appDefine = appDefines.get(app.toLowerCase());
