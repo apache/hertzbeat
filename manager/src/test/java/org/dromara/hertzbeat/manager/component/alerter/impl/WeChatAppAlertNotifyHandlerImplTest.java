dc71d17c0d52fe2d65c623556b71542bf3dacdb2
hudongdong <hudongdong9406@zto.com>
2023-04-05 22:52:01 +0800

    [manage]modify CI

--- manager/src/test/java/org/dromara/hertzbeat/manager/component/alerter/impl/WeChatAppAlertNotifyHandlerImplTest.java
+++ manager/src/test/java/org/dromara/hertzbeat/manager/component/alerter/impl/WeChatAppAlertNotifyHandlerImplTest.java
@@ -1,47 +1,22 @@
 package org.dromara.hertzbeat.manager.component.alerter.impl;
 
-import lombok.extern.slf4j.Slf4j;
-import org.dromara.hertzbeat.common.entity.alerter.Alert;
-import org.dromara.hertzbeat.common.entity.manager.NoticeReceiver;
-import org.dromara.hertzbeat.common.util.CommonConstants;
-import org.dromara.hertzbeat.manager.AbstractSpringIntegrationTest;
-import org.junit.jupiter.api.Test;
-
-import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.Map;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @description:
  * @author: hdd
  * @create: 2023/04/05
  */
-@Slf4j
-public class WeChatAppAlertNotifyHandlerImplTest extends AbstractSpringIntegrationTest {
+public class WeChatAppAlertNotifyHandlerImplTest {
 
 
-    @Resource
+    @InjectMocks
     private WeChatAppAlertNotifyHandlerImpl weChatAppAlertNotifyHandler;
 
-    @Test
-    void send() {
-        NoticeReceiver receiver = new NoticeReceiver();
-        receiver.setId(1L);
-        receiver.setName("Mock 告警");
-        receiver.setAgentId(1000003);
-        receiver.setAppSecret("oUydwn92ey0lnuY0bMwuNa57eNK-20dJn5NEOG-u2uE");
-        receiver.setCorpId("ww1a603432904d0dc1");
-        Alert alert = new Alert();
-        alert.setId(1L);
-        alert.setTarget("Mock Target");
-        Map<String, String> map = new HashMap<>();
-        map.put(CommonConstants.TAG_MONITOR_ID, "Mock monitor id");
-        map.put(CommonConstants.TAG_MONITOR_NAME, "Mock monitor name");
-        alert.setTags(map);
-        alert.setContent("mock content");
-        alert.setPriority((byte) 0);
-        alert.setLastTriggerTime(System.currentTimeMillis());
+    @Mock
+    private RestTemplate restTemplate;
 
-        weChatAppAlertNotifyHandler.send(receiver, alert);
-    }
+
 }
