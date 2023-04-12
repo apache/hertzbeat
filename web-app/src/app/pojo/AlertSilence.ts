e076e5bb0f3787b17e218dc3c8541e52c7c7aade
tomsun28 <tomsun28@outlook.com>
2023-04-13 00:22:11 +0800

    support alert silence feature

---
+++ web-app/src/app/pojo/AlertSilence.ts
@@ -0,0 +1,18 @@
+import { TagItem } from './NoticeRule';
+
+export class AlertSilence {
+  id!: number;
+  name!: string;
+  enable: boolean = true;
+  type: number = 0;
+  times!: number;
+  priorities!: number[];
+  tags!: TagItem[];
+  days!: number[];
+  periodStart: Date = new Date();
+  periodEnd: Date = new Date();
+  creator!: string;
+  modifier!: string;
+  gmtCreate!: number;
+  gmtUpdate!: number;
+}
