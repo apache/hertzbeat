e7d0eb51be1d906eefcd90e481bc94f67fecead3
gcdd1993 <gcwm99@gmail.com>
2023-04-22 22:34:29 +0800

    [manager] support huaweiyun smn to send alert notify

--- web-app/src/app/pojo/NoticeReceiver.ts
+++ web-app/src/app/pojo/NoticeReceiver.ts
@@ -1,7 +1,8 @@
 export class NoticeReceiver {
   id!: number;
   name!: string;
-  // 通知信息方式: 0-手机短信 1-邮箱 2-webhook 3-微信公众号 4-企业微信机器人 5-钉钉机器人 6-飞书机器人 7-Telegram机器人 8-SlackWebHook 9-Discord机器人 10-企业微信应用消息
+  // 通知信息方式: 0-手机短信 1-邮箱 2-webhook 3-微信公众号 4-企业微信机器人 5-钉钉机器人 6-飞书机器人
+  // 7-Telegram机器人 8-SlackWebHook 9-Discord机器人 10-企业微信应用消息 11-华为云SMN
   type: number = 1;
   phone!: string;
   email!: string;
@@ -16,6 +17,11 @@ export class NoticeReceiver {
   corpId!: string;
   agentId!: number;
   appSecret!: string;
+  smnAk!: string;
+  smnSk!: string;
+  smnProjectId!: string;
+  smnRegion!: string;
+  smnTopicUrn!: string;
   creator!: string;
   modifier!: string;
   gmtCreate!: number;
