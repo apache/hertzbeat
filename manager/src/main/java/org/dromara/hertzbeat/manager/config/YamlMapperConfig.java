a789133dfd667979470a9fb72c45e35296b36cb8
zqr10159 <zqr10159@126.com>
2023-04-22 16:21:43 +0800

    [manager]import and export by YAML via YamlImExportServiceImpl.java (#896)
      [manager]import and export by YAML via YamlImExportServiceImpl.java
    tips:. The request parameter needs to use type:=YAML.
    todo:The front-end doesn't have an YAML version, and I hope someone can make it.
    
      [manager]fix log level and static method.
    
      [manager]modified variable name.
    
      [manager]delete unnesscessary constuctor.

---
+++ manager/src/main/java/org/dromara/hertzbeat/manager/config/YamlMapperConfig.java
@@ -0,0 +1,29 @@
+package org.dromara.hertzbeat.manager.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+/**
+ *
+ * Created by zqr10159 on 2023/4/20
+ */
+@Configuration
+public class YamlMapperConfig {
+    @Bean
+    public YAMLMapper yamlMapper() {
+        YAMLMapper yamlMapper = new YAMLMapper(new YAMLFactory());
+        yamlMapper.registerModule((new JavaTimeModule()));
+        return yamlMapper;
+    }
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().registerModule(new JavaTimeModule());
+    }
+
+}
