6aa102049e6b1fc8b1a003e7424f28e61183b1be
yujianzhong520 <63705063+yujianzhong520@users.noreply.github.com>
2023-05-15 23:53:00 +0800

    [collector] fix dispatch concurrency bug (#956)

--- common/src/main/java/org/dromara/hertzbeat/common/entity/job/Job.java
+++ common/src/main/java/org/dromara/hertzbeat/common/entity/job/Job.java
@@ -217,7 +217,8 @@ public class Job {
             if (priorMetrics.isEmpty()) {
                 return null;
             }
-            return priorMetrics.peek();
+            Set<Metrics> source = priorMetrics.peek();
+            return new HashSet<>(source);
         } else {
             return Collections.emptySet();
         }
