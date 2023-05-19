23b4dde8b149d9ea998c30965e2c3e400688f965
TableRow <59152619+baiban114@users.noreply.github.com>
2023-05-19 11:03:36 +0800

    [common] feature:add kafka common queue (#966)

---
+++ common/src/main/java/org/dromara/hertzbeat/common/serialize/AlertDeserializer.java
@@ -0,0 +1,34 @@
+package org.dromara.hertzbeat.common.serialize;
+
+import org.dromara.hertzbeat.common.entity.alerter.Alert;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.dromara.hertzbeat.common.util.JsonUtil;
+
+import java.util.Map;
+
+/**
+ * kafka告警记录反序列化类
+ *
+ */
+public class AlertDeserializer implements Deserializer {
+    @Override
+    public void configure(Map configs, boolean isKey) {
+        Deserializer.super.configure(configs, isKey);
+    }
+
+    @Override
+    public Object deserialize(String s, byte[] bytes) {
+        return JsonUtil.fromJson(new String(bytes), Alert.class);
+    }
+
+    @Override
+    public Object deserialize(String topic, Headers headers, byte[] data) {
+        return Deserializer.super.deserialize(topic, headers, data);
+    }
+
+    @Override
+    public void close() {
+        Deserializer.super.close();
+    }
+}
