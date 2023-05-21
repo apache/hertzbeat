ea05a439e437f069202d8bee693a1848bc0d63fc
zqr10159 <zqr10159@126.com>
2023-05-21 10:18:03 +0800

    [feature]Support config email server on web-ui (#954)
    Co-authored-by: tomsun28 <tomsun28@outlook.com>

---
+++ manager/src/main/java/org/dromara/hertzbeat/manager/dao/GeneralConfigDao.java
@@ -0,0 +1,35 @@
+package org.dromara.hertzbeat.manager.dao;
+
+import org.dromara.hertzbeat.common.entity.manager.GeneralConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Component;
+
+/**
+ * 消息通知服务端配置Dao
+ *
+ * <p>该接口继承了JpaRepository和JpaSpecificationExecutor两个接口，提供基本的CRUD操作和规范查询能力。</p>
+ *
+ * @version 1.0
+ * @since 2023/5/9 22:39
+ * @author zqr10159
+ */
+@Component
+public interface GeneralConfigDao extends JpaRepository<GeneralConfig, Long>, JpaSpecificationExecutor<GeneralConfig> {
+
+    /**
+     * 通过类型删除
+     *
+     * @param type 类型
+     * @return 返回受影响的行数
+     */
+    int deleteByType(Byte type);
+
+    /**
+     * 通过类型查询
+     *
+     * @param type 类型
+     * @return 返回查询到的配置信息
+     */
+    GeneralConfig findByType(Byte type);
+}
