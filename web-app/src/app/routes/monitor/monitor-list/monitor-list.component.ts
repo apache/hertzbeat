a623bb5194f53eb3a8f1080297931cb67605d368
tomsun28 <tomsun28@outlook.com>
2023-06-14 22:55:36 +0800

    add monitors center and support tag group

--- web-app/src/app/routes/monitor/monitor-list/monitor-list.component.ts
+++ web-app/src/app/routes/monitor/monitor-list/monitor-list.component.ts
@@ -25,7 +25,6 @@ export class MonitorListComponent implements OnInit {
     private router: Router,
     private modal: NzModalService,
     private notifySvc: NzNotificationService,
-    private msg: NzMessageService,
     private monitorSvc: MonitorService,
     private messageSvc: NzMessageService,
     @Inject(ALAIN_I18N_TOKEN) private i18nSvc: I18NService
