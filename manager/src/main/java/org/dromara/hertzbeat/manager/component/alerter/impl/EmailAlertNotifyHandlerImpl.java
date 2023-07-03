79b8eaeecff2d9662ac69cfdbe9da799c46fe388
tomsun28 <tomsun28@outlook.com>
2023-07-03 17:07:28 +0800

    bugfix some email server config can not send message (#1066)

--- manager/src/main/java/org/dromara/hertzbeat/manager/component/alerter/impl/EmailAlertNotifyHandlerImpl.java
+++ manager/src/main/java/org/dromara/hertzbeat/manager/component/alerter/impl/EmailAlertNotifyHandlerImpl.java
@@ -25,7 +25,6 @@ import org.dromara.hertzbeat.common.entity.manager.GeneralConfig;
 import org.dromara.hertzbeat.common.entity.manager.NoticeReceiver;
 import org.dromara.hertzbeat.common.util.ResourceBundleUtil;
 import org.dromara.hertzbeat.manager.component.alerter.AlertNotifyHandler;
-import org.dromara.hertzbeat.manager.config.MailConfigProperties;
 import org.dromara.hertzbeat.manager.dao.GeneralConfigDao;
 import org.dromara.hertzbeat.manager.pojo.dto.EmailNoticeSender;
 import org.dromara.hertzbeat.manager.service.MailService;
@@ -52,12 +51,22 @@ final class EmailAlertNotifyHandlerImpl implements AlertNotifyHandler {
 
     private final JavaMailSender javaMailSender;
 
-    private final MailConfigProperties mailConfigProperties;
-
     private final MailService mailService;
-
-    @Value("${spring.mail.username}")
-    private String emailFromUser;
+    
+    @Value("${spring.mail.host:smtp.demo.com}")
+    private String host;
+    
+    @Value("${spring.mail.username:demo}")
+    private String username;
+    
+    @Value("${spring.mail.password:demo}")
+    private String password;
+    
+    @Value("${spring.mail.port:465}")
+    private Integer port;
+    
+    @Value("${spring.mail.properties.mail.smtp.ssl.enable:true}")
+    private boolean sslEnable = true;
 
     private final GeneralConfigDao generalConfigDao;
 
@@ -72,6 +81,7 @@ final class EmailAlertNotifyHandlerImpl implements AlertNotifyHandler {
         try {
             //获取sender
             JavaMailSenderImpl sender = (JavaMailSenderImpl) javaMailSender;
+            String fromUsername = username;
             try {
                 boolean useDatabase = false;
                 GeneralConfig emailConfig = generalConfigDao.findByType(TYPE);
@@ -85,18 +95,19 @@ final class EmailAlertNotifyHandlerImpl implements AlertNotifyHandler {
                         sender.setUsername(emailNoticeSenderConfig.getEmailUsername());
                         sender.setPassword(emailNoticeSenderConfig.getEmailPassword());
                         Properties props = sender.getJavaMailProperties();
-                        props.put("spring.mail.smtp.ssl.enable", emailNoticeSenderConfig.isEmailSsl());
-                        emailFromUser = emailNoticeSenderConfig.getEmailUsername();  
+                        props.put("mail.smtp.ssl.enable", emailNoticeSenderConfig.isEmailSsl());
+                        fromUsername = emailNoticeSenderConfig.getEmailUsername();  
                         useDatabase = true;
                     }
                 } 
                 if (!useDatabase) {
                     // 若数据库未配置则启用yml配置
-                    sender.setHost(mailConfigProperties.getHost());
-                    sender.setPort(mailConfigProperties.getPort());
-                    sender.setUsername(mailConfigProperties.getUsername());
-                    sender.setPassword(mailConfigProperties.getPassword());
-                    emailFromUser = mailConfigProperties.getUsername();
+                    sender.setHost(host);
+                    sender.setPort(port);
+                    sender.setUsername(username);
+                    sender.setPassword(password);
+                    Properties props = sender.getJavaMailProperties();
+                    props.put("mail.smtp.ssl.enable", sslEnable);
                 }
             } catch (Exception e) {
                 log.error("Type not found {}",e.getMessage());
@@ -105,7 +116,7 @@ final class EmailAlertNotifyHandlerImpl implements AlertNotifyHandler {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
             messageHelper.setSubject(bundle.getString("alerter.notify.title"));
             //Set sender Email 设置发件人Email
-            messageHelper.setFrom(emailFromUser);
+            messageHelper.setFrom(fromUsername);
             //Set recipient Email 设定收件人Email
             messageHelper.setTo(receiver.getEmail());
             messageHelper.setSentDate(new Date());
