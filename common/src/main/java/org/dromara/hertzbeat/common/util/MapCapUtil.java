a8c5d26286166dbc9f53860660b48bfbc6e0fa65
tomsun28 <tomsun28@outlook.com>
2023-07-03 10:30:23 +0800

    support collector run as an independent process (#1060)

--- common/src/main/java/org/dromara/hertzbeat/common/util/MapCapUtil.java
+++ common/src/main/java/org/dromara/hertzbeat/common/util/MapCapUtil.java
@@ -2,8 +2,7 @@ package org.dromara.hertzbeat.common.util;
 
 /**
  * map initial capacity calculation
- * @author: hdd
- * @create: 2023/03/04
+ * @author hdd
  */
 public class MapCapUtil {
 
@@ -12,8 +11,8 @@ public class MapCapUtil {
 
     /**
      * Prevent expansion
-     * @param size
-     * @return
+     * @param size size
+     * @return capacity
      */
     public static int calInitMap(int size) {
         return (int) Math.ceil (size / LOAD_FACTOR);
