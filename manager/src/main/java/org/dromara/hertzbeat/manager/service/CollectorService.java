16c51e0f9ec1dceaf59f0967aa3f18787e833db6
tomsun28 <tomsun28@outlook.com>
2023-07-16 22:26:54 +0800

    add collector endpoint api

---
+++ manager/src/main/java/org/dromara/hertzbeat/manager/service/CollectorService.java
@@ -0,0 +1,21 @@
+package org.dromara.hertzbeat.manager.service;
+
+import org.dromara.hertzbeat.common.entity.manager.Collector;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
+/**
+ * collector service
+ * @author tom
+ */
+public interface CollectorService {
+    
+    /**
+     * Dynamic conditional query
+     * @param specification Query conditions   
+     * @return Search result   
+     */
+    List<Collector> getCollectors(Specification<Collector> specification);
+    
+}
