ab6bbe40b4da4161c1d6e7cf148bc852473aa5fd
Mike Zhou <mikezhoudev@gmail.com>
2023-07-19 10:59:29 +0800

    feat: aviator fn for str contains, exists & matches (#1106)

--- common/src/main/java/org/dromara/hertzbeat/common/config/AviatorConfiguration.java
+++ common/src/main/java/org/dromara/hertzbeat/common/config/AviatorConfiguration.java
@@ -22,10 +22,12 @@ import com.googlecode.aviator.lexer.token.OperatorType;
 import com.googlecode.aviator.runtime.function.AbstractFunction;
 import com.googlecode.aviator.runtime.type.*;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * @author tomsun28
@@ -43,6 +45,8 @@ public class AviatorConfiguration {
         AviatorEvaluator.getInstance()
                 .useLRUExpressionCache(AVIATOR_LRU_CACHE_SIZE)
                 .addFunction(new StrEqualFunction());
+
+        // 配置自定义aviator函数
         AviatorEvaluator.getInstance().addOpFunction(OperatorType.BIT_OR, new AbstractFunction() {
             @Override
             public AviatorObject call(final Map<String, Object> env, final AviatorObject arg1,
@@ -66,6 +70,10 @@ public class AviatorConfiguration {
                 return OperatorType.BIT_OR.getToken();
             }
         });
+
+        AviatorEvaluator.getInstance().addFunction(new StrContainsFunction());
+        AviatorEvaluator.getInstance().addFunction(new StrExistsFunction());
+        AviatorEvaluator.getInstance().addFunction(new StrMatchesFunction());
     }
 
     /**
@@ -75,12 +83,12 @@ public class AviatorConfiguration {
         @Override
         public AviatorObject call(Map<String, Object> env, AviatorObject arg1, AviatorObject arg2) {
             if (arg1 == null || arg2 == null) {
-                return AviatorBoolean.valueOf(false);
+                return AviatorBoolean.FALSE;
             }
             Object leftTmp = arg1.getValue(env);
             Object rightTmp = arg2.getValue(env);
             if (leftTmp == null || rightTmp == null) {
-                return AviatorBoolean.valueOf(false);
+                return AviatorBoolean.FALSE;
             }
             String left = String.valueOf(leftTmp);
             String right = String.valueOf(rightTmp);
@@ -89,6 +97,78 @@ public class AviatorConfiguration {
         @Override
         public String getName() {
             return "equals";
+        }
+    }
+
+    /**
+     * 自定义aviator判断字符串1是否包含字符串2 (case-insensitive)
+     */
+    private static class StrContainsFunction extends AbstractFunction {
+        @Override
+        public AviatorObject call(Map<String, Object> env, AviatorObject arg1, AviatorObject arg2) {
+            if (arg1 == null || arg2 == null) {
+                return AviatorBoolean.FALSE;
+            }
+            Object leftTmp = arg1.getValue(env);
+            Object rightTmp = arg2.getValue(env);
+            if (leftTmp == null || rightTmp == null) {
+                return AviatorBoolean.FALSE;
+            }
+            String left = String.valueOf(leftTmp);
+            String right = String.valueOf(rightTmp);
+            return AviatorBoolean.valueOf(StringUtils.containsIgnoreCase(left, right));
+        }
+        @Override
+        public String getName() {
+            return "contains";
+        }
+    }
+
+    /**
+     * 自定义aviator判断环境中是否存在字符串
+     */
+    private static class StrExistsFunction extends AbstractFunction {
+        @Override
+        public AviatorObject call(Map<String, Object> env, AviatorObject arg) {
+            if (arg == null) {
+                return AviatorBoolean.FALSE;
+            }
+            Object keyTmp = arg.getValue(env);
+            if (keyTmp == null) {
+                return AviatorBoolean.FALSE;
+            }
+            String key = String.valueOf(keyTmp);
+            return AviatorBoolean.valueOf(env.containsKey(key));
+        }
+        @Override
+        public String getName() {
+            return "exists";
+        }
+    }
+
+    /**
+     * 自定义aviator判断字符串是否匹配regex
+     * - regex需要加上""或者''
+     */
+    private static class StrMatchesFunction extends AbstractFunction {
+        @Override
+        public AviatorObject call(Map<String, Object> env, AviatorObject arg1, AviatorObject arg2) {
+            if (arg1 == null || arg2 == null) {
+                return AviatorBoolean.FALSE;
+            }
+            Object strTmp = arg1.getValue(env);
+            Object regexTmp = arg2.getValue(env);
+            if (strTmp == null || regexTmp == null) {
+                return AviatorBoolean.FALSE;
+            }
+            String str = String.valueOf(strTmp);
+            String regex = String.valueOf(regexTmp);
+            boolean isMatch = Pattern.compile(regex).matcher(str).matches();
+            return AviatorBoolean.valueOf(isMatch);
+        }
+        @Override
+        public String getName() {
+            return "matches";
         }
     }
 }
