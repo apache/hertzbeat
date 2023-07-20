4a1a56ffb21da7ebe2f03e8d0b52a46868442305
zqr10159 <zqr10159@126.com>
2023-07-20 15:57:11 +0800

    [feature]bug fixed

--- alerter/src/main/java/org/dromara/hertzbeat/alert/service/AlertConvertService.java
+++ alerter/src/main/java/org/dromara/hertzbeat/alert/service/AlertConvertService.java
@@ -1,12 +1,16 @@
 package org.dromara.hertzbeat.alert.service;
 
 /**
- * @author zqr10159
- * Alert Convert Interface
+ * Alert Convert Interface.
+ *
+ * @param <T> the type of object to convert the JSON into
  */
 public interface AlertConvertService<T> {
-    /*
-    Alert Convert Method
+    /**
+     * Alert Convert Method.
+     *
+     * @param json the JSON string to convert
+     * @return the converted object of type T
      */
     T convert(String json);
 }
