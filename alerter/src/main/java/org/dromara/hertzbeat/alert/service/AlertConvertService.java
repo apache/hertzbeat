07e3fab07cdd764da4da3b973239162df4617b14
zqr10159 <zqr10159@126.com>
2023-07-20 15:50:01 +0800

    [feature]bug fixed

--- alerter/src/main/java/org/dromara/hertzbeat/alert/service/AlertConvertService.java
+++ alerter/src/main/java/org/dromara/hertzbeat/alert/service/AlertConvertService.java
@@ -2,8 +2,7 @@ package org.dromara.hertzbeat.alert.service;
 
 /**
  * @author zqr10159
- * 告警转换接口
- * @param <T>
+ * Alert Convert Interface
  */
 public interface AlertConvertService<T> {
     T convert(String json);
