19c0e568424fe551483df5c921f65c9b7d53ff9c
姚贤丰 <987846224@qq.com>
2023-08-08 21:35:18 +0800

    [script] dockerfile: detect app version and auto generator docker tag (#1162)
    Co-authored-by: yaoxf <yaoxf@unitay.com.cn>

--- script/docker/server/build.sh
+++ script/docker/server/build.sh
@@ -15,22 +15,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-VERSION="$1"
-
-if [ ! -n "$VERSION" ]; then
-    echo "MUST INPUT THE BUILD VERSION"
-    exit 1
-fi
-
+# 调整脚本目录
 cd `dirname $0`
 # 当前脚本目录
 CURRENT_DIR=`pwd`
-
+# 跳转制品目录
 cd ../../../manager/target
+# 自动捕获hertzbeat版本
+VERSION=`ls *.tar| awk -F"-" '{print $2}' | awk -F".tar" '{print $1}'`
+# 强制使用版本参数
+if [ -n "$1" ]; then
+    VERSION="$1";
+fi
+
 # 编译上下文目录
 CONTEXT_DIR=`pwd`
 
-echo "docker build -t tancloud/hertzbeat:$VERSION -f $CURRENT_DIR/Dockerfile $CONTEXT_DIR"
+echo "docker build -t tancloud/hertzbeat:$VERSION -f $CURRENT_DIR/Dockerfile $CONTEXT_DIR --build-arg VERSION="$VERSION""
 
-docker build -t tancloud/hertzbeat:"$VERSION" -f $CURRENT_DIR/Dockerfile $CONTEXT_DIR
+docker build -t tancloud/hertzbeat:"$VERSION" -f $CURRENT_DIR/Dockerfile $CONTEXT_DIR --build-arg VERSION="$VERSION"
+#docker build -t tancloud/hertzbeat:latest -f $CURRENT_DIR/Dockerfile $CONTEXT_DIR --build-arg VERSION="$VERSION"
