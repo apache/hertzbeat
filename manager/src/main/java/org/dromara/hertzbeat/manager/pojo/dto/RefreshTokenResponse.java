28156939e23954f3c72d4bf891f40ee83c04d3e4
Carpe-Wang <wangcarpe@126.com>
2023-08-21 23:49:27 +0800

    签发令牌的逻辑剥离出来，用RefreshTokenResponse来封装响应数据

---
+++ manager/src/main/java/org/dromara/hertzbeat/manager/pojo/dto/RefreshTokenResponse.java
@@ -0,0 +1,21 @@
+package org.dromara.hertzbeat.manager.pojo.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "刷新令牌响应")
+public class RefreshTokenResponse {
+    @Schema(title = "Access Token")
+    private String token;
+
+    @Schema(title = "Refresh Token")
+    private String refreshToken;
+}
