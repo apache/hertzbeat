ed883042f1d040ca16a99beec6db30927d0674e1
tomsun28 <tomsun28@outlook.com>
2023-09-08 16:26:13 +0800

    init collector cluster ui

--- web-app/src/app/service/collector.service.ts
+++ web-app/src/app/service/collector.service.ts
@@ -1,8 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { Collector } from '../pojo/Collector';
 import { CollectorSummary } from '../pojo/CollectorSummary';
 import { Message } from '../pojo/Message';
 import { Page } from '../pojo/Page';
@@ -17,5 +16,54 @@ export class CollectorService {
 
   public getCollectors(): Observable<Message<Page<CollectorSummary>>> {
     return this.http.get<Message<Page<CollectorSummary>>>(collector_uri);
+  }
+
+  public queryCollectors(search: string | undefined, pageIndex: number, pageSize: number): Observable<Message<Page<CollectorSummary>>> {
+    pageIndex = pageIndex ? pageIndex : 0;
+    pageSize = pageSize ? pageSize : 8;
+    // 注意HttpParams是不可变对象 需要保存set后返回的对象为最新对象
+    let httpParams = new HttpParams();
+    httpParams = httpParams.appendAll({
+      pageIndex: pageIndex,
+      pageSize: pageSize
+    });
+    if (search != undefined && search != '' && search.trim() != '') {
+      httpParams = httpParams.append('name', search.trim());
+    }
+    const options = { params: httpParams };
+    return this.http.get<Message<Page<CollectorSummary>>>(collector_uri, options);
+  }
+
+  public goOnlineCollector(collectors: Set<string>): Observable<Message<any>> {
+    let httpParams = new HttpParams();
+    collectors.forEach(collector => {
+      // 注意HttpParams是不可变对象 需要保存append后返回的对象为最新对象
+      // append方法可以叠加同一key, set方法会把key之前的值覆盖只留一个key-value
+      httpParams = httpParams.append('collectors', collector);
+    });
+    const options = { params: httpParams };
+    return this.http.put<Message<any>>(`${collector_uri}/online/`, null, options);
+  }
+
+  public goOfflineCollector(collectors: Set<string>): Observable<Message<any>> {
+    let httpParams = new HttpParams();
+    collectors.forEach(collector => {
+      // 注意HttpParams是不可变对象 需要保存append后返回的对象为最新对象
+      // append方法可以叠加同一key, set方法会把key之前的值覆盖只留一个key-value
+      httpParams = httpParams.append('collectors', collector);
+    });
+    const options = { params: httpParams };
+    return this.http.put<Message<any>>(`${collector_uri}/offline/`, null, options);
+  }
+
+  public deleteCollector(collectors: Set<string>): Observable<Message<any>> {
+    let httpParams = new HttpParams();
+    collectors.forEach(collector => {
+      // 注意HttpParams是不可变对象 需要保存append后返回的对象为最新对象
+      // append方法可以叠加同一key, set方法会把key之前的值覆盖只留一个key-value
+      httpParams = httpParams.append('collectors', collector);
+    });
+    const options = { params: httpParams };
+    return this.http.delete<Message<any>>(`${collector_uri}`, options);
   }
 }
