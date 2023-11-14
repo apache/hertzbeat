090f17f828522ad84cd556ef46093d4abf308ce6
余与鱼与驴 <91116490+SurryChen@users.noreply.github.com>
2023-11-14 10:14:19 +0800

    [alerter] Enhanced reporting of external general alert API (#1326)

--- alerter/src/main/java/org/dromara/hertzbeat/alert/dto/CloudAlertReportAbstract.java
+++ alerter/src/main/java/org/dromara/hertzbeat/alert/dto/CloudAlertReportAbstract.java
@@ -14,48 +14,48 @@ public abstract class CloudAlertReportAbstract {
      * 云告警转为内部告警时的告警名称
      */
     @JsonIgnore
-    public abstract String getAlertName() throws Exception;
+    public abstract String getAlertName();
 
     /**
      * 云告警转为内部告警时的告警间隔
      */
     @JsonIgnore
-    public abstract Integer getAlertDuration() throws Exception;
+    public abstract Integer getAlertDuration();
 
     /**
      * 云告警转为内部告警时的告警时间
      */
     @JsonIgnore
-    public abstract long getAlertTime() throws Exception;
+    public abstract long getAlertTime();
 
     /**
      * 云告警转为内部告警时的告警严重程度
      */
     @JsonIgnore
-    public abstract Integer getPriority() throws Exception;
+    public abstract Integer getPriority();
 
     /**
      * 云告警转为内部告警时的告警类型
      */
     @JsonIgnore
-    public abstract Integer getReportType() throws Exception;
+    public abstract Integer getReportType();
 
     /**
      * 云告警转为内部告警时的告警标签信息
      */
     @JsonIgnore
-    public abstract Map<String, String> getLabels() throws Exception;
+    public abstract Map<String, String> getLabels();
 
     /**
      * 云告警转为内部告警时的告警标注
      */
     @JsonIgnore
-    public abstract Map<String, String> getAnnotations() throws Exception;
+    public abstract Map<String, String> getAnnotations();
 
     /**
      * 云告警转为内部告警时的告警内容
      */
     @JsonIgnore
-    public abstract String getContent() throws Exception;
+    public abstract String getContent();
 
 }
