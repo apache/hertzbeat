3687ed3ed7d6e790e3184c0ddbe4bf65762a62c1
xiaoguolong <33684988+xiaoguolong@users.noreply.github.com>
2023-11-23 14:48:28 +0800

    Update Alert.java
    delete  violation of column content 
    
    Signed-off-by: xiaoguolong <33684988+xiaoguolong@users.noreply.github.com>

--- common/src/main/java/org/dromara/hertzbeat/common/entity/alerter/Alert.java
+++ common/src/main/java/org/dromara/hertzbeat/common/entity/alerter/Alert.java
@@ -85,8 +85,8 @@ public class Alert {
             description = "告警通知实际内容",
             example = "linux_192.134.32.1: 534543534 cpu usage high",
             accessMode = READ_WRITE)
-    @Length(max = 2048)
-    @Column(length = 2048)
+    //@Length(max = 2048)
+    //@Column(length = 2048)
     private String content;
 
     @Schema(title = "Alarm status: 0-normal alarm (to be processed) 1-threshold triggered but not reached the number of alarms 2-recovered alarm 3-processed",
