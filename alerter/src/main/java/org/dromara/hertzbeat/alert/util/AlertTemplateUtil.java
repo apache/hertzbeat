591fadd65958bc922f1fe2188a7c25b0a4081c19
Carpe-Wang <wangcarpe@126.com>
2023-12-04 23:42:09 +0800

    fix issue#1355, add test case. 并且没有高并发情况把StringBuffer修改为StringBuilder

--- alerter/src/main/java/org/dromara/hertzbeat/alert/util/AlertTemplateUtil.java
+++ alerter/src/main/java/org/dromara/hertzbeat/alert/util/AlertTemplateUtil.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.dromara.hertzbeat.alert.util;
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,8 +9,6 @@ import java.util.regex.Pattern;
 /**
  * Alarm template keyword matching replacement engine tool
  * 告警模版关键字匹配替换引擎工具
- * @author tom
- *
  */
 @Slf4j
 public class AlertTemplateUtil {
@@ -41,20 +22,21 @@ public class AlertTemplateUtil {
 
     public static String render(String template, Map<String, Object> replaceData) {
         if (template == null) {
-            return null;  
+            return null;
         }
         try {
             Matcher matcher = PATTERN.matcher(template);
-            StringBuffer buffer = new StringBuffer();
+            StringBuilder builder = new StringBuilder();
             while (matcher.find()) {
                 Object objectValue = replaceData.getOrDefault(matcher.group(1), "NullValue");
                 String value = objectValue.toString();
-                matcher.appendReplacement(buffer, value);
+                // Use Matcher.quoteReplacement to escape special characters like $ and \
+                matcher.appendReplacement(builder, Matcher.quoteReplacement(value));
             }
-            matcher.appendTail(buffer);
-            return buffer.toString();
+            matcher.appendTail(builder);
+            return builder.toString();
         } catch (Exception e) {
-            log.error(e.getMessage(), e);
+            log.error("An error occurred while rendering the alert template.", e);
             return template;
         }
     }
