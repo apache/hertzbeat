af58e855194e1120bea3360e51e484c192515bdb
东风 <74083801+ZY945@users.noreply.github.com>
2023-12-15 21:06:34 +0800

    support monitoring memcached metrics and add a help doc (#1423)
    Co-authored-by: 东风 <1335799468@qq.com>

--- common/src/main/java/org/dromara/hertzbeat/common/entity/job/Metrics.java
+++ common/src/main/java/org/dromara/hertzbeat/common/entity/job/Metrics.java
@@ -131,6 +131,11 @@ public class Metrics {
      */
     private WebsocketProtocol websocket;
     /**
+     * Monitoring configuration information using the memcached protocol
+     * 使用memcached的监控配置信息
+     */
+    private MemcachedProtocol memcached;
+    /**
      * Use udp implemented by socket for service port detection configuration information
      * 使用socket实现的udp进行服务端口探测配置信息
      */
