34c8e7e78072a6aacc106b94a4866993f33e2298
tomsun28 <tomsun28@outlook.com>
2023-12-20 17:20:22 +0800

    bugfix ^o^ token error, protect metrics api auth (#1434)
    Signed-off-by: tomsun28 <tomsun28@outlook.com>

--- collector/src/main/java/org/dromara/hertzbeat/collector/Collector.java
+++ collector/src/main/java/org/dromara/hertzbeat/collector/Collector.java
@@ -3,6 +3,8 @@ package org.dromara.hertzbeat.collector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 /**
  * collector startup
  * @author tom
@@ -11,5 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Collector {
     public static void main(String[] args) {
         SpringApplication.run(Collector.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.setProperty("jdk.jndi.object.factoriesFilter", "!com.zaxxer.hikari.HikariJNDIFactory");
     }
 }
