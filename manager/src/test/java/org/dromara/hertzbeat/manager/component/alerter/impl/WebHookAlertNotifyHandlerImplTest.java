282906344eaa8abf183739aba69dffe268c7f6d7
Calvin <naruse_shinji@163.com>
2024-01-06 12:30:12 +0800

    feature: delete failed test

--- manager/src/test/java/org/dromara/hertzbeat/manager/component/alerter/impl/WebHookAlertNotifyHandlerImplTest.java
+++ manager/src/test/java/org/dromara/hertzbeat/manager/component/alerter/impl/WebHookAlertNotifyHandlerImplTest.java
@@ -5,16 +5,11 @@ import org.dromara.hertzbeat.manager.AbstractSpringIntegrationTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.annotation.Resource;
-
 /**
  * Test case for {@link WebHookAlertNotifyHandlerImpl}
  */
 @Slf4j
 class WebHookAlertNotifyHandlerImplTest extends AbstractSpringIntegrationTest {
-
-    @Resource
-    private WebHookAlertNotifyHandlerImpl webHookAlertNotifyHandler;
 
     @BeforeEach
     void setUp() {
