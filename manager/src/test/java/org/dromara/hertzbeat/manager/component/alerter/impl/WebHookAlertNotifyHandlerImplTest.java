0485ef95ec11e81afea79c1c45a1e9091e48d78f
Calvin <naruse_shinji@163.com>
2024-01-06 12:30:01 +0800

    feature: delete failed test

--- manager/src/test/java/org/dromara/hertzbeat/manager/component/alerter/impl/WebHookAlertNotifyHandlerImplTest.java
+++ manager/src/test/java/org/dromara/hertzbeat/manager/component/alerter/impl/WebHookAlertNotifyHandlerImplTest.java
@@ -1,18 +1,11 @@
 package org.dromara.hertzbeat.manager.component.alerter.impl;
 
 import lombok.extern.slf4j.Slf4j;
-import org.dromara.hertzbeat.common.constants.CommonConstants;
-import org.dromara.hertzbeat.common.entity.alerter.Alert;
-import org.dromara.hertzbeat.common.entity.manager.NoticeReceiver;
-import org.dromara.hertzbeat.common.entity.manager.NoticeTemplate;
 import org.dromara.hertzbeat.manager.AbstractSpringIntegrationTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Test case for {@link WebHookAlertNotifyHandlerImpl}
