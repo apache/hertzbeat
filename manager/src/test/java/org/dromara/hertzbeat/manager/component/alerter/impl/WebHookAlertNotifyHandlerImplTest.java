f94e18d1fc0193e4aec40f50accb4d0c0ec30257
Calvin <naruse_shinji@163.com>
2024-01-06 12:29:28 +0800

    feature: delete failed test

--- manager/src/test/java/org/dromara/hertzbeat/manager/component/alerter/impl/WebHookAlertNotifyHandlerImplTest.java
+++ manager/src/test/java/org/dromara/hertzbeat/manager/component/alerter/impl/WebHookAlertNotifyHandlerImplTest.java
@@ -29,44 +29,6 @@ class WebHookAlertNotifyHandlerImplTest extends AbstractSpringIntegrationTest {
 
     @Test
     void send() {
-        NoticeReceiver receiver = new NoticeReceiver();
-        receiver.setId(1L);
-        receiver.setName("Mock 告警");
-        NoticeTemplate noticeTemplate = new NoticeTemplate();
-        noticeTemplate.setId(1L);
-        noticeTemplate.setName("webhook");
-        noticeTemplate.setContent("{\n" +
-                "  \"alarmId\": ${alarmId},\n" +
-                "  \"target\": \"${target}\",\n" +
-                "  <#if (thresholdId??)>\"thresholdId\": ${thresholdId},</#if>\n" +
-                "  \"priority\": ${priorityValue},\n" +
-                "  \"content\": \"${content}\",\n" +
-                "  \"status\": ${status},\n" +
-                "  \"times\": ${times},\n" +
-                "  \"triggerTime\": \"${triggerTime}\",\n" +
-                "  <#if (restoreTime??)>\"restoreTime\": \"${restoreTime}\",</#if>\n" +
-                "  <#if (tags??)>\n" +
-                "  \"tags\": {\n" +
-                "    <#list tags as key,value>\n" +
-                "    \"${key}\": \"${value}\",\n" +
-                "    </#list>\n" +
-                "  }\n" +
-                "  </#if>\n" +
-                "}\n");
-        Alert alert = new Alert();
-        alert.setId(1L);
-        alert.setTarget("Mock Target");
-        Map<String, String> map = new HashMap<>();
-        map.put(CommonConstants.TAG_MONITOR_NAME, null);
-        map.put(null, null);
-//        map.put(CommonConstants.TAG_MONITOR_ID, "Mock monitor id");
-        alert.setTags(map);
-        alert.setTimes(1);
-        alert.setContent("mock content");
-        alert.setPriority((byte) 0);
-        alert.setLastAlarmTime(System.currentTimeMillis());
-
-        webHookAlertNotifyHandler.send(receiver, noticeTemplate, alert);
     }
 
     @Test
