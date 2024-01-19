7f2d0a1cdef8cab3ab1fe427a1309b1fd4469391
yqxxgh <42080876+yqxxgh@users.noreply.github.com>
2024-01-19 23:16:24 +0800

    Alert define add export and import function (#1499)
    Signed-off-by: tomsun28 <tomsun28@outlook.com>
    Co-authored-by: a-little-fool <2030509072@qq.com>
    Co-authored-by: 淞筱 <105542329+a-little-fool@users.noreply.github.com>
    Co-authored-by: tomsun28 <tomsun28@outlook.com>

---
+++ alerter/src/main/java/org/dromara/hertzbeat/alert/service/impl/AlertDefineAbstractImExportServiceImpl.java
@@ -0,0 +1,138 @@
+package org.dromara.hertzbeat.alert.service.impl;
+
+
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import cn.afterturn.easypoi.excel.annotation.ExcelTarget;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.dromara.hertzbeat.alert.service.AlertDefineImExportService;
+import org.dromara.hertzbeat.alert.service.AlertDefineService;
+import org.dromara.hertzbeat.common.entity.alerter.AlertDefine;
+import org.dromara.hertzbeat.common.entity.manager.TagItem;
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Configuration Import Export
+ * 配置导入导出
+ *
+ * @author a-little-fool
+ * Created by a-little-fool on 2023/12/25
+ */
+@Slf4j
+public abstract class AlertDefineAbstractImExportServiceImpl implements AlertDefineImExportService {
+    @Resource
+    @Lazy
+    private AlertDefineService alertDefineService;
+
+    @Override
+    public void importConfig(InputStream is) {
+        var formList = parseImport(is)
+                .stream()
+                .map(this::convert)
+                .collect(Collectors.toUnmodifiableList());
+        if (!CollectionUtils.isEmpty(formList)) {
+            formList.forEach(alertDefine -> {
+                alertDefineService.validate(alertDefine, false);
+                alertDefineService.addAlertDefine(alertDefine);
+            });
+        }
+    }
+
+    @Override
+    public void exportConfig(OutputStream os, List<Long> configList) {
+        var monitorList = configList.stream()
+                .map(it -> alertDefineService.getAlertDefine(it))
+                .map(this::convert)
+                .collect(Collectors.toUnmodifiableList());
+        writeOs(monitorList, os);
+    }
+
+
+    /**
+     * Parsing an input stream into a form
+     * 将输入流解析为表单
+     *
+     * @param is 输入流
+     * @return 表单
+     */
+    abstract List<ExportAlertDefineDTO> parseImport(InputStream is);
+
+    /**
+     * Export Configuration to Output Stream
+     * 导出配置到输出流
+     *
+     * @param exportAlertDefineList 配置列表
+     * @param os          输出流
+     */
+    abstract void writeOs(List<ExportAlertDefineDTO> exportAlertDefineList, OutputStream os);
+
+
+    private ExportAlertDefineDTO convert(AlertDefine alertDefine) {
+        var exportAlertDefine = new ExportAlertDefineDTO();
+        var alertDefineDTO = new AlertDefineDTO();
+        BeanUtils.copyProperties(alertDefine, alertDefineDTO);
+        exportAlertDefine.setAlertDefine(alertDefineDTO);
+        return exportAlertDefine;
+    }
+
+    private AlertDefine convert(ExportAlertDefineDTO exportAlertDefineDTO) {
+        var alertDefine = new AlertDefine();
+        var alertDefineDTO = exportAlertDefineDTO.getAlertDefine();
+        BeanUtils.copyProperties(alertDefineDTO, alertDefine);
+        return alertDefine;
+    }
+
+    protected String
+    fileNamePrefix() {
+        return "hertzbeat_alertDefine_" + LocalDate.now();
+    }
+
+    @Data
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @ExcelTarget(value = "ExportAlertDefineDTO")
+    protected static class ExportAlertDefineDTO {
+        @Excel(name = "AlertDefine")
+        private AlertDefineDTO alertDefine;
+    }
+
+    @Data
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @ExcelTarget(value = "AlertDefineDTO")
+    protected static class AlertDefineDTO {
+        @Excel(name = "App")
+        private String app;
+        @Excel(name = "Metric")
+        private String metric;
+        @Excel(name = "Field")
+        private String field;
+        @Excel(name = "Preset")
+        private Boolean preset;
+        @Excel(name = "Expr")
+        private String expr;
+        @Excel(name = "Priority")
+        private Byte priority;
+        @Excel(name = "Times")
+        private Integer times;
+        @Excel(name = "Tags")
+        private List<TagItem> tags;
+        @Excel(name = "Enable")
+        private Boolean enable;
+        @Excel(name = "RecoverNotice")
+        private Boolean recoverNotice;
+        @Excel(name = "Template")
+        private String template;
+    }
+}
