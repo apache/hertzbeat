293bfba02ec19a954a318b68709c90c30d724754
yqxxgh <42080876+yqxxgh@users.noreply.github.com>
2024-01-21 13:04:55 +0800

    Alert define export tags combine (#1506)
    Co-authored-by: 淞筱 <105542329+a-little-fool@users.noreply.github.com>

--- common/src/test/java/org/dromara/hertzbeat/common/util/JsonUtilTest.java
+++ common/src/test/java/org/dromara/hertzbeat/common/util/JsonUtilTest.java
@@ -1,7 +1,12 @@
 package org.dromara.hertzbeat.common.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.dromara.hertzbeat.common.entity.manager.TagItem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Test case for {@link JsonUtil}
@@ -14,6 +19,11 @@ class JsonUtilTest {
 
     @Test
     void toJson() {
+        List<TagItem> tagList = new ArrayList<>(4);
+        TagItem proTag = new TagItem("test", "pro");
+        tagList.add(proTag);
+        tagList.add(new TagItem("test", "dev"));
+        System.out.println(JsonUtil.toJson(tagList));
     }
 
     @Test
@@ -22,6 +32,10 @@ class JsonUtilTest {
 
     @Test
     void testFromJson() {
+        String jsonStr = "[{\"name\":\"test\",\"value\":\"pro\"},{\"name\":\"test\",\"value\":\"dev\"}]";
+        List<TagItem> tagItems = JsonUtil.fromJson(jsonStr, new TypeReference<List<TagItem>>() {
+        });
+        System.out.println(tagItems);
     }
 
     @Test
