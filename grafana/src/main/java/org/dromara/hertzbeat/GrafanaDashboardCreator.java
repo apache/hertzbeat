ba35cc7bb36550116009e2732ae55af9071cd974
zqr10159 <zqr10159@126.com>
2024-01-25 01:13:54 +0800

    grafana init.

---
+++ grafana/src/main/java/org/dromara/hertzbeat/GrafanaDashboardCreator.java
@@ -0,0 +1,22 @@
+package org.dromara.hertzbeat;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.dromara.hertzbeat.common.util.JsonUtil;
+import org.springframework.stereotype.Component;
+
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+@Component
+public class GrafanaDashboardCreator {
+
+
+}
