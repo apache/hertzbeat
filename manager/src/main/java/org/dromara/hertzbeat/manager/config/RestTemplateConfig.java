382e121bf6178785c224253d79444734582922c7
tomsun28 <tomsun28@outlook.com>
2024-02-08 10:49:43 +0800

    use okhttp instead of HttpURLConnection in rest template (#1542)
    Signed-off-by: tomsun28 <tomsun28@outlook.com>

--- manager/src/main/java/org/dromara/hertzbeat/manager/config/RestTemplateConfig.java
+++ manager/src/main/java/org/dromara/hertzbeat/manager/config/RestTemplateConfig.java
@@ -17,13 +17,15 @@
 
 package org.dromara.hertzbeat.manager.config;
 
+import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 /**
  * restTemplate config
@@ -35,18 +37,20 @@ import java.util.Collections;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
         RestTemplate restTemplate = new RestTemplate(factory);
         restTemplate.setInterceptors(Collections.singletonList(new HeaderRequestInterceptor()));
         return restTemplate;
     }
 
     @Bean
-    public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(5000);
-        factory.setReadTimeout(5000);
-        return factory;
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .build();
+        return new OkHttp3ClientHttpRequestFactory(client);
     }
 
 }
