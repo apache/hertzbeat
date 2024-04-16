4ed29566ef136c37896aa4ebf7a9d25ed2d27354
zqr10159 <zqr10159@126.com>
2024-04-16 08:41:13 +0800

    [improve]Add licence.

--- common/src/main/java/org/apache/hertzbeat/common/entity/grafana/ServiceAccount.java
+++ common/src/main/java/org/apache/hertzbeat/common/entity/grafana/ServiceAccount.java
@@ -1,15 +1,3 @@
-package org.apache.hertzbeat.common.entity.grafana;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -26,6 +14,20 @@ import lombok.NoArgsConstructor;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.hertzbeat.common.entity.grafana;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
 
 /**
  * Grafana service account entity
