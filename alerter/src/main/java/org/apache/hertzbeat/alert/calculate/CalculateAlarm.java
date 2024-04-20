46b4d30b73152ac5f89462c6a0712fbbc16c0267
栗磊 <15613060203@163.com>
2024-04-20 18:58:14 +0800

    [Improve]When multiple lines are returned, each alarm is triggered instead of only the first alarm

--- alerter/src/main/java/org/apache/hertzbeat/alert/calculate/CalculateAlarm.java
+++ alerter/src/main/java/org/apache/hertzbeat/alert/calculate/CalculateAlarm.java
@@ -175,7 +175,7 @@ public class CalculateAlarm {
                         } catch (Exception e) {
                             log.error(e.getMessage(), e);
                         }
-                    } catch (Exception ignored) {} 
+                    } catch (Exception ignored) {}
                 }
                 for (CollectRep.ValueRow valueRow : metricsData.getValuesList()) {
 
@@ -220,15 +220,13 @@ public class CalculateAlarm {
                             if (match) {
                                 // If the threshold rule matches, the number of times the threshold has been triggered is determined and an alarm is triggered
                                 afterThresholdRuleMatch(currentTimeMilli, monitorId, app, metrics, tagBuilder.toString(), fieldValueMap, define);
-                                // if the threshold is triggered, ignore other data rows
-                                break;
                             } else {
                                 String alarmKey = String.valueOf(monitorId) + define.getId() + tagBuilder;
                                 triggeredAlertMap.remove(alarmKey);
                                 if (define.isRecoverNotice()) {
                                     handleRecoveredAlert(currentTimeMilli, define, expr, alarmKey);
                                 }
-                            }   
+                            }
                         } catch (Exception e) {
                             log.error(e.getMessage(), e);
                         }
@@ -258,7 +256,7 @@ public class CalculateAlarm {
         }
     }
 
-    private void afterThresholdRuleMatch(long currentTimeMilli, long monitorId, String app, String metrics, String tagStr, 
+    private void afterThresholdRuleMatch(long currentTimeMilli, long monitorId, String app, String metrics, String tagStr,
                                          Map<String, Object> fieldValueMap, AlertDefine define) {
         String alarmKey = String.valueOf(monitorId) + define.getId() + tagStr;
         Alert triggeredAlert = triggeredAlertMap.get(alarmKey);
@@ -377,7 +375,7 @@ public class CalculateAlarm {
                     notRecoveredAlertMap.put(notResolvedAlertKey, alertBuilder.build());
                     alarmCommonReduce.reduceAndSendAlarm(alertBuilder.build());
                 } else {
-                    triggeredAlertMap.put(String.valueOf(monitorId), alertBuilder.build());   
+                    triggeredAlertMap.put(String.valueOf(monitorId), alertBuilder.build());
                 }
             } else {
                 int times = preAlert.getTriggerTimes() + 1;
@@ -418,7 +416,7 @@ public class CalculateAlarm {
                         .build();
                 alarmCommonReduce.reduceAndSendAlarm(resumeAlert);
                 Runnable updateStatusJob = () -> {
-                    // todo update pre all type alarm status 
+                    // todo update pre all type alarm status
                     updateAvailabilityAlertStatus(monitorId, resumeAlert);
                 };
                 workerPool.executeJob(updateStatusJob);
