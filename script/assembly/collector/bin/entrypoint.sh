0d17a47160677550415094b5a7af25a56a261130
Logic <zqr10159@dromara.org>
2024-04-21 10:48:40 +0800

    [improve] add external lib folder to store mysql and oracle driver. (#1783)
    Signed-off-by: tomsun28 <tomsun28@outlook.com>
    Co-authored-by: tomsun28 <tomsun28@outlook.com>

--- script/assembly/collector/bin/entrypoint.sh
+++ script/assembly/collector/bin/entrypoint.sh
@@ -35,7 +35,9 @@ DEPLOY_DIR=`pwd`
 # 外部配置文件绝对目录,如果是目录需要/结尾，也可以直接指定文件
 # 如果指定的是目录,spring则会读取目录中的所有配置文件
 CONF_DIR=$DEPLOY_DIR/config
-
+MAIN_CLASS="org.apache.hertzbeat.collector.Collector"
+EXT_LIB_PATH="$DEPLOY_DIR/ext-lib"
+CLASSPATH="$DEPLOY_DIR/$JAR_NAME:$EXT_LIB_PATH/*"
 # 项目日志输出绝对路径
 LOGS_DIR=$DEPLOY_DIR/logs
 # 如果logs文件夹不存在,则创建文件夹
@@ -58,4 +60,4 @@ then
 fi
 CONFIG_FILES=" -Dlogging.path=$LOGS_DIR $LOGGING_CONFIG -Dspring.config.location=$CONF_DIR/ "
 echo -e "Starting the HertzBeat $SERVER_NAME ..."
-java $JAVA_OPTS $JAVA_MEM_OPTS $CONFIG_FILES -jar $DEPLOY_DIR/$JAR_NAME --spring.profiles.active=cluster
+java $JAVA_OPTS $JAVA_MEM_OPTS $CONFIG_FILES -cp $CLASSPATH $MAIN_CLASS --spring.profiles.active=cluster
