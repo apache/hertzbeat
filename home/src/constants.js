d4ccc209516bf79607e80a008708467d7280fd9f
tomsun28 <tomsun28@outlook.com>
2024-05-02 15:25:41 +0800

    remove unused

--- home/src/constants.js
+++ home/src/constants.js
@@ -67,27 +67,7 @@ export const features = [
 
 export const usersLink = [
   {
-    img: 'skyworth_logo.png',
-    alt: 'skyworth',
-  },
-  {
-    img: 'sc_edu_logo.png',
-    alt: 'sc_edu_logo',
-  },
-  {
-    img: 'cmge_logo.png',
-    alt: 'cmge',
-  },
-  {
-    img: 'cnsodata_logo.svg',
-    alt: 'cnsodata',
-  },
-  {
     img: 'tancloud_logo.svg',
     alt: 'tancloud',
-  },
-  {
-    img: 'hibobi_logo.svg',
-    alt: 'hibobi',
   }
 ]
