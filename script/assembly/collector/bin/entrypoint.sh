0553d7d08c3971e12c409144a73052ea0d125548
tomsun28 <tomsun28@outlook.com>
2024-05-02 16:31:49 +0800

    update doc
    Signed-off-by: tomsun28 <tomsun28@outlook.com>

--- script/assembly/collector/bin/entrypoint.sh
+++ script/assembly/collector/bin/entrypoint.sh
@@ -16,31 +16,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# 项目名称
+# project name
 SERVER_NAME="${project.build.finalName}"
 
-# jar名称
+# jar name
 JAR_NAME="${project.build.finalName}.jar"
 
-# 进入bin目录
+# cd bin
 cd `dirname $0`
-# bin目录绝对路径
+# bin dir
 BIN_DIR=`pwd`
-# 返回到上一级项目根目录路径
+# return root
 cd ..
-# 打印项目根目录绝对路径
-# `pwd` 执行系统命令并获得结果
+# root path dir
 DEPLOY_DIR=`pwd`
 
-# 外部配置文件绝对目录,如果是目录需要/结尾，也可以直接指定文件
-# 如果指定的是目录,spring则会读取目录中的所有配置文件
+# config dir
 CONF_DIR=$DEPLOY_DIR/config
 MAIN_CLASS="org.apache.hertzbeat.collector.Collector"
 EXT_LIB_PATH="$DEPLOY_DIR/ext-lib"
 CLASSPATH="$DEPLOY_DIR/$JAR_NAME:$EXT_LIB_PATH/*"
-# 项目日志输出绝对路径
+# log dir
 LOGS_DIR=$DEPLOY_DIR/logs
-# 如果logs文件夹不存在,则创建文件夹
+# create logs dir when not exist
 if [ ! -d $LOGS_DIR ]; then
     mkdir $LOGS_DIR
 fi
@@ -51,7 +49,7 @@ JAVA_OPTS=" -Doracle.jdbc.timezoneAsRegion=false"
 # JVM Configuration
 JAVA_MEM_OPTS=" -server -XX:SurvivorRatio=6 -XX:+UseParallelGC -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$LOGS_DIR"
 
-# 加载外部log文件的配置
+# load logback config
 LOG_IMPL_FILE=logback-spring.xml
 LOGGING_CONFIG=""
 if [ -f "$CONF_DIR/$LOG_IMPL_FILE" ]
