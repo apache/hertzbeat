7645dcf487aef37c89c5d5383519506b7b471559
xuziyang <767637918@qq.com>
2024-05-09 13:15:26 +0800

    [refactoring] give a warning at system startup if no historical data repository is configured. (issue 1933)

---
+++ warehouse/src/main/java/org/apache/hertzbeat/warehouse/listener/WareHouseApplicationReadyListener.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hertzbeat.warehouse.listener;
+
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hertzbeat.warehouse.store.history.AbstractHistoryDataStorage;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+
+
+/**
+ *
+ */
+@Slf4j
+@Component
+public class WareHouseApplicationReadyListener {
+
+    private Optional<AbstractHistoryDataStorage> historyDataStorage;
+
+    public WareHouseApplicationReadyListener(Optional<AbstractHistoryDataStorage> historyDataStorage) {
+        this.historyDataStorage = historyDataStorage;
+    }
+
+    @EventListener(classes = {ApplicationReadyEvent.class})
+    public void listen() {
+        if (historyDataStorage.isEmpty()) {
+            log.warn("The historical data repository is not configured");
+        }
+    }
+}
