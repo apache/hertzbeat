a7697423a4439931803097d69eb2becceb148e8b
yuluo-yx <yuluo08290126@gmail.com>
2024-05-14 11:21:15 +0800

    fix
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- collector/src/main/java/org/apache/hertzbeat/collector/collect/http/HttpCollectImpl.java
+++ collector/src/main/java/org/apache/hertzbeat/collector/collect/http/HttpCollectImpl.java
@@ -37,7 +37,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.net.ssl.SSLException;
