341e1adee70a2ec141ac41550794b69e688d0173
linDong <56677297@qq.com>
2024-06-22 16:13:28 +0800

    [feature] : fix AIConstants

--- common/src/main/java/org/apache/hertzbeat/common/constants/AIConstants.java
+++ common/src/main/java/org/apache/hertzbeat/common/constants/AIConstants.java
@@ -26,6 +26,7 @@ public interface AIConstants {
          * The model outputs the maximum tokens, with a maximum output of 8192 and a default value of 1024
          */
          Integer maxTokens =1024;
+
         /**
          * The sampling temperature, which controls the randomness of the output, must be positive
          * The value ranges from 0.0 to 1.0, and cannot be equal to 0. The default value is 0.95. The larger the value, the more random and creative the output will be. The smaller the value, the more stable or certain the output will be
