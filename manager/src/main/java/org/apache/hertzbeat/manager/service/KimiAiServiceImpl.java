eeda17fe72f5592fc78dc19f2412c21d8e67e70b
linDong <56677297@qq.com>
2024-07-01 22:08:14 +0800

    [feature] : support Kimi Ai

--- manager/src/main/java/org/apache/hertzbeat/manager/service/KimiAiServiceImpl.java
+++ manager/src/main/java/org/apache/hertzbeat/manager/service/KimiAiServiceImpl.java
@@ -1,10 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hertzbeat.manager.service;
 
 import com.alibaba.fastjson.JSON;
+import java.util.List;
+import java.util.Objects;
+import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hertzbeat.common.constants.AiConstants;
 import org.apache.hertzbeat.common.constants.AiTypeEnum;
-import org.apache.hertzbeat.manager.pojo.dto.*;
+import org.apache.hertzbeat.manager.pojo.dto.AiMessage;
+import org.apache.hertzbeat.manager.pojo.dto.KimiAiRequestParamDTO;
+import org.apache.hertzbeat.manager.pojo.dto.KimiAiResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -14,9 +36,6 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import javax.annotation.PostConstruct;
-import java.util.List;
-import java.util.Objects;
 
 /**
  * Kimi Ai
