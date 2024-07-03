132f33b9b447181eb6236c47ed9913012d881c9a
linDong <56677297@qq.com>
2024-07-03 22:07:57 +0800

    [doc] : add kimiAi doc

--- manager/src/main/java/org/apache/hertzbeat/manager/service/impl/SparkDeskAiServiceImpl.java
+++ manager/src/main/java/org/apache/hertzbeat/manager/service/impl/SparkDeskAiServiceImpl.java
@@ -1,15 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hertzbeat.manager.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import java.util.List;
 import java.util.Objects;
-import lombok.extern.slf4j.Slf4j;
 import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hertzbeat.common.constants.AiConstants;
 import org.apache.hertzbeat.common.constants.AiTypeEnum;
 import org.apache.hertzbeat.manager.pojo.dto.AiMessage;
 import org.apache.hertzbeat.manager.pojo.dto.SparkDeskRequestParamDTO;
-import org.apache.hertzbeat.manager.pojo.dto.ZhiPuAiResponse;
+import org.apache.hertzbeat.manager.pojo.dto.SparkDeskResponse;
 import org.apache.hertzbeat.manager.service.AiService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -87,9 +104,9 @@ public class SparkDeskAiServiceImpl implements AiService {
 
     private String convertToResponse(String aiRes) {
         try {
-            ZhiPuAiResponse zhiPuAiResponse = JSON.parseObject(aiRes, ZhiPuAiResponse.class);
-            if (Objects.nonNull(zhiPuAiResponse)) {
-                ZhiPuAiResponse.Choice choice = zhiPuAiResponse.getChoices().get(0);
+            SparkDeskResponse sparkDeskResponse = JSON.parseObject(aiRes, SparkDeskResponse.class);
+            if (Objects.nonNull(sparkDeskResponse)) {
+                SparkDeskResponse.Choice choice = sparkDeskResponse.getChoices().get(0);
                 return choice.getDelta().getContent();
             }
         } catch (Exception e) {
