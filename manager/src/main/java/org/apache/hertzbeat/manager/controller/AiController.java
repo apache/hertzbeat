bf27eb4ea76739f669b48b6a99948633fec6520e
linDong <56677297@qq.com>
2024-07-07 22:06:24 +0800

    [improve] : Delete Chinese comments, modify word errors, add Operation comments

--- manager/src/main/java/org/apache/hertzbeat/manager/controller/AiController.java
+++ manager/src/main/java/org/apache/hertzbeat/manager/controller/AiController.java
@@ -49,7 +49,7 @@ public class AiController {
     /**
      * Types of artificial intelligence
      */
-    @Value("${aiConfig.type:0}")
+    @Value("${aiConfig.type:zhiPu}")
     private String type;
 
     /**
