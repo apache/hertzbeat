792075deacb59f4be3b6ad00715a9f8ce3fa4e68
kerwin612 <kerwin612@qq.com>
2024-07-09 23:24:20 +0800

    Fixed lint error

--- web-app/src/app/shared/components/tags-select/tags-select.component.ts
+++ web-app/src/app/shared/components/tags-select/tags-select.component.ts
@@ -19,6 +19,7 @@
 
 import { Component, EventEmitter, Input, Output, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
 import { TagItem } from '../../../pojo/NoticeRule';
 import { Tag } from '../../../pojo/Tag';
 import { TagService } from '../../../service/tag.service';
