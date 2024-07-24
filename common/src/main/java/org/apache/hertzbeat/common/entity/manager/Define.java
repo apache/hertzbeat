7e523dacaca1bdecfa818d2ee12144281672398c
pwallk <1363539513@qq.com>
2024-07-24 15:21:36 +0800

    imporve file server config

---
+++ common/src/main/java/org/apache/hertzbeat/common/entity/manager/Define.java
@@ -0,0 +1,6 @@
+package org.apache.hertzbeat.common.entity.manager;/**  
+  * @author LiKang
+  * @date 2024-07-24 09:52
+  */
+public class Define {
+}
---
+++ manager/src/main/java/org/apache/hertzbeat/manager/dao/DefineDao.java
@@ -0,0 +1,6 @@
+package org.apache.hertzbeat.manager.dao;/**  
+  * @author LiKang
+  * @date 2024-07-24 10:04
+  */
+public interface DefineDao {
+}
