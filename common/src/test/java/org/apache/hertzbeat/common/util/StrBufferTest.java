f2d5b5fec9d97d2cbd5ae63baed3d8d6ed611b6f
yuluo-yx <yuluo08290126@gmail.com>
2024-07-24 10:46:38 +0800

    fix
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- common/src/test/java/org/apache/hertzbeat/common/util/StrBufferTest.java
+++ common/src/test/java/org/apache/hertzbeat/common/util/StrBufferTest.java
@@ -1,9 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hertzbeat.common.util;
 
 import org.junit.jupiter.api.Test;
 
-import static java.lang.Double.NEGATIVE_INFINITY;
-import static java.lang.Double.POSITIVE_INFINITY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -122,8 +137,8 @@ class StrBufferTest {
 	void testParseLong() {
 
 		assertEquals(12345L, StrBuffer.parseLong("12345"));
-		assertEquals(POSITIVE_INF_VALUE, StrBuffer.parseLong("+inf"));
-		assertEquals(NEGATIVE_INF_VALUE, StrBuffer.parseLong("-inf"));
+		assertEquals(POSITIVE_INF_VALUE, StrBuffer.parseLong(POSITIVE_INF));
+		assertEquals(NEGATIVE_INF_VALUE, StrBuffer.parseLong(NEGATIVE_INF));
 	}
 
 	@Test
