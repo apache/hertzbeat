b25064b4b91e79bf594f3f1a6a6c6d3fef6c3755
liuhy <rokkki@163.com>
2024-07-29 15:16:03 +0800

    [type:feature] add sms config, remove backend modifications

--- manager/src/main/java/org/apache/hertzbeat/manager/component/alerter/impl/SmsAlertNotifyHandlerImpl.java
+++ manager/src/main/java/org/apache/hertzbeat/manager/component/alerter/impl/SmsAlertNotifyHandlerImpl.java
@@ -20,19 +20,14 @@ package org.apache.hertzbeat.manager.component.alerter.impl;
 import java.util.ResourceBundle;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.hertzbeat.common.config.CommonProperties;
 import org.apache.hertzbeat.common.constants.CommonConstants;
-import org.apache.hertzbeat.common.constants.SmsTypeEnum;
 import org.apache.hertzbeat.common.entity.alerter.Alert;
-import org.apache.hertzbeat.common.entity.manager.GeneralConfig;
 import org.apache.hertzbeat.common.entity.manager.NoticeReceiver;
 import org.apache.hertzbeat.common.entity.manager.NoticeTemplate;
-import org.apache.hertzbeat.common.util.JsonUtil;
 import org.apache.hertzbeat.common.util.ResourceBundleUtil;
-import org.apache.hertzbeat.manager.dao.GeneralConfigDao;
-import org.apache.hertzbeat.manager.pojo.dto.SmsNoticeSender;
 import org.apache.hertzbeat.manager.service.TencentSmsClient;
 import org.apache.hertzbeat.manager.support.exception.AlertNoticeException;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 /**
@@ -41,58 +36,28 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@ConditionalOnProperty("common.sms.tencent.app-id")
 final class SmsAlertNotifyHandlerImpl extends AbstractAlertNotifyHandlerImpl {
     
-    private static final String TYPE = "sms";
-    
-    private final GeneralConfigDao generalConfigDao;
-    
-    private final CommonProperties commonProperties;
+    private final TencentSmsClient tencentSmsClient;
     
     private final ResourceBundle bundle = ResourceBundleUtil.getBundle("alerter");
-    
-    private TencentSmsClient tencentSmsClient;
     
     @Override
     public void send(NoticeReceiver receiver, NoticeTemplate noticeTemplate, Alert alert) {
         // SMS notification
         try {
-            getSmsSender();
-            doSendSms(receiver, alert);
+            String monitorName = null;
+            if (alert.getTags() != null) {
+                monitorName = alert.getTags().get(CommonConstants.TAG_MONITOR_NAME);
+            }
+            String[] params = new String[3];
+            params[0] = monitorName == null ? alert.getTarget() : monitorName;
+            params[1] = bundle.getString("alerter.priority." + alert.getPriority());
+            params[2] = alert.getContent();
+            tencentSmsClient.sendMessage(params, new String[]{receiver.getPhone()});
         } catch (Exception e) {
             throw new AlertNoticeException("[Sms Notify Error] " + e.getMessage());
-        }
-    }
-    
-    private void doSendSms(final NoticeReceiver receiver, final Alert alert) {
-        String monitorName = null;
-        if (alert.getTags() != null) {
-            monitorName = alert.getTags().get(CommonConstants.TAG_MONITOR_NAME);
-        }
-        String[] params = new String[3];
-        params[0] = monitorName == null ? alert.getTarget() : monitorName;
-        params[1] = bundle.getString("alerter.priority." + alert.getPriority());
-        params[2] = alert.getContent();
-        tencentSmsClient.sendMessage(params, new String[]{receiver.getPhone()});
-    }
-    
-    private void getSmsSender() {
-        boolean useDatabase = false;
-        GeneralConfig smsConfig = generalConfigDao.findByType(TYPE);
-        if (smsConfig != null && smsConfig.getContent() != null) {
-            // enable database configuration
-            String content = smsConfig.getContent();
-            SmsNoticeSender smsNoticeSenderConfig = JsonUtil.fromJson(content, SmsNoticeSender.class);
-            if (null != smsNoticeSenderConfig && smsNoticeSenderConfig.isEnable()) {
-                // tencent sms
-                if (SmsTypeEnum.tencent.name().equalsIgnoreCase(smsNoticeSenderConfig.getType())) {
-                    tencentSmsClient = new TencentSmsClient(smsNoticeSenderConfig.getTencent());
-                    useDatabase = true;
-                }
-            }
-        }
-        if (!useDatabase) {
-            tencentSmsClient = new TencentSmsClient(commonProperties);
         }
     }
     
--- manager/src/main/java/org/apache/hertzbeat/manager/service/TencentSmsClient.java
+++ manager/src/main/java/org/apache/hertzbeat/manager/service/TencentSmsClient.java
@@ -25,11 +25,14 @@ import com.tencentcloudapi.sms.v20210111.models.SendStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hertzbeat.common.config.CommonProperties;
 import org.apache.hertzbeat.common.support.exception.SendMessageException;
-import org.apache.hertzbeat.manager.pojo.dto.SmsTencentConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
 
 /**
  * sms service client for tencent cloud
  */
+@Component
+@ConditionalOnProperty("common.sms.tencent.app-id")
 @Slf4j
 public class TencentSmsClient {
 
@@ -47,14 +50,6 @@ public class TencentSmsClient {
             throw new IllegalArgumentException("please config TencentSmsClient props");
         }
         initSmsClient(properties.getSms().getTencent());
-    }
-    
-    public TencentSmsClient(SmsTencentConfig tencent) {
-        this.appId = tencent.getAppId();
-        this.signName = tencent.getSignName();
-        this.templateId = tencent.getTemplateId();
-        Credential cred = new Credential(tencent.getSecretId(), tencent.getSecretKey());
-        smsClient = new SmsClient(cred, REGION);
     }
 
     private void initSmsClient(CommonProperties.TencentSmsProperties tencent) {
