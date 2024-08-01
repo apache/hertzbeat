4cad8b7e9f6e7f40b3d743a00140c4f088d7f614
yuluo-yx <yuluo08290126@gmail.com>
2024-08-01 10:09:10 +0800

    fix
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- alerter/src/test/java/org/apache/hertzbeat/alert/controller/AlertSilencesControllerTest.java
+++ alerter/src/test/java/org/apache/hertzbeat/alert/controller/AlertSilencesControllerTest.java
@@ -1,8 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hertzbeat.alert.controller;
 
 import java.util.Collections;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hertzbeat.alert.service.AlertSilenceService;
 import org.apache.hertzbeat.common.constants.CommonConstants;
 import org.apache.hertzbeat.common.entity.alerter.AlertSilence;
@@ -12,7 +28,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
