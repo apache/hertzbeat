724c5cfeef320e81382dc538fa3278194d7fdf16
yuluo-yx <yuluo08290126@gmail.com>
2024-08-01 11:44:49 +0800

    fix
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- common/src/main/java/org/apache/hertzbeat/common/serialize/KafkaMetricsDataSerializer.java
+++ common/src/main/java/org/apache/hertzbeat/common/serialize/KafkaMetricsDataSerializer.java
@@ -18,7 +18,6 @@
 package org.apache.hertzbeat.common.serialize;
 
 import java.util.Map;
-
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hertzbeat.common.entity.message.CollectRep;
 import org.apache.kafka.common.header.Headers;
