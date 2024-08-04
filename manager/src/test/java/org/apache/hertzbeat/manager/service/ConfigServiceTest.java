cf85b8905561836738e7e1b5b74ed44f954d8304
yuluo-yx <yuluo08290126@gmail.com>
2024-08-04 17:44:50 +0800

    [Improve] add ExcelImExportServiceImpl unit test
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- manager/src/test/java/org/apache/hertzbeat/manager/service/ConfigServiceTest.java
+++ manager/src/test/java/org/apache/hertzbeat/manager/service/ConfigServiceTest.java
@@ -17,7 +17,6 @@
 
 package org.apache.hertzbeat.manager.service;
 
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.any;
@@ -39,7 +38,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 
 /**
  * Test case for {@link ConfigService}
