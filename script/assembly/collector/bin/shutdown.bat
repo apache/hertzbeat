febc911639a2692791faac5b0f4bef57195bba83
aias00 <rokkki@163.com>
2024-09-19 16:07:39 +0800

    [improve] modify bin shells (#2723)

--- script/assembly/collector/bin/shutdown.bat
+++ script/assembly/collector/bin/shutdown.bat
@@ -19,17 +19,17 @@
 @echo off 
 setlocal enabledelayedexpansion
 
-set SERVER_NAME="${project.artifactId}"
+set SERVER_NAME="hertzbeat-collector"
 
 
 set SERVER_PORT=1159
 
-echo Start shutdown HertzBeat %SERVER_NAME% 
+echo Start shutdown HertzBeat %SERVER_NAME%
 
 for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":%SERVER_PORT%"') do (
     echo kill the process %%m who use the port 
     taskkill /pid %%m -t -f
-    echo Shutdown HertzBeat %SERVER_NAME% Success!  
+    echo Shutdown HertzBeat %SERVER_NAME% Success!
     goto q
 )
 echo Failed shutdown HertzBeat %SERVER_NAME%
