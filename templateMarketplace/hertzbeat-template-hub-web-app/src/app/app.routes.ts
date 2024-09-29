62f4becd38ad42c0de784d42fef2e039fe24510c
All-The-Best-for <wang_bs2022@163.com>
2024-09-30 01:02:18 +0800

    clear

--- templateMarketplace/hertzbeat-template-hub-web-app/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-// import {NgModule} from '@angular/core';
-// import {RouterModule, Routes} from '@angular/router';
-// import {LayoutMarketComponent} from './layout/market/market.component';
-//
-// export const routes: Routes = [
-//   {path: '', component: LayoutMarketComponent},
-// ];
