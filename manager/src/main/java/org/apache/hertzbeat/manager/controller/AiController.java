4054c4f751768a03c2eeb97da567726a5b765e04
linDong <56677297@qq.com>
2024-10-08 16:33:23 +0800

    [improve]:Improve the way Ai is entered and requested

--- manager/src/main/java/org/apache/hertzbeat/manager/controller/AiController.java
+++ manager/src/main/java/org/apache/hertzbeat/manager/controller/AiController.java
@@ -22,15 +22,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.hertzbeat.manager.config.AiProperties;
+import org.apache.hertzbeat.manager.pojo.dto.AiControllerRequestParam;
 import org.apache.hertzbeat.manager.service.ai.AiService;
 import org.apache.hertzbeat.manager.service.ai.factory.AiServiceFactoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 /**
@@ -55,12 +53,13 @@ public class AiController {
      * @param text                  request text
      * @return                      AI response
      */
-    @GetMapping(path = "/get", produces = {TEXT_EVENT_STREAM_VALUE})
+    @PostMapping(path = "/get", produces = {TEXT_EVENT_STREAM_VALUE})
     @Operation(summary = "Artificial intelligence questions and Answers",
             description = "Artificial intelligence questions and Answers")
-    public Flux<ServerSentEvent<String>> requestAi(@Parameter(description = "Request text", example = "Who are you") @RequestParam("text") String text)  {
+    public Flux<ServerSentEvent<String>> requestAi(@Parameter(description = "Request text", example = "Who are you")
+                                                       @RequestBody AiControllerRequestParam requestParam)  {
         Assert.notNull(aiServiceFactory, "please check that your type value is consistent with the documentation on the website");
         AiService aiServiceImplBean = aiServiceFactory.getAiServiceImplBean(aiProperties.getType());
-        return aiServiceImplBean.requestAi(text);
+        return aiServiceImplBean.requestAi(requestParam.getText());
     }
 }
