819ba93913d29dbb4448c2ed4cbcfa9d4feb94a2
Ayu <14359932+lin-Ayu0v0@user.noreply.gitee.com>
2024-11-30 22:14:39 +0800

    [feature] Desensitize sensitive information.

---
+++ hertzbeat-common/src/main/java/org/apache/hertzbeat/common/serialize/EmailDesensitizationSerializer.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hertzbeat.common.serialize;
+
+import cn.hutool.core.util.StrUtil;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+/**
+ * @author Ayu
+ * @date 2024-11-30
+ * @Description Email Desensitizing serializer
+ */
+public class EmailDesensitizationSerializer extends JsonSerializer<String> {
+
+    @Override
+    public void serialize(String email, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        String emailDesensitization = "";
+        if (StrUtil.isNotBlank(email)) {
+            int index = StrUtil.indexOf(email, '@');
+            emailDesensitization = index <= 1 ? email :
+                    StrUtil.replace(email, 1,index,'.');
+        }
+        jsonGenerator.writeString(emailDesensitization);
+    }
+}
