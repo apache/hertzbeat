481e3f48a6d10a84119584c1f128d4a95b5866b3
vinci <1552443053@qq.com>
2024-12-01 19:24:47 +0800

    add an online parser for prometheus, which replaces org.apache.hertzbeat.collector.collect.prometheus.parser.TextParser.textToMetricFamilies

--- hertzbeat-collector/hertzbeat-collector-basic/src/main/java/org/apache/hertzbeat/collector/collect/prometheus/parser/OnlineParser.java
+++ hertzbeat-collector/hertzbeat-collector-basic/src/main/java/org/apache/hertzbeat/collector/collect/prometheus/parser/OnlineParser.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hertzbeat.collector.collect.prometheus.parser;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,19 +29,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 public class OnlineParser {
-
-    // An unknown format occurred during parsing because parsing cannot continue
-    // or the end of the input stream has been reached
-//    private static final int WRONG_FORMAT = -1;
-
-    //The input stream ends normally
-//    private static final int NORMAL_END = -2;
-
-//    private static final int COMMENT_LINE = -3;
-
-    private static final String HELP_PREFIX = "HELP";
-
-    private static final String TYPE_PREFIX = "TYPE";
 
     private static class FormatException extends Exception {
         public FormatException() {}
