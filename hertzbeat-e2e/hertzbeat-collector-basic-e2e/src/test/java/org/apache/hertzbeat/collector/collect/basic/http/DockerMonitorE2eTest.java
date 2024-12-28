edf449c9c0b0762c47f4d170276d79cd24530751
shenghang <shenghang@apache.org>
2024-12-28 21:16:01 +0800

    format

--- hertzbeat-e2e/hertzbeat-collector-basic-e2e/src/test/java/org/apache/hertzbeat/collector/collect/basic/http/DockerMonitorE2eTest.java
+++ hertzbeat-e2e/hertzbeat-collector-basic-e2e/src/test/java/org/apache/hertzbeat/collector/collect/basic/http/DockerMonitorE2eTest.java
@@ -42,7 +42,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
-import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -66,68 +65,37 @@ public class DockerMonitorE2eTest {
     @Mock
     private ObjectStoreConfigServiceImpl objectStoreConfigService;
 
-    @AfterAll
-    public static void tearDown() {
-        if (mockServer != null) {
-            mockServer.stop(0);
-        }
-    }
-
     @BeforeEach
     public void setUp() throws Exception {
-        initializeServices();
-        setupMockServer();
-        setupDockerEndpoints();
-    }
-
-    @Test
-    public void testDockerMonitor() {
-        Job dockerJob = appService.getAppDefine("docker");
-        dockerJob.getMetrics().forEach(this::validateMetricsCollection);
-    }
-
-    private void initializeServices() throws Exception {
+        // Initialize services
         appService.run();
         httpCollector = new HttpCollectImpl();
         metricsTemplate = new Metrics();
-    }
 
-    private void setupMockServer() throws Exception {
+        // Setup mock server and endpoints
         mockServer = HttpServer.create(new InetSocketAddress(MOCK_SERVER_PORT), 0);
         mockServer.setExecutor(null);
         mockServer.start();
-    }
 
-    private void setupDockerEndpoints() throws Exception {
-        setupContainersEndpoint();
-        setupInfoEndpoint();
-    }
+        // Setup Docker API endpoints
+        String containerResponse = loadResponseFromFile("classpath:http/docker/containers_result.txt");
+        String infoResponse = loadResponseFromFile("classpath:http/docker/system_result.txt");
 
-    private void setupContainersEndpoint() throws Exception {
-        String containerResponse = loadResponseFromFile("classpath:http/docker/system_result.txt");
-        mockServer.createContext("/containers/json", exchange -> handleContainersRequest(exchange, containerResponse));
-    }
+        mockServer.createContext("/containers/json", exchange -> {
+            String query = exchange.getRequestURI().getQuery();
+            if (query != null && query.contains("all=true")) {
+                sendJsonResponse(exchange, containerResponse);
+            } else {
+                exchange.sendResponseHeaders(404, 0);
+                exchange.close();
+            }
+        });
 
-    private void setupInfoEndpoint() throws Exception {
-        String infoResponse = loadResponseFromFile("classpath:http/docker/containers_result.txt");
-        mockServer.createContext("/info", exchange -> handleInfoRequest(exchange, infoResponse));
+        mockServer.createContext("/info", exchange -> sendJsonResponse(exchange, infoResponse));
     }
 
     private String loadResponseFromFile(String resourcePath) throws Exception {
         return new String(Files.readAllBytes(ResourceUtils.getFile(resourcePath).toPath()));
-    }
-
-    private void handleContainersRequest(HttpExchange exchange, String response) throws IOException {
-        String query = exchange.getRequestURI().getQuery();
-        if (query != null && query.contains("all=true")) {
-            sendJsonResponse(exchange, response);
-        } else {
-            sendNotFoundResponse(exchange);
-        }
-    }
-
-    private void handleInfoRequest(HttpExchange exchange, String response) throws IOException {
-        sendJsonResponse(exchange, response);
     }
 
     private void sendJsonResponse(HttpExchange exchange, String response) throws IOException {
@@ -138,61 +106,21 @@ public class DockerMonitorE2eTest {
         }
     }
 
-    private void sendNotFoundResponse(HttpExchange exchange) throws IOException {
-        exchange.sendResponseHeaders(404, 0);
-        exchange.close();
+    @Test
+    public void testDockerMonitor() {
+        Job dockerJob = appService.getAppDefine("docker");
+        dockerJob.getMetrics().forEach(this::validateMetricsCollection);
     }
-
 
     private void validateMetricsCollection(Metrics metricsDef) {
-        if (shouldSkipMetric(metricsDef)) {
+        // Skip metrics containing "^o^" as parameter substitution is not supported in e2e tests
+        if (metricsDef.getHttp().getUrl().contains("^o^")) {
             return;
         }
+
         String metricName = metricsDef.getName();
-        CollectRep.MetricsData.Builder metricsData = collectMetrics(metricsDef);
 
-        validateMetricsResults(metricName, metricsData);
-        log.info("{} metrics validation passed", metricName);
-    }
-
-    private boolean shouldSkipMetric(Metrics metricsDef) {
-        // TODO Skip metrics containing "^o^" as parameter substitution is not supported in e2e tests
-        return metricsDef.getHttp().getUrl().contains("^o^");
-    }
-
-    private CollectRep.MetricsData.Builder collectMetrics(Metrics metricsDef) {
-        HttpProtocol httpProtocol = createHttpProtocol(metricsDef);
-        metricsTemplate.setHttp(httpProtocol);
-        metricsTemplate.setAliasFields(determineAliasFields(metricsDef));
-
-        CollectRep.MetricsData.Builder metricsData = CollectRep.MetricsData.newBuilder();
-        httpCollector.collect(metricsData, metricsTemplate);
-        return metricsData;
-    }
-
-    private List<String> determineAliasFields(Metrics metricsDef) {
-        return metricsDef.getAliasFields() == null ? metricsDef.getFields().stream()
-                        .map(Metrics.Field::getField)
-                        .collect(Collectors.toList()) :
-                metricsDef.getAliasFields();
-    }
-
-    private void validateMetricsResults(String metricName, CollectRep.MetricsData.Builder metricsData) {
-        Assertions.assertTrue(metricsData.getValuesList().size() > 0,
-                String.format("%s metrics values should not be empty", metricName));
-
-        CollectRep.ValueRow firstRow = metricsData.getValuesList().get(0);
-        validateRowColumns(metricName, firstRow);
-    }
-
-    private void validateRowColumns(String metricName, CollectRep.ValueRow row) {
-        for (int i = 0; i < row.getColumnsCount(); i++) {
-            Assertions.assertFalse(row.getColumns(i).isEmpty(),
-                    String.format("%s metric column %d should not be empty", metricName, i));
-        }
-    }
-
-    private HttpProtocol createHttpProtocol(Metrics metricsDef) {
+        // Setup HTTP protocol and collect metrics
         HttpProtocol protocol = new HttpProtocol();
         protocol.setHost(LOCALHOST);
         protocol.setPort(String.valueOf(MOCK_SERVER_PORT));
@@ -200,8 +128,33 @@ public class DockerMonitorE2eTest {
         protocol.setParseType(metricsDef.getHttp().getParseType());
         protocol.setParseScript(metricsDef.getHttp().getParseScript());
         protocol.setUrl(metricsDef.getHttp().getUrl());
-        return protocol;
+
+        metricsTemplate.setHttp(protocol);
+        metricsTemplate.setAliasFields(metricsDef.getAliasFields() == null ? metricsDef.getFields().stream()
+                        .map(Metrics.Field::getField)
+                        .collect(Collectors.toList()) : metricsDef.getAliasFields());
+
+        // Collect and validate metrics
+        CollectRep.MetricsData.Builder metricsData = CollectRep.MetricsData.newBuilder();
+        httpCollector.collect(metricsData, metricsTemplate);
+
+        // Validate results
+        Assertions.assertTrue(metricsData.getValuesList().size() > 0,
+                String.format("%s metrics values should not be empty", metricName));
+
+        CollectRep.ValueRow firstRow = metricsData.getValuesList().get(0);
+        for (int i = 0; i < firstRow.getColumnsCount(); i++) {
+            Assertions.assertFalse(firstRow.getColumns(i).isEmpty(),
+                    String.format("%s metric column %d should not be empty", metricName, i));
+        }
+
+        log.info("{} metrics validation passed", metricName);
     }
 
-
+    @AfterAll
+    public static void tearDown() {
+        if (mockServer != null) {
+            mockServer.stop(0);
+        }
+    }
 }
