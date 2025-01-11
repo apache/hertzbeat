1e5a255fc970ec11b82de612a6d9828a78b6b013
tomsun28 <tomsun28@outlook.com>
2025-01-11 17:08:45 +0800

    [webapp] add alert integration
    Signed-off-by: tomsun28 <tomsun28@outlook.com>

--- web-app/src/app/routes/alert/alert-integration/alert-integration.component.spec.ts
+++ web-app/src/app/routes/alert/alert-integration/alert-integration.component.spec.ts
@@ -9,9 +9,8 @@ describe('AlertIntegrationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AlertIntegrationComponent]
-    })
-    .compileComponents();
-    
+    }).compileComponents();
+
     fixture = TestBed.createComponent(AlertIntegrationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
