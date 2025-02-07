aeb0083394169a205957f4521502f042f7d9f010
tomsun28 <tomsun28@outlook.com>
2025-02-07 15:47:19 +0800

    [improve] update theme ui color  (#3057)
    Signed-off-by: tomsun28 <tomsun28@outlook.com>

--- web-app/src/app/layout/basic/basic.component.ts
+++ web-app/src/app/layout/basic/basic.component.ts
@@ -41,9 +41,6 @@ import { CONSTANTS } from '../../shared/constants';
             <div nz-menu-item>
               <header-fullscreen></header-fullscreen>
             </div>
-            <div nz-menu-item>
-              <header-clear-storage></header-clear-storage>
-            </div>
             <div nz-menu-item routerLink="/setting/labels">
               <i nz-icon nzType="tag" class="mr-sm"></i>
               <span style="margin-left: 4px">{{ 'menu.advanced.labels' | i18n }}</span>
