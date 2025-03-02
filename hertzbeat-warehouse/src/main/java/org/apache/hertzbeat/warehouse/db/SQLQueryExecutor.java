5f0132a8d6e8ac11a0217f52ca66afd8722ff11b
vinci <1552443053@qq.com>
2025-03-02 23:49:58 +0800

    add PromqlQueryExecutor

--- hertzbeat-warehouse/src/main/java/org/apache/hertzbeat/warehouse/db/SQLQueryExecutor.java
+++ hertzbeat-warehouse/src/main/java/org/apache/hertzbeat/warehouse/db/SQLQueryExecutor.java
@@ -9,7 +9,16 @@ public abstract class SQLQueryExecutor implements QueryExecutor {
 
     private final static String supportQueryLanguage = SQL;
 
+    protected record ConnectorSQLProperties (
+    ){};
+    protected abstract List<Map<String, Object>> do_sql(Map<String, Object> params);
+
     public abstract List<Map<String, Object>> execute(String query);
+
+    public abstract List<Map<String, Object>> query(String query, long time);
+
+    public abstract List<Map<String, Object>> query_range(String query, long start, long end, String step);
+
 
     public boolean support(String datasource) {
         return supportQueryLanguage.equals(datasource);
--- hertzbeat-warehouse/src/main/java/org/apache/hertzbeat/warehouse/service/impl/MetricsDataQueryServiceImpl.java
+++ hertzbeat-warehouse/src/main/java/org/apache/hertzbeat/warehouse/service/impl/MetricsDataQueryServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -25,11 +26,25 @@ public class MetricsDataQueryServiceImpl implements MetricsDataQueryService {
         if (executor == null) {
             throw new IllegalArgumentException("Unsupported datasource: " + queryType);
         }
+        List<MetricQueryData> metricQueryDataList = new ArrayList<>();
+        for (String query : queries) {
+            executor.query(query, time);
+        }
         return null;
     }
 
     @Override
     public List<MetricQueryData> queryRange(List<String> queries, String queryType, long start, long end, String step) {
+        if (queries == null || executors.isEmpty()) {
+            throw new IllegalArgumentException("No query executor found");
+        }
+        QueryExecutor executor = executors.stream().filter(e -> e.support(queryType)).findFirst().orElse(null);
+        if (executor == null) {
+            throw new IllegalArgumentException("Unsupported datasource: " + queryType);
+        }
+        for (String query : queries) {
+            executor.query_range(query, start, end, step);
+        }
         return null;
     }
 }
