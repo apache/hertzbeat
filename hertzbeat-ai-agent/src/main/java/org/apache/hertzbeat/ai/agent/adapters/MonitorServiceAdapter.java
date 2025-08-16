73a724bbcc6e143ffc1c56073d627df65c3a3c9b
Sarthak Arora <f20200060@pilani.bits-pilani.ac.in>
2025-08-16 16:38:13 +0530

    [GSOC] MCP server setup, authorization, and basic tool support (#3610)
    Signed-off-by: Sarthak Arora <f20200060@pilani.bits-pilani.ac.in>
    Co-authored-by: Calvin <zhengqiwei@apache.org>
    Co-authored-by: Jast <shenghang@apache.org>
    Co-authored-by: tomsun28 <tomsun28@outlook.com>

---
+++ hertzbeat-ai-agent/src/main/java/org/apache/hertzbeat/ai/agent/adapters/MonitorServiceAdapter.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.apache.hertzbeat.ai.agent.adapters;
+
+import org.springframework.data.domain.Page;
+import org.apache.hertzbeat.common.entity.manager.Monitor;
+import java.util.List;
+
+/**
+ * Interface that provides access to monitor information by retrieving monitor data
+ * through the underlying monitor service.
+ */
+public interface MonitorServiceAdapter {
+    Page<Monitor> getMonitors(
+        List<Long> ids,
+        String app,
+        String search,
+        Byte status,
+        String sort,
+        String order,
+        Integer pageIndex,
+        Integer pageSize,
+        String labels
+    );
+} 
